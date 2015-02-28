104ce5e5ac78768ce5d08d57d7662edada82fe61
meghasandesh <msandesh@cs-it-6789454.local>
2015-02-27 17:00:48 -0800

    Revert "[Issue #105] new branch for data upload dismiss"
    This reverts commit 678fee2a2fdefad38cc6fb4e35450718a42b2061.

--- seed/static/seed/js/controllers/data_upload_modal_ctrl.js
+++ seed/static/seed/js/controllers/data_upload_modal_ctrl.js
@@ -96,7 +96,6 @@ angular.module('BE.seed.controller.data_upload_modal', [])
      */
     $scope.goto_step = function (step) {
         $scope.step.number = step;
-        check_suppress_click();
     };
     /**
      * close: closes the modal, routes to the close function of the parent scope
@@ -319,12 +318,6 @@ angular.module('BE.seed.controller.data_upload_modal', [])
         });
     };
 
-    var check_suppress_click = function() {
-        if($scope.step.number === 2) {
-            angular.element('.modal').off('click');
-        }
-    };
-
     /**
      * init: ran upon the controller load
      */
@@ -347,8 +340,6 @@ angular.module('BE.seed.controller.data_upload_modal', [])
                 angular.element('.modal').off('click');
             }
 
-            check_suppress_click();
-            
         }, 50);
     };
     init();
