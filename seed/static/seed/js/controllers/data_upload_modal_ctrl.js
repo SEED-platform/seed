e05d22e7631415113b39111a27dbac2c5c4b5deb
danielmcquillen <daniel@mcquilleninteractive.com>
2015-07-03 11:31:27 +1000

    Fixes Issue #44: 'Count' labels in menu are now updating dynamically when number of relevant item changes. Fix adds  functions in menu controller as well as properties to each related service to represent current count.

--- seed/static/seed/js/controllers/data_upload_modal_ctrl.js
+++ seed/static/seed/js/controllers/data_upload_modal_ctrl.js
@@ -324,7 +324,9 @@ angular.module('BE.seed.controller.data_upload_modal', [])
                             } else {
                                 $scope.step.number = 10;
                             }
+                            building_services.get_total_number_of_buildings_for_user();
                         });
+
                     }, function(data) {
                         // Do nothing
                     },
