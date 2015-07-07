31e6d38add46b98bb4d4d0b49c5efd2821453d32
Nicholas Long <nicholas.long@nrel.gov>
2015-07-07 17:02:04 -0600

    set contents from file object, not filename

--- seed/lib/exporter.py
+++ seed/lib/exporter.py
@@ -6,19 +6,18 @@ Refactored 6/12/15 to make this a class
 
 import os
 import tempfile
+
 import unicodecsv as csv
 import xlwt
-
 from django.conf import settings
 from django.db.models.fields import FieldDoesNotExist
 from django.core.files.storage import DefaultStorage
-
-
 from django.db.models import Manager
 from django.db.models.fields.related import (
     ForeignRelatedObjectsDescriptor,
     ReverseSingleRelatedObjectDescriptor
 )
+
 
 class Exporter:
     """
@@ -71,7 +70,9 @@ class Exporter:
                 f.close()
             else:
                 s3_key = DefaultStorage().bucket.new_key(self.filename())
-                s3_key.set_contents_from_file(self.tempfile)
+                f = open(self.tempfile)
+                s3_key.set_contents_from_file(f)
+                f.close()
                 os.remove(self.tempfile)
 
         return self.filename
@@ -105,7 +106,6 @@ class Exporter:
             except FieldDoesNotExist:
                 continue
 
-
     def subdirectory(self):
         """
         Create and return the subdirectory
@@ -124,7 +124,6 @@ class Exporter:
         """
         return os.path.join(self.subdirectory(), "%s.%s" % (self.export_name, self.export_type))
 
-
     # Old methods that should be converted into private methods (will require test changes)
 
     def _make_object_row(self, obj, fields):
@@ -141,7 +140,6 @@ class Exporter:
             else:
                 row.append(unicode(value))
         return row
-
 
     def _get_field_name(self, field, qs):
         """
