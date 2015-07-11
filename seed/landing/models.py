2828e6567d4011d4e30196fb3b9334a58480bc63
Miles Clark <mmc@obispo.com>
2015-07-11 21:41:40 +0000

    Commented out import of SiteProfileNotAvailable, which is no longer available in django 1.7

--- seed/landing/models.py
+++ seed/landing/models.py
@@ -12,10 +12,12 @@ except ImportError:
     import sha
     sha1 = sha.sha
 
-from django.contrib.auth.models import (AbstractBaseUser,
-                                        PermissionsMixin,
-                                        UserManager,
-                                        SiteProfileNotAvailable)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    UserManager,
+    # SiteProfileNotAvailable
+)
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.db import models
