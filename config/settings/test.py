7c083f07256c8bcb3de8eeb19168f75d263d73c1
Nicholas Long <nicholas.long@nrel.gov>
2015-07-24 14:56:56 -0600

    start at uploading data file

--- config/settings/test.py
+++ config/settings/test.py
@@ -74,7 +74,6 @@ NOSE_PLUGINS = [
     'nose_exclude.NoseExclude',
 ]
 NOSE_ARGS = [
-    '--exclude-dir=seed/data_importer',
     '--exclude-dir=seed/common',
     '--nocapture',
     '--nologcapture'
--- seed/tests/functional/test_api.py
+++ seed/tests/functional/test_api.py
@@ -1,10 +1,9 @@
-from django.test import TestCase
-# from selenium.webdriver.firefox.webdriver import WebDriver
+import json
+import os
 
+from django.test import TestCase
 from seed.lib.superperms.orgs.models import Organization, OrganizationUser
 from seed.landing.models import SEEDUser as User
-
-import json
 
 
 class TestApi(TestCase):
@@ -150,5 +149,123 @@ class TestApi(TestCase):
 
         r = self.client.get('/app/accounts/get_organization/?organization_id=%s' % organization_id, follow=True,
                             **self.headers)
+        self.assertEqual(r.status_code, 200)
         r = json.loads(r.content)
         self.assertEqual(r['organization']['number_of_users'], 2)
+
+        # get org users
+        r = self.client.post('/app/accounts/get_organizations_users/', data='{"organization_id": %s}' % organization_id,
+                             content_type='application/json', **self.headers)
+        self.assertEqual(r.status_code, 200)
+        # {
+        #     "status": "success",
+        #     "users": [
+        #         {
+        #             "role": "owner",
+        #             "first_name": "Jaqen",
+        #             "last_name": "H'ghar",
+        #             "user_id": 1,
+        #             "email": "test_user@demo.com"
+        #         },
+        #         {
+        #             "role": "member",
+        #             "first_name": "Brienne",
+        #             "last_name": "Tarth",
+        #             "user_id": 2,
+        #             "email": "test+1@demo.com"
+        #         }
+        #     ]
+        # }
+
+        r = json.loads(r.content)
+        self.assertEqual(r['status'], 'success')
+        self.assertEqual(len(r['users']), 2)
+
+        # get the user id of the new user
+        user_id = [i for i in r['users'] if i['last_name'] == 'Tarth'][0]['user_id']
+
+        # Change the user role
+        payload = {
+            'organization_id': organization_id,
+            'user_id': user_id,
+            'role': 'owner'
+        }
+
+        r = self.client.post('/app/accounts/update_role/', data=json.dumps(payload), content_type='application/json',
+                             **self.headers)
+
+        self.assertEqual(r.status_code, 200)
+        r = json.loads(r.content)
+        self.assertEqual(r['status'], 'success')
+
+        r = self.client.post('/app/accounts/get_organizations_users/', data='{"organization_id": %s}' % organization_id,
+                             content_type='application/json', **self.headers)
+        self.assertEqual(r.status_code, 200)
+        r = json.loads(r.content)
+        new_user = [i for i in r['users'] if i['last_name'] == 'Tarth'][0]
+        self.assertEqual(new_user['role'], 'owner')
+
+    def test_get_query_threshold(self):
+        r = self.client.get('/app/accounts/get_organizations/', follow=True, **self.headers)
+        r = json.loads(r.content)
+        organization_id = self.get_org_id(r, self.user.username)
+
+        r = self.client.get("/app/accounts/get_query_threshold/?organization_id=%s" % organization_id, follow=True,
+                            **self.headers)
+        self.assertEqual(r.status_code, 200)
+        r = json.loads(r.content)
+        self.assertEqual(r['status'], 'success')
+        self.assertEqual(r['query_threshold'], None)
+
+    def test_shared_fields(self):
+        r = self.client.get('/app/accounts/get_organizations/', follow=True, **self.headers)
+        r = json.loads(r.content)
+        organization_id = self.get_org_id(r, self.user.username)
+
+        r = self.client.get("/app/accounts/get_shared_fields/?organization_id=%s" % organization_id, follow=True,
+                            **self.headers)
+
+        self.assertEqual(r.status_code, 200)
+        r = json.loads(r.content)
+        self.assertEqual(r['status'], 'success')
+        self.assertEqual(r['public_fields'], [])
+        self.assertEqual(r['shared_fields'], [])
+
+    def test_upload_buildings_file(self):
+        r = self.client.get('/app/accounts/get_organizations/', follow=True, **self.headers)
+        r = json.loads(r.content)
+        organization_id = self.get_org_id(r, self.user.username)
+
+        raw_building_file = os.path.relpath(os.path.join('seed/tests/data', 'covered-buildings-sample.csv'))
+        self.assertTrue(os.path.isfile(raw_building_file), 'could not find file')
+
+        payload = {'organization_id': organization_id, 'name': 'API Test'}
+
+        # create the data set
+        r = self.client.post('/app/create_dataset/', data=json.dumps(payload), content_type='application/json',
+                             **self.headers)
+        self.assertEqual(r.status_code, 200)
+        r = json.loads(r.content)
+        self.assertEqual(r['status'], 'success')
+
+        data_set_id = r['id']
+
+        # retrieve the upload details
+        upload_details = self.client.get('/data/get_upload_details/', follow=True, **self.headers)
+        self.assertEqual(upload_details.status_code, 200)
+        upload_details = json.loads(upload_details.content)
+        self.assertEqual(upload_details['upload_mode'], 'filesystem')
+
+        fsysparams = {
+            'qqfile': raw_building_file,
+            'import_record': data_set_id,
+            'source_type': 'Assessed Raw'
+        }
+
+        #  files={'filename': open(raw_building_file, 'rb')},
+        # print upload_details['upload_path']
+        # r = self.client.post(upload_details['upload_path'], json.dumps(fsysparams),
+        #                      content_type='application/json', **self.headers)
+        # self.assertEqual(r.status_code, 200)
+        # print r
+
