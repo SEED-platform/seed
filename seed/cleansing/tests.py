5e58223c5c4185cabbc986fc6e08ab8a705e7444
Nicholas Long <nicholas.long@nrel.gov>
2015-09-25 09:45:07 -0600

    add simple test for data cleasning file

--- seed/cleansing/tests.py
+++ seed/cleansing/tests.py
@@ -105,7 +105,7 @@ class CleansingDataTestCoveredBuilding(TestCase):
         else:
             raise RuntimeError('Non unity results')
 
-        res = [{'field': u'year_built', 'message': 'Value [0] < 1500', 'severity': u'warning'},
+        res = [{'field': u'year_built', 'message': 'Value [0] < 1700', 'severity': u'error'},
                {'field': u'gross_floor_area', 'message': 'Value [10000000000.0] > 7000000', 'severity': u'error'},
                {'field': u'custom_id_1', 'message': 'Value is missing', 'severity': 'error'},
                {'field': u'pm_property_id', 'message': 'Value is missing', 'severity': 'error'}]
@@ -189,9 +189,7 @@ class CleansingDataTestPM(TestCase):
         else:
             raise RuntimeError('Non unity results')
 
-        res = [{'field': u'tax_lot_id', 'message': 'Matching field not found', 'severity': 'error'},
-               {'field': u'custom_id_1', 'message': 'Matching field not found', 'severity': 'error'},
-               {'field': u'pm_property_id', 'message': 'Value is missing', 'severity': 'error'}]
+        res = [{'field': u'pm_property_id', 'message': 'Value is missing', 'severity': 'error'}]
         self.assertEqual(res, result['cleansing_results'])
 
         result = [v for v in c.results.values() if v['address_line_1'] == '95373 E Peach Avenue']
@@ -200,7 +198,101 @@ class CleansingDataTestPM(TestCase):
         else:
             raise RuntimeError('Non unity results')
 
-        res = [{'field': u'tax_lot_id', 'message': 'Matching field not found', 'severity': 'error'},
-               {'field': u'custom_id_1', 'message': 'Matching field not found', 'severity': 'error'},
-               {'field': u'site_eui', 'message': 'Value [0.1] < 10', 'severity': u'warning'}]
+        res = [{'field': u'site_eui', 'message': 'Value [0.1] < 10', 'severity': u'warning'}]
         self.assertEqual(res, result['cleansing_results'])
+
+
+class CleansingDataSample(TestCase):
+    def setUp(self):
+        self.user_details = {
+            'username': 'testuser@example.com',
+            'email': 'testuser@example.com',
+            'password': 'test_password'
+        }
+        self.user = User.objects.create_user(**self.user_details)
+        self.login_url = reverse('landing:login')
+
+        self.org = Organization.objects.create()
+        OrganizationUser.objects.create(user=self.user, organization=self.org)
+
+        self.import_record = ImportRecord.objects.create(owner=self.user)
+        self.import_record.super_organization = self.org
+        self.import_record.save()
+        self.import_file = ImportFile.objects.create(
+            import_record=self.import_record
+        )
+
+        self.import_file.is_espm = False
+        self.import_file.source_type = 'ASSESSED_RAW'
+        self.import_file.file = File(
+            open(path.join(path.dirname(__file__), 'test_data', 'data-cleansing-sample.csv'))
+        )
+
+        self.import_file.save()
+
+        # tasks.save_raw_data(self.import_file.pk)
+
+    def test_cleanse(self):
+        # Import the file and run mapping
+
+        # This is silly, the mappings are backwards from what you would expect. The key is the BS field, and the
+        # value is the value in the CSV
+        fake_mappings = {
+            'block_number': 'block_number',
+            'error_type': 'error type',
+            'building_count': 'building_count',
+            'conditioned_floor_area': 'conditioned_floor_area',
+            'energy_score': 'energy_score',
+            'gross_floor_area': 'gross_floor_area',
+            'lot_number': 'lot_number',
+            'occupied_floor_area': 'occupied_floor_area',
+            'postal_code': 'postal_code',
+            'site_eui': 'site_eui',
+            'site_eui_weather_normalized': 'site_eui_weather_normalized',
+            'source_eui': 'source_eui',
+            'source_eui_weather_normalized': 'source_eui_weather_normalized',
+            'address_line_1': 'address_line_1',
+            'address_line_2': 'address_line_2',
+            'building_certification': 'building_certification',
+            'city': 'city',
+            'custom_id_1': 'custom_id_1',
+            'district': 'district',
+            'energy_alerts': 'energy_alerts',
+            'owner': 'owner',
+            'owner_address': 'owner_address',
+            'owner_city_state': 'owner_city_state',
+            'owner_email': 'owner_email',
+            'owner_postal_code': 'owner_postal_code',
+            'owner_telephone': 'owner_telephone',
+            'pm_property_id': 'pm_property_id',
+            'property_name': 'property_name',
+            'property_notes': 'property_notes',
+            'space_alerts': 'space_alerts',
+            'state_province': 'state_province',
+            'tax_lot_id': 'tax_lot_id',
+            'use_description': 'use_description',
+            'generation_date': 'generation_date',
+            'recent_sale_date': 'recent_sale_date',
+            'release_date': 'release_date',
+            'year_built': 'year_built',
+            'year_ending': 'year_ending',
+        }
+
+        tasks.save_raw_data(self.import_file.id)
+        util.make_fake_mappings(fake_mappings, self.org)
+        tasks.map_data(self.import_file.id)
+
+        qs = BuildingSnapshot.objects.filter(
+            import_file=self.import_file,
+            source_type=ASSESSED_BS,
+        ).iterator()
+
+        c = Cleansing()
+        c.cleanse(qs)
+
+        data = c.results
+
+        # print data
+        # This only checks to make sure the 32 errors have occurred.
+        self.assertEqual(len(c.results), 32)
+
