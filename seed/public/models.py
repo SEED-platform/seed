055582dda2b333f1ead947356c0f837fa9586dee
Piper Merriam <pmerriam@quickleft.com>
2016-01-14 14:12:04 -0700

    Flake8 fixes for new pylint version

--- seed/public/models.py
+++ seed/public/models.py
@@ -3,13 +3,11 @@
 """
 :copyright (c) 2014 - 2015, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.  # NOQA
 :author
-"""
-"""
+
 Because migrations are complicated, we're keeping our public fields here.
 
 This deals with circular dependency issues between LANDINGUser and Organization
 """
-
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
