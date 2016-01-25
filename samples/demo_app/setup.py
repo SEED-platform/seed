c124ffa7f637ac67defbfb11bde716f7c0020b72
Stephen Czarnecki <sczarnecki@lbl.gov>
2016-01-25 20:31:08 +0000

    Updating copyright on other python files that still had 2014 Building Energy message.

--- samples/demo_app/setup.py
+++ samples/demo_app/setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-:copyright: (c) 2014 Building Energy Inc
+:copyright (c) 2014 - 2015, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.  # NOQA
+:author
 """
 from setuptools import setup, find_packages
 
