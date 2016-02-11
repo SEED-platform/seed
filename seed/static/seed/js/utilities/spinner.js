5909d42ee5a0b91fda69824ecd142035a4189ceb
Nicholas Serra <nicks@ydekproductions.com>
2016-02-10 21:47:41 -0500

    Tiny bit of refactoring. This may need to return instances.

--- seed/static/seed/js/utilities/spinner.js
+++ seed/static/seed/js/utilities/spinner.js
@@ -2,19 +2,20 @@
  * :copyright (c) 2014 - 2016, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.
  * :author
  */
-angular.module('BE.seed.utility.spinner', []).service('spinner_utility', [
+angular.module('BE.seed.utility.spinner', []).factory('spinner_utility', [
   function () {
 
-    var spinner_utility = {spinner : null};
+    var spinner_utility = {};
+    var _spinner;
 
     spinner_utility.show = function(params, target) {
-      var target = target || $('.display')[0]
-      spinner_utility.spinner = new Spinner(params).spin(target);
+      var target = target || $('.display')[0];
+      _spinner = new Spinner(params).spin(target);
       $('.page')[0].style.opacity = 0.4;
     };
 
     spinner_utility.hide = function() {
-      spinner_utility.spinner.stop();
+      _spinner.stop();
       $('.page')[0].style.opacity = 1;
     };
 
