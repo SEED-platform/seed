a7ac7b2694e685e0609225b547d9a4747d49767b
Nicholas Long <nicholas.long@nrel.gov>
2016-03-01 08:54:55 -0700

    update migration

---
+++ seed/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('seed', '0013_auto_20160225_1213'),
+        ('seed', '0013_auto_20160204_1338'),
+    ]
+
+    operations = [
+    ]
