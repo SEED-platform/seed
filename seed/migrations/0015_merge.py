d6064d604f5611437cf163a732b499dc5f589450
wanghp18 <wanghp18@gmail.com>
2016-03-11 13:06:16 -0500

    migration merge

---
+++ seed/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('seed', '0014_merge'),
+        ('seed', '0014_auto_20160308_1106'),
+    ]
+
+    operations = [
+    ]
