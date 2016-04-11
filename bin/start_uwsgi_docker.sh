d5553cfd1125c1fe42e8ea27b59f0b40e029ea28
Nicholas Serra <nicks@ydekproductions.com>
2016-04-11 16:55:59 -0400

    Remove new relic stuff

--- bin/start_uwsgi_docker.sh
+++ bin/start_uwsgi_docker.sh
@@ -7,6 +7,6 @@ cd /seed
 
 WORKERS=$(($(nproc) / 2))
 WORKERS=$(($WORKERS>1?$WORKERS:1))
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program /usr/local/bin/uwsgi --http 0.0.0.0:8000 --module wsgi \
+/usr/local/bin/uwsgi --http 0.0.0.0:8000 --module wsgi \
     --max-requests 5000 --cheaper-initial 1 -p $WORKERS --single-interpreter --enable-threads \
     --wsgi-file /seed/config/wsgi.py
