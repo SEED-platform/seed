bb509e2770728d6954f2cf96d2a8fdc9b2972698
wanghp18 <wanghp18@gmail.com>
2016-04-29 21:33:04 +0000

    force insert for GB unit test

--- seed/energy/tests/test_GreenButton_request.py
+++ seed/energy/tests/test_GreenButton_request.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 
-from django.conf import settings
 from seed.energy.meter_data_processor import green_button_driver as driver
 from seed.energy.meter_data_processor import green_button_data_analyser as analyser
 from seed.models import (
@@ -12,7 +11,7 @@ class GreenButtonRequest(TestCase):
     def test_save_green_button_data(self):
         url = 'https://epo.schneider-electric.com/PEPCO/espi/1_1/resource/Batch/Subscription.aspx?SubscriptionID=C8C25FC1C944B813A5CB790&Published_Min=11/1/2015&Published_Max=11/1/2015'
         new_canonical_bld = CanonicalBuilding(id=99999, active=False)
-        new_canonical_bld.save()
+        new_canonical_bld.save(force_insert=True)
 
         ts_data = driver.get_gb_data(url, 99999)
         self.assertTrue(ts_data)
