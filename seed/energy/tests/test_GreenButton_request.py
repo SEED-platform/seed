a2b2974277766318b32d364ccf8ac91dbf7e343f
wanghp18 <wanghp18@gmail.com>
2016-05-02 18:55:59 +0000

    Add signal capturer for GB test

--- seed/energy/tests/test_GreenButton_request.py
+++ seed/energy/tests/test_GreenButton_request.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 
 from seed.energy.meter_data_processor import green_button_driver as driver
 from seed.energy.meter_data_processor import green_button_data_analyser as analyser
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from seed.models import (
     CanonicalBuilding,
 )
@@ -10,11 +14,18 @@ from seed.models import (
 class GreenButtonRequest(TestCase):
     def test_save_green_button_data(self):
         url = 'https://epo.schneider-electric.com/PEPCO/espi/1_1/resource/Batch/Subscription.aspx?SubscriptionID=C8C25FC1C944B813A5CB790&Published_Min=11/1/2015&Published_Max=11/1/2015'
-        new_canonical_bld = CanonicalBuilding(id=99999, active=False)
+        new_canonical_bld = CanonicalBuilding(id=99999, active=True)
         new_canonical_bld.save(force_insert=True)
 
-        print new_canonical_bld.pk
-        self.assertTrue(CanonicalBuilding.objects.get(pk=new_canonical_bld.pk))
+
+@receiver(post_save, sender=CanonicalBuilding, dispatch_uid="GreenButtonRequest_cont")
+def GreenButtonRequest_cont(sender, instance, **kwargs):
+    print "Post save signal captured"
+    test_bld = CanonicalBuilding.objects.get(pk=99999)
+    self.assertTrue(test_bld)
+
+    if test_bld.active:
+        test_bld.active = False
 
         ts_data = driver.get_gb_data(url, 99999)
         self.assertTrue(ts_data)
