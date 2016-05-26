5030930ccf5980aac9cf1e577fdd717b470875d6
Nicholas Serra <nicks@ydekproductions.com>
2016-05-26 17:38:17 -0400

    Json output

--- seed/bluesky/views.py
+++ seed/bluesky/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
 
+from django.forms.models import model_to_dict
+
+
 from seed.bluesky.models import PropertyView, TaxLotView, TaxLotProperty
 from seed.decorators import ajax_request, require_organization_id
 from seed.lib.superperms.orgs.decorators import has_perm
@@ -15,9 +18,15 @@ def get_properties(request):
     property_views = PropertyView.objects.select_related('property', 'state', 'cycle') \
         .filter(property__organization_id=request.GET['organization_id'])
 
-    return {
-        'property_views': property_views
-    }
+    response = []
+    for prop in property_views:
+        p = model_to_dict(prop)
+        p['state'] = model_to_dict(prop.state)
+        p['property'] = model_to_dict(prop.property)
+        p['cycle'] = model_to_dict(prop.cycle)
+        response.append(p)
+
+    return response
 
 
 @require_organization_id
@@ -27,15 +36,22 @@ def get_properties(request):
 @has_perm('requires_viewer')
 def get_property(request, property_pk):
     property_view = PropertyView.objects.select_related('property', 'cycle', 'state') \
-        .filter(property_id=property_pk, property__organization_id=request.GET['organization_id'])
+        .get(property_id=property_pk, property__organization_id=request.GET['organization_id'])
 
     # Lots this property is on
     lot_view_pks = TaxLotProperty.objects.filter(property_view_id=property_view.pk).values_list('taxlot_view_id', flat=True)
     lot_views = TaxLotView.objects.filter(pk__in=lot_view_pks).select_related('cycle', 'state')
-    return {
-        'property_view': property_view,
-        'lot_views': lot_views,
-    }
+
+    p = model_to_dict(property_view)
+    p['state'] = model_to_dict(property_view.state)
+    p['property'] = model_to_dict(property_view.property)
+    p['cycle'] = model_to_dict(property_view.cycle)
+    p['lots'] = []
+
+    for lot in lot_views:
+        p['lots'].append(model_to_dict(lot))
+
+    return p
 
 
 @require_organization_id
@@ -47,9 +63,15 @@ def get_taxlots(request):
     taxlot_views = TaxLotView.objects.select_related('taxlot', 'state', 'cycle') \
         .filter(taxlot__organization_id=request.GET['organization_id'])
 
-    return {
-        'taxlot_views': taxlot_views
-    }
+    response = []
+    for lot in taxlot_views:
+        l = model_to_dict(lot)
+        l['state'] = model_to_dict(lot.state)
+        l['taxlot'] = model_to_dict(lot.taxlot)
+        l['cycle'] = model_to_dict(lot.cycle)
+        response.append(l)
+
+    return response
 
 
 @require_organization_id
@@ -59,13 +81,20 @@ def get_taxlots(request):
 @has_perm('requires_viewer')
 def get_taxlot(request, taxlot_pk):
     taxlot_view = TaxLotView.objects.select_related('taxlot', 'cycle', 'state') \
-        .filter(taxlot_id=taxlot_pk, taxlot__organization_id=request.GET['organization_id'])
+        .get(taxlot_id=taxlot_pk, taxlot__organization_id=request.GET['organization_id'])
 
     # Properties on this lot
     property_view_pks = TaxLotProperty.objects.filter(taxlot_view_id=taxlot_view.pk).values_list('property_view_id', flat=True)
     property_views = PropertyView.objects.filter(pk__in=property_view_pks).select_related('cycle', 'state')
-    return {
-        'taxlot_view': taxlot_view,
-        'property_views': property_views,
-    }
+
+    l = model_to_dict(taxlot_view)
+    l['state'] = model_to_dict(taxlot_view.state)
+    l['taxlot'] = model_to_dict(taxlot_view.taxlot)
+    l['cycle'] = model_to_dict(taxlot_view.cycle)
+    l['properties'] = []
+
+    for prop in property_views:
+        l['properties'].append(model_to_dict(prop))
+
+    return l
 
