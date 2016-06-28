8e31c5bf1e9398da61b1b6d7f97d560a8268d464
Nathan Addy <nathan.addy@gmail.com>
2016-06-27 17:16:49 -0700

    Flake8

--- seed/bluesky/management/commands/destroy_bluesky_data.py
+++ seed/bluesky/management/commands/destroy_bluesky_data.py
@@ -1,43 +1,45 @@
 from __future__ import unicode_literals
 
-from django.db import models, migrations
 from django.core.management.base import BaseCommand
 from django.apps import apps
-import pdb
+
 
 def delete_based_on_org(apps, org):
-#     Things that can be deleted because they have a direct reference to an org:
-#
-#     Cycle
-#     Property
-#     TaxLot
-#
-#     Things that can be deleted because they have a reference to something with a direct reference:
-#
-#     PropertyView -> Cycle
-#     TaxLotView -> Cycle
-#     TaxLotProperty -> Cycle
-#
-#     Things that are 2 levels removed:
-#     PropertyState <- PropertyView
-#     TaxLotState <- TaxLotView
-#
-#     Order of deletion:
-#
-#     Delete PropertyStates and TaxLotStates
-#     Delete PropertyViews, TaxLotViews, and TaxLotProperties
-#     Delete Cycles, Properties, TaxLots
+    """Delete data in the BlueSky tables associated with an organiation"""
+    #     Things that can be deleted because they have a direct reference to an org:
+    #
+    #     Cycle
+    #     Property
+    #     TaxLot
+    #
+    #     Things that can be deleted because they have a reference to something with a direct reference:
+    #
+    #     PropertyView -> Cycle
+    #     TaxLotView -> Cycle
+    #     TaxLotProperty -> Cycle
+    #
+    #     Things that are 2 levels removed:
+    #     PropertyState <- PropertyView
+    #     TaxLotState <- TaxLotView
+    #
+    #     Order of deletion:
+    #
+    #     Delete PropertyStates and TaxLotStates
+    #     Delete PropertyViews, TaxLotViews, and TaxLotProperties
+    #     Delete Cycles, Properties, TaxLots
 
-    apps.get_model("bluesky", "PropertyState").objects.filter(propertyview__cycle__organization__id = org).delete()
-    apps.get_model("bluesky", "TaxLotState").objects.filter(taxlotview__cycle__organization__id = org).delete()
+    apps.get_model("bluesky", "PropertyState").objects.filter(propertyview__cycle__organization__id=org).delete()
+    apps.get_model("bluesky", "TaxLotState").objects.filter(taxlotview__cycle__organization__id=org).delete()
 
-    apps.get_model("bluesky", "PropertyView").objects.filter(cycle__organization__id = org).delete()
-    apps.get_model("bluesky", "TaxLotView").objects.filter(cycle__organization__id = org).delete()
-    apps.get_model("bluesky", "TaxLotProperty").objects.filter(cycle__organization__id = org).delete()
+    apps.get_model("bluesky", "PropertyView").objects.filter(cycle__organization__id=org).delete()
+    apps.get_model("bluesky", "TaxLotView").objects.filter(cycle__organization__id=org).delete()
+    apps.get_model("bluesky", "TaxLotProperty").objects.filter(cycle__organization__id=org).delete()
 
-    apps.get_model("bluesky", "Cycle").objects.filter(organization__id = org).delete()
-    apps.get_model("bluesky", "Property").objects.filter(organization__id = org).delete()
-    apps.get_model("bluesky", "TaxLot").objects.filter(organization__id = org).delete()
+    apps.get_model("bluesky", "Cycle").objects.filter(organization__id=org).delete()
+    apps.get_model("bluesky", "Property").objects.filter(organization__id=org).delete()
+    apps.get_model("bluesky", "TaxLot").objects.filter(organization__id=org).delete()
+    return
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
