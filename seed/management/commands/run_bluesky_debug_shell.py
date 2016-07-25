7d62d6a276a6266555b43f88237c0cda927c310c
Nicholas Long <nicholas.long@nrel.gov>
2016-07-25 15:06:21 -0600

    hook back up the lab icon

--- seed/management/commands/run_bluesky_debug_shell.py
+++ seed/management/commands/run_bluesky_debug_shell.py
@@ -8,7 +8,6 @@ import os
 
 # DEBUG - HOHO
 from seed.models import *
-from seed.models import *
 
 from django.db import models, migrations
 from django.core.management.base import BaseCommand
@@ -21,7 +20,6 @@ import os
 import logging
 from IPython import embed
 # from networkx.drawing.nx_agraph import graphviz_layout
-import seed.models
 # import numpy as np
 # from scipy.sparse import dok_matrix
 # from scipy.sparse.csgraph import connected_components
