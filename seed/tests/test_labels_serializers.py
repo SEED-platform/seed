d0a75105afbb8dfeab49f7af5e0d24ac6562c0e2
Nicholas Long <nicholas.long@nrel.gov>
2016-10-04 14:06:50 -0600

    spelling and remove UpdateBuildingLabelSerializer"

--- seed/tests/test_labels_serializers.py
+++ seed/tests/test_labels_serializers.py
@@ -20,7 +20,6 @@ from seed.models import (
 )
 from seed.serializers.labels import (
     LabelSerializer,
-    UpdateBuildingLabelsSerializer,
 )
 
 """
@@ -113,135 +112,135 @@ class TestLabelSerializer(TestCase):
         self.assertFalse(serializer.data['is_applied'])
 
 
-@skip('CanonicalBuilding is referenced many times in this.')
-class TestUpdateBuildingLabelsSerializer(TestCase):
-    def test_initialization_requires_organization_as_argument(self):
-        with self.assertRaises(KeyError):
-            UpdateBuildingLabelsSerializer(
-                queryset=CanonicalBuilding.objects.none(),
-            )
+# @skip('CanonicalBuilding is referenced many times in this.')
+# class TestUpdateBuildingLabelsSerializer(TestCase):
+#     def test_initialization_requires_organization_as_argument(self):
+#         with self.assertRaises(KeyError):
+#             UpdateBuildingLabelsSerializer(
+#                 queryset=CanonicalBuilding.objects.none(),
+#             )
 
-        organization = SuperOrganization.objects.create(name='test-org')
-        UpdateBuildingLabelsSerializer(
-            queryset=CanonicalBuilding.objects.none(),
-            super_organization=organization,
-        )
+#         organization = SuperOrganization.objects.create(name='test-org')
+#         UpdateBuildingLabelsSerializer(
+#             queryset=CanonicalBuilding.objects.none(),
+#             super_organization=organization,
+#         )
 
-    def test_initialization_requires_queryset_as_argument(self):
-        organization = SuperOrganization.objects.create(name='test-org')
+#     def test_initialization_requires_queryset_as_argument(self):
+#         organization = SuperOrganization.objects.create(name='test-org')
 
-        with self.assertRaises(KeyError):
-            UpdateBuildingLabelsSerializer(
-                super_organization=organization,
-            )
+#         with self.assertRaises(KeyError):
+#             UpdateBuildingLabelsSerializer(
+#                 super_organization=organization,
+#             )
 
-        UpdateBuildingLabelsSerializer(
-            super_organization=organization,
-            queryset=CanonicalBuilding.objects.none(),
-        )
+#         UpdateBuildingLabelsSerializer(
+#             super_organization=organization,
+#             queryset=CanonicalBuilding.objects.none(),
+#         )
 
-    def test_labels_are_applied(self):
-        organization = SuperOrganization.objects.create(name='test-org')
+#     def test_labels_are_applied(self):
+#         organization = SuperOrganization.objects.create(name='test-org')
 
-        label_a = Label.objects.create(
-            color="red",
-            name="label-a",
-            super_organization=organization,
-        )
-        label_b = Label.objects.create(
-            color="red",
-            name="label-b",
-            super_organization=organization,
-        )
+#         label_a = Label.objects.create(
+#             color="red",
+#             name="label-a",
+#             super_organization=organization,
+#         )
+#         label_b = Label.objects.create(
+#             color="red",
+#             name="label-b",
+#             super_organization=organization,
+#         )
 
-        bs_1 = SEEDFactory.building_snapshot(
-            canonical_building=CanonicalBuilding.objects.create(),
-        )
-        bs_2 = SEEDFactory.building_snapshot(
-            canonical_building=CanonicalBuilding.objects.create(),
-        )
+#         bs_1 = SEEDFactory.building_snapshot(
+#             canonical_building=CanonicalBuilding.objects.create(),
+#         )
+#         bs_2 = SEEDFactory.building_snapshot(
+#             canonical_building=CanonicalBuilding.objects.create(),
+#         )
 
-        self.assertFalse(bs_1.canonical_building.labels.filter(pk=label_a.pk).exists())
-        self.assertFalse(bs_1.canonical_building.labels.filter(pk=label_b.pk).exists())
-        self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_a.pk).exists())
-        self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_b.pk).exists())
+#         self.assertFalse(bs_1.canonical_building.labels.filter(pk=label_a.pk).exists())
+#         self.assertFalse(bs_1.canonical_building.labels.filter(pk=label_b.pk).exists())
+#         self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_a.pk).exists())
+#         self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_b.pk).exists())
 
-        qs = CanonicalBuilding.objects.all()
-        self.assertEqual(qs.count(), 2)
+#         qs = CanonicalBuilding.objects.all()
+#         self.assertEqual(qs.count(), 2)
 
-        data = {
-            'add_label_ids': [label_a.pk, label_b.pk],
-            'remove_label_ids': [],
-            'selected_buildings': [],
-            'select_all_checkbox': True,
-        }
+#         data = {
+#             'add_label_ids': [label_a.pk, label_b.pk],
+#             'remove_label_ids': [],
+#             'selected_buildings': [],
+#             'select_all_checkbox': True,
+#         }
 
-        serializer = UpdateBuildingLabelsSerializer(
-            data=data,
-            super_organization=organization,
-            queryset=qs,
-        )
-        self.assertTrue(serializer.is_valid(), serializer.errors)
+#         serializer = UpdateBuildingLabelsSerializer(
+#             data=data,
+#             super_organization=organization,
+#             queryset=qs,
+#         )
+#         self.assertTrue(serializer.is_valid(), serializer.errors)
 
-        serializer.save()
-        self.assertEqual(qs.count(), 2)
+#         serializer.save()
+#         self.assertEqual(qs.count(), 2)
 
-        self.assertTrue(bs_1.canonical_building.labels.filter(pk=label_a.pk).exists())
-        self.assertTrue(bs_1.canonical_building.labels.filter(pk=label_b.pk).exists())
-        self.assertTrue(bs_2.canonical_building.labels.filter(pk=label_a.pk).exists())
-        self.assertTrue(bs_2.canonical_building.labels.filter(pk=label_b.pk).exists())
+#         self.assertTrue(bs_1.canonical_building.labels.filter(pk=label_a.pk).exists())
+#         self.assertTrue(bs_1.canonical_building.labels.filter(pk=label_b.pk).exists())
+#         self.assertTrue(bs_2.canonical_building.labels.filter(pk=label_a.pk).exists())
+#         self.assertTrue(bs_2.canonical_building.labels.filter(pk=label_b.pk).exists())
 
-    def test_labels_are_removed(self):
-        organization = SuperOrganization.objects.create(name='test-org')
+#     def test_labels_are_removed(self):
+#         organization = SuperOrganization.objects.create(name='test-org')
 
-        label_a = Label.objects.create(
-            color="red",
-            name="label-a",
-            super_organization=organization,
-        )
-        label_b = Label.objects.create(
-            color="red",
-            name="label-b",
-            super_organization=organization,
-        )
+#         label_a = Label.objects.create(
+#             color="red",
+#             name="label-a",
+#             super_organization=organization,
+#         )
+#         label_b = Label.objects.create(
+#             color="red",
+#             name="label-b",
+#             super_organization=organization,
+#         )
 
-        bs_1 = SEEDFactory.building_snapshot(
-            canonical_building=CanonicalBuilding.objects.create(),
-        )
-        bs_2 = SEEDFactory.building_snapshot(
-            canonical_building=CanonicalBuilding.objects.create(),
-        )
+#         bs_1 = SEEDFactory.building_snapshot(
+#             canonical_building=CanonicalBuilding.objects.create(),
+#         )
+#         bs_2 = SEEDFactory.building_snapshot(
+#             canonical_building=CanonicalBuilding.objects.create(),
+#         )
 
-        bs_1.canonical_building.labels.add(label_a, label_b)
-        bs_2.canonical_building.labels.add(label_b)
+#         bs_1.canonical_building.labels.add(label_a, label_b)
+#         bs_2.canonical_building.labels.add(label_b)
 
-        # Sanity check
-        self.assertTrue(bs_1.canonical_building.labels.filter(pk=label_a.pk).exists())
-        self.assertTrue(bs_1.canonical_building.labels.filter(pk=label_b.pk).exists())
-        self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_a.pk).exists())
-        self.assertTrue(bs_2.canonical_building.labels.filter(pk=label_b.pk).exists())
+#         # Sanity check
+#         self.assertTrue(bs_1.canonical_building.labels.filter(pk=label_a.pk).exists())
+#         self.assertTrue(bs_1.canonical_building.labels.filter(pk=label_b.pk).exists())
+#         self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_a.pk).exists())
+#         self.assertTrue(bs_2.canonical_building.labels.filter(pk=label_b.pk).exists())
 
-        qs = CanonicalBuilding.objects.all()
-        self.assertEqual(qs.count(), 2)
+#         qs = CanonicalBuilding.objects.all()
+#         self.assertEqual(qs.count(), 2)
 
-        data = {
-            'add_label_ids': [],
-            'remove_label_ids': [label_a.pk, label_b.pk],
-            'selected_buildings': [],
-            'select_all_checkbox': True,
-        }
+#         data = {
+#             'add_label_ids': [],
+#             'remove_label_ids': [label_a.pk, label_b.pk],
+#             'selected_buildings': [],
+#             'select_all_checkbox': True,
+#         }
 
-        serializer = UpdateBuildingLabelsSerializer(
-            data=data,
-            super_organization=organization,
-            queryset=qs,
-        )
-        self.assertTrue(serializer.is_valid(), serializer.errors)
+#         serializer = UpdateBuildingLabelsSerializer(
+#             data=data,
+#             super_organization=organization,
+#             queryset=qs,
+#         )
+#         self.assertTrue(serializer.is_valid(), serializer.errors)
 
-        serializer.save()
-        self.assertEqual(qs.count(), 2)
+#         serializer.save()
+#         self.assertEqual(qs.count(), 2)
 
-        self.assertFalse(bs_1.canonical_building.labels.filter(pk=label_a.pk).exists())
-        self.assertFalse(bs_1.canonical_building.labels.filter(pk=label_b.pk).exists())
-        self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_a.pk).exists())
-        self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_b.pk).exists())
+#         self.assertFalse(bs_1.canonical_building.labels.filter(pk=label_a.pk).exists())
+#         self.assertFalse(bs_1.canonical_building.labels.filter(pk=label_b.pk).exists())
+#         self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_a.pk).exists())
+#         self.assertFalse(bs_2.canonical_building.labels.filter(pk=label_b.pk).exists())
