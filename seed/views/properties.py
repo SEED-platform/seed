896bb844770428d0b6abc9d6d2da502e72d33411
Edwin Lee <leeed2001@gmail.com>
2016-11-23 10:09:24 -0700

    Dummy commit to start PR

--- seed/views/properties.py
+++ seed/views/properties.py
@@ -4,6 +4,7 @@
 :copyright (c) 2014 - 2016, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.  # NOQA
 :author
 """
+
 import datetime
 import itertools
 import json
