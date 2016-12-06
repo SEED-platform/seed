854296e9eeb43a0819e2c47bcadb700c8edefeeb
Nicholas Long <nicholas.long@nrel.gov>
2016-12-06 13:43:08 -0700

    fix migration for cycles and import file

--- seed/data_importer/migrations/0006_auto_20161007_0317.py
+++ seed/data_importer/migrations/0006_auto_20161007_0317.py
@@ -3,33 +3,34 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import django.db.models.deletion
 from datetime import date, datetime, timedelta
-from seed.models import Cycle
 
 
 class Migration(migrations.Migration):
+    def forwards_func(apps, schema_editor):
+        cycle = None
+        if apps.get_model("seed", "Cycle").objects.count() == 0:
+            year = date.today().year - 1
+            cycle_name = 'Migration Created Cycle'
+            if apps.get_model("seed", "Cycle").objects.filter(name=cycle_name).exists():
+                cycle = apps.get_model("seed", "Cycle").objects.get(name=cycle_name)
+            else:
+                print "Creating new cycle instance"
+                cycle = apps.get_model("seed", "Cycle").objects.create(
+                    name=cycle_name,
+                    start=datetime(year, 1, 1),
+                    end=datetime(year + 1, 1, 1) - timedelta(seconds=1)
+                )
+
+        # Go through the importfiles and add the default cycle
+        for c in apps.get_model("data_importer", "ImportFile").objects.all():
+            c.cycle = cycle
+            c.save()
 
     dependencies = [
         ('data_importer', '0005_importfile_cycle'),
     ]
 
-    if Cycle.objects.count() == 0:
-        year = date.today().year - 1
-        cycle_name = str(year) + ' Calendar Year'
-        if Cycle.objects.filter(name=cycle_name).exists():
-            c = Cycle.objects.get(name=cycle_name)
-        else:
-            c = Cycle.objects.create(name=cycle_name,
-                                     start=datetime(year, 1, 1),
-                                     end=datetime(year + 1, 1, 1) - timedelta(seconds=1)
-                                     )
-
     operations = [
-        migrations.AlterField(
-            model_name='importfile',
-            name='cycle',
-            field=models.ForeignKey(default=Cycle.objects.first().pk, on_delete=django.db.models.deletion.CASCADE, to='seed.Cycle'),
-            preserve_default=False,
-        ),
+        migrations.RunPython(forwards_func),
     ]
