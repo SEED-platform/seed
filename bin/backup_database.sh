6d770270b60c8c4dd6adc5f0a5d3369e8e673f95
Nicholas Long <nicholas.long@nrel.gov>
2017-01-25 09:52:09 -0700

    small changes

--- bin/backup_database.sh
+++ bin/backup_database.sh
@@ -3,13 +3,14 @@
 DB_NAME=$1
 DB_USERNAME=$2
 # Set PGPASSWORD as pg_dump uses this env var.
-PGPASSWORD=$3
+DB_PASSWORD=$3
+
 
 function file_name(){
     echo ${BACKUP_DIR}/${DB_NAME}_$(date '+%Y%m%d_%H%M%S').sql
 }
 
-if [[ (-z $DB_NAME) || (-z $DB_USERNAME) || (-z $PGPASSWORD) ]] ; then
+if [[ (-z $DB_NAME) || (-z $DB_USERNAME) || (-z $DB_PASSWORD) ]] ; then
     echo "Expecting command to be of form ./backup_database.sh <db_name> <db_username> <db_password>"
     exit 1
 fi
@@ -18,5 +19,7 @@ fi
 BACKUP_DIR=~/seed-backups
 mkdir -p $BACKUP_DIR
 
-echo "pg_dump -U $DB_USERNAME -Fc $DB_NAME -p $DB_PASSWORD > $(file_name)"
+export PGPASSWORD=$DB_PASSWORD
+echo "pg_dump -U $DB_USERNAME -Fc $DB_NAME > $(file_name)"
 pg_dump -U $DB_USERNAME -Fc $DB_NAME > $(file_name)
+unset PGPASSWORD
