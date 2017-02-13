b4c0846c970d5b918224afcafa8b616b1bc2de94
Daniel Bressan <dbressan42@gmail.com>
2017-02-13 11:46:53 -0700

    added saucelabs to travis ci for e2e testing

--- seed/static/seed/tests/protractor-tests/protractorConfig.js
+++ seed/static/seed/tests/protractor-tests/protractorConfig.js
@@ -27,3 +27,12 @@ exports.config = {
 		}
 	}
 }
+if (process.env.TRAVIS) {
+	exports.config.sauceUser = process.env.SAUCE_USERNAME;
+	exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+	exports.config.capabilities = {
+		'browserName': 'chrome',
+		'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+		'build': process.env.TRAVIS_BUILD_NUMBER
+	};
+}
