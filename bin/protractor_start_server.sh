1b2c7c0d9d46c5336317427345170a66c2b3d9ae
Daniel Bressan <dbressan42@gmail.com>
2017-02-13 16:44:36 -0700

    add 'y' reply and pipe all data to logs

--- bin/protractor_start_server.sh
+++ bin/protractor_start_server.sh
@@ -4,13 +4,13 @@
 echo "updating webdriver"
 ./node_modules/protractor/bin/webdriver-manager update
 echo "migrating"
-./manage.py migrate > toxMigrate.log
+./manage.py migrate > toxMigrate.log 2>&1
 echo "creating default user"
 ./manage.py create_default_user --username=demo@example.com --password=demo123
 echo "making SU"
-./manage.py make_superuser --user demo@example.com > makeSU.log
+echo "y" | ./manage.py make_superuser --user demo@example.com > makeSU.log 2>&1
 echo "starting celery"
-celery -A seed worker -l INFO -c 2 -B --events --maxtasksperchild 1000 & > main.log
+celery -A seed worker -l INFO -c 2 -B --events --maxtasksperchild 1000 & > main.log 2>&1
 echo "starting server"
-./manage.py runserver --settings=config.settings.travis & >> main.log
+./manage.py runserver --settings=config.settings.travis & >> main.log 2>&1
 
