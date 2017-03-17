319cbbf175b18a3221b358b0d4d11f8cdd3d5532
fablet <fable@raintechpdx.com>
2017-03-17 12:45:20 -0700

    updated migration after develop was again updated before merge

--- seed/migrations/0053_green_assessments.py
+++ seed/migrations/0053_green_assessments.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('orgs', '0003_auto_20160412_1123'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('seed', '0048_auto_20170219_1214'),
+        ('seed', '0052_auto_20170307_0955'),
     ]
 
     operations = [
