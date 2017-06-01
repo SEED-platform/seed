aba60a1a3c5e3a3a47f78a516a7a7791ce2c326f
Nicholas Long <nicholas.long@nrel.gov>
2017-05-31 19:54:02 -0600

    flake8. move six and coveralls to requirements/test.txt

--- seed/tests/data_quality/test_functional.py
+++ seed/tests/data_quality/test_functional.py
@@ -41,6 +41,7 @@ _log = logging.getLogger(__name__)
 
 
 class DataQualityTestCoveredBuilding(TestCase):
+
     def setUp(self):
         self.user_details = {
             'username': 'testuser@example.com',
@@ -163,6 +164,7 @@ class DataQualityTestCoveredBuilding(TestCase):
 
 
 class DataQualityTestPM(TestCase):
+
     def setUp(self):
         self.user_details = {
             'username': 'testuser@example.com',
@@ -308,6 +310,7 @@ class DataQualityTestPM(TestCase):
 
 
 class DataQualitySample(TestCase):
+
     def setUp(self):
         self.user_details = {
             'username': 'testuser@example.com',
@@ -710,8 +713,7 @@ class DataQualitySample(TestCase):
 
         # make sure that the label has been applied
         props = PropertyView.objects.filter(property__labels=sl_year).select_related('state')
-        addresses = [p.state.address_line_1 for p in props]
-        addresses.sort()
+        addresses = sorted([p.state.address_line_1 for p in props])
         expected = [u'84807 Buell Trail', u'1 International Road']
         expected.sort()
         self.assertListEqual(expected, addresses)
@@ -822,8 +824,7 @@ class DataQualitySample(TestCase):
 
         # Check multiple strings
         props = PropertyView.objects.filter(property__labels=sl_ok_1).select_related('state')
-        addresses = [p.state.address_line_1 for p in props]
-        addresses.sort()
+        addresses = sorted([p.state.address_line_1 for p in props])
         expected = [
             u'1 International Road',
             u'17246 Esch Drive',
