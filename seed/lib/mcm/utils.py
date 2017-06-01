603b618d269905d0cf413a584fb6059cb0376be3
Nicholas Long <nicholas.long@nrel.gov>
2017-06-01 17:27:54 -0600

    remove unused code

--- seed/lib/mcm/utils.py
+++ seed/lib/mcm/utils.py
@@ -4,10 +4,7 @@
 :copyright (c) 2014 - 2016, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.  # NOQA
 :author
 """
-import json
 from itertools import islice, chain
-
-from dateutil import parser
 
 
 def batch(iterable, size):
@@ -22,14 +19,3 @@ def batch(iterable, size):
     while True:
         batchiter = islice(sourceiter, size)
         yield list(chain([batchiter.next()], batchiter))
-
-
-def date_str_to_date(date_str):
-    if date_str:
-        return parser.parse(date_str)
-
-
-def load_ontology(filename):
-    """Load json structure from a file."""
-    with open(filename, 'rb') as f:
-        return json.loads(f.read())
