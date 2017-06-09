c17cb111a04679bc9d948e7f57c1267c2aadedcc
Nicholas Long <nicholas.long@nrel.gov>
2017-06-09 10:34:53 -0600

    import measures from buildingsync enum json file

--- seed/models/measures.py
+++ seed/models/measures.py
@@ -4,6 +4,10 @@
 :author
 """
 
+import json
+import re
+import string
+
 from django.db import models
 
 BUILDINGSYNC_MEASURES = [
@@ -16,18 +20,63 @@ BUILDINGSYNC_MEASURES = [
 ]
 
 
+def _snake_case(display_name):
+    """
+    Convert the BuildingSync measure display names into reasonable snake_case for storing into
+    database.
+
+    :param display_name: BuidingSync measure displayname
+    :return: string
+    """
+    str_re = re.compile('[{0}]'.format(re.escape(string.punctuation)))
+    str = str_re.sub(' ', display_name)
+    str = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)
+    str = re.sub('([a-z0-9])([A-Z])', r'\1_\2', str).lower()
+    return re.sub(' +', '_', str)
+
+
 class Measure(models.Model):
     name = models.CharField(max_length=255)
     display_name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
-    display_category = models.CharField(max_length=255)
+    category_display_name = models.CharField(max_length=255)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-        return u'Measure - %s' % self.name
+        return u'Measure - %s.%s' % (self.category, self.name)
 
     class Meta:
         ordering = ['-created']
         get_latest_by = 'created'
+
+    @classmethod
+    def populate_measures(cls):
+        """
+        Populate the list of measures from the BuildingSync
+        :return:
+        """
+        filename = "seed/lib/buildingsync/enumerations.json"
+        with open(filename) as f:
+            data = json.load(f)
+
+            for datum in data:
+                # "name": "MeasureName",
+                # "sub_name": "AdvancedMeteringSystems",
+                # "documentation": "Advanced Metering Systems",
+                # "enumerations": [
+                #                     "Install advanced metering systems",
+                #                     "Clean and/or repair",
+                #                     "Implement training and/or documentation",
+                #                     "Upgrade operating protocols, calibration, and/or sequencing",
+                #                     "Other"
+                #                 ],
+                if datum["name"] == "MeasureName":
+                    for enum in datum["enumerations"]:
+                        Measure.objects.get_or_create(
+                            category=_snake_case(datum["sub_name"]),
+                            category_display_name=datum["documentation"],
+                            name=_snake_case(enum),
+                            display_name=enum,
+                        )
---
+++ seed/tests/test_measures.py
@@ -0,0 +1,33 @@
+# !/usr/bin/env python
+# encoding: utf-8
+"""
+:copyright (c) 2014 - 2017, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.  # NOQA
+:author
+"""
+
+from django.test import TestCase
+
+from seed.models.measures import Measure
+from seed.models.measures import _snake_case
+
+
+class TestException(TestCase):
+    def setUp(self):
+        pass
+
+    def test_populate_measures(self):
+        Measure.populate_measures()
+        self.assertEqual(Measure.objects.count(), 174)
+
+        # if we run it again, it shouldn't add anything new
+        Measure.populate_measures()
+        self.assertEqual(Measure.objects.count(), 174)
+
+    def test_snake_case(self):
+        self.assertEqual(_snake_case("AbCdEf"), "ab_cd_ef")
+        self.assertEqual(_snake_case("Clean and/or repair"), "clean_and_or_repair")
+        self.assertEqual(
+            _snake_case("Upgrade operating protocols, calibration, and/or sequencing"),
+            "upgrade_operating_protocols_calibration_and_or_sequencing"
+        )
+        self.assertEqual(_snake_case("AdvancedMeteringSystems"), "advanced_metering_systems")
