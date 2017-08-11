c518db57f23a91005057389218e9fc6d91013d7f
Nicholas Long <nicholas.long@nrel.gov>
2017-08-11 15:49:12 -0600

    stub out new properties viewset

---
+++ seed/api/v2_1/views.py
@@ -0,0 +1,63 @@
+# !/usr/bin/env python
+# encoding: utf-8
+"""
+:copyright (c) 2014 - 2017, The Regents of the University of California,
+through Lawrence Berkeley National Laboratory (subject to receipt of any
+required approvals from the U.S. Department of Energy) and contributors.
+All rights reserved.  # NOQA
+:author
+"""
+
+# Imports from Standard Library
+
+# Imports from Django
+
+# Local Imports
+from seed.models import Property as PropertyModel
+from seed.serializers.properties import (
+    PropertySerializer
+)
+from seed.utils.viewsets import (
+    SEEDOrgCreateUpdateModelViewSet
+)
+
+
+class PropertyViewSetV21(SEEDOrgCreateUpdateModelViewSet):
+    """Properties API Endpoint
+
+        Returns::
+            {
+                'status': 'success',
+                'properties': [
+                    {
+                        'id': Property primary key,
+                        'campus': property is a campus,
+                        'parent_property': dict of associated parent property
+                        'labels': list of associated label ids
+                    }
+                ]
+            }
+
+
+    retrieve:
+        Return a Property instance by pk if it is within specified org.
+
+    list:
+        Return all Properties available to user through specified org.
+
+    create:
+        Create a new Property within user`s specified org.
+
+    delete:
+        Remove an existing Property.
+
+    update:
+        Update a Property record.
+
+    partial_update:
+        Update one or more fields on an existing Property.
+    """
+    serializer_class = PropertySerializer
+    model = PropertyModel
+    data_name = "properties"
+    # filter_class = PropertyFilter
