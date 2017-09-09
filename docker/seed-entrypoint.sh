963a0e2f6cd87c4b4ffc59382851a7306805c393
Nicholas Long <nicholas.long@nrel.gov>
2017-09-09 08:46:27 -0600

    create default user print to command line

--- docker/seed-entrypoint.sh
+++ docker/seed-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo 'Configuring SEED directories'
+# Make sure not to echo anything out here becase some of the scripts that test SEED
+# call docker-compose run and expect the response to be in the command line.
 mkdir -p /seed/collected_static && chmod 775 /seed/collected_static
 mkdir -p /seed/media && chmod 777 /seed/media
 
