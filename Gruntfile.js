6219495bce1d5a7f01613e4e523d9c41d0baae47
Ryan McCuaig <ryan@ryanmccuaig.net>
2017-11-15 10:02:48 -0800

    Remove grunt-angular-translate
    This is a low-value dependency now... it was only about 60% effective at
    the time of initial bulk extraction. Now that the majority of the
    interface strings are tagged as `translate`, it's simpler to just add
    any new strings directly into lokalise when we're in marking the
    translatable UI anyway.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,135 +1,125 @@
-module.exports = function (grunt) {
-  grunt.initConfig({
-    // connect: {
-    //     options: {
-    //         port: 8000,
-    //         hostname: 'localhost'
-    //     },
-    // runtime: {
-    //     options: {
-    //         middleware: function (connect) {
-    //             return [
-    //                 lrSnippet,
-    //                 mountFolder(connect, 'protractorInstrumented'),
-    //                 mountFolder(connect, '.......')
-    //             ];
-    //         }
-    //     }
-    // }
 
-    // Before generating any new files, remove any previously-created files.
-    // },
-    clean: {
-      options: {
-        force: true
-      },
-      tests: ['tmp', 'build', 'protractorInstrumented', 'protractorCoverage', 'protractorReports', 'protractorSaved']
-    },
-    connect: {
-      server: {
-        options: {
-          port: 3000,
-          base: 'protractorInstrumented/seed/static/seed/js'
-        }
-      }
-    },
-    instrument: {
-      files: ['seed/static/seed/js/**/*.js', '!seed/static/seed/js/decorators/**/*.js', '!seed/static/seed/js/seed.js'],
-      options: {
-        lazy: true,
-        basePath: 'protractorInstrumented'
-      }
-    },
-    copy: {
-      save: {
-        expand: true,
-        cwd: 'seed/static/seed/js',
-        src: '**',
-        dest: 'protractorSaved/'
-      },
-      instrument: {
-        expand: true,
-        cwd: 'protractorInstrumented/seed/static/seed/js',
-        src: '**',
-        dest: 'seed/static/seed/js/'
-      },
-      copyBack: {
-        expand: true,
-        cwd: 'protractorSaved',
-        src: '**',
-        dest: 'seed/static/seed/js/'
-      }
-    },
-    protractor_coverage: {
-      options: {
-        keepAlive: false, // If false, the grunt process stops when the test fails.
-        noColor: false,
-        coverageDir: 'protractorCoverage',
-        args: {
-          baseUrl: 'http://localhost:8000'
-        }
-      },
-      local: {
-        options: {
-          configFile: 'seed/static/seed/tests/protractor-tests/protractorConfigCoverage.js'
-        }
-      }
-    },
-    makeReport: {
-      src: 'protractorCoverage/**/*.json',
-      options: {
-        type: 'lcov',
-        dir: 'protractorReports',
-        print: ''
-      }
-    },
-    coveralls: {
-      main: {
-        src: 'protractorReports/**/*.info',
-        options: {
-          force: true
-        }
-      }
-    },
-    i18nextract: {
-      default_options: {
-        src: [
-          'seed/static/seed/**/*.js',
-          'seed/static/seed/**/*.html'
-        ],
-        interpolation: { startDelimiter: '{$', endDelimiter: '$}' },
-        lang: ['en_US', 'fr_CA'],
-        dest: 'seed/static/seed/locales'
-      }
-    }
-  });
+var tmp = require('tmp');
+module.exports = function(grunt) {
+    grunt.initConfig({
+        // connect: {
+        //     options: {
+        //         port: 8000,
+        //         hostname: 'localhost'
+        //     },
+            // runtime: {
+            //     options: {
+            //         middleware: function (connect) {
+            //             return [
+            //                 lrSnippet,
+            //                 mountFolder(connect, 'protractorInstrumented'),
+            //                 mountFolder(connect, '.......')
+            //             ];
+            //         }
+            //     }
+            // }
 
-  // Actually load this plugin's task(s).
-  // grunt.loadTasks('tasks');
+        // Before generating any new files, remove any previously-created files.
+        // },
+        clean: {
+          options: {
+            force:true
+          },
+          tests: ['tmp', 'build', 'protractorInstrumented', 'protractorCoverage', 'protractorReports', 'protractorSaved'],
+        },
+        connect: {
+          server: {
+            options: {
+              port: 3000,
+              base: 'protractorInstrumented/seed/static/seed/js'
+            }
+          },
+        },
+        instrument: {
+            files: ['seed/static/seed/js/**/*.js','!seed/static/seed/js/decorators/**/*.js', '!seed/static/seed/js/seed.js'],
+            options: {
+            lazy: true,
+                basePath: "protractorInstrumented"
+            }
+        },
+        copy: {
+          'save': {
+            expand: true,
+            cwd: 'seed/static/seed/js',
+            src: '**',
+            dest: 'protractorSaved/'
+          },
+          'instrument': {
+            expand: true,
+            cwd: 'protractorInstrumented/seed/static/seed/js',
+            src: '**',
+            dest: 'seed/static/seed/js/'
+          },
+          'copyBack': {
+            expand: true,
+            cwd: 'protractorSaved',
+            src: '**',
+            dest: 'seed/static/seed/js/'
+          },
+        },
+        protractor_coverage: {
+            options: {
+                keepAlive: false, // If false, the grunt process stops when the test fails.
+                noColor: false,
+                coverageDir: 'protractorCoverage',
+                args: {
+                    baseUrl: 'http://localhost:8000'
+                }
+            },
+            local: {
+                options: {
+                    configFile: 'seed/static/seed/tests/protractor-tests/protractorConfigCoverage.js'
+                }
+            }
+        },
+        makeReport: {
+            src: 'protractorCoverage/**/*.json',
+            options: {
+                type: 'lcov',
+                dir: 'protractorReports',
+                print: ''
+            }
+        },
+        coveralls: {
+            main:{
+                src: 'protractorReports/**/*.info',
+                options: {
+                    force: true
+                },
+            },
+        },
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-protractor-coverage');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-istanbul');
-  grunt.loadNpmTasks('grunt-coveralls');
-  grunt.loadNpmTasks('grunt-angular-translate');
+    // Actually load this plugin's task(s).
+    // grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-protractor-coverage');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-istanbul');
+    grunt.loadNpmTasks('grunt-coveralls');
 
-  //same as npm test more or less
-  // grunt.registerTask('coverage', ['protractor_coverage:local']);
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
 
-  //don't use instrumented code
-  // grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'protractor_coverage:local', 'copy:copyBack']);
+    //same as npm test more or less
+    // grunt.registerTask('coverage', ['protractor_coverage:local']);
+    
+    //don't use instrumented code
+    // grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'protractor_coverage:local', 'copy:copyBack']);    
 
-  //without coveralls
-  grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'copy:instrument', 'protractor_coverage:local', 'copy:copyBack', 'makeReport']);
-  grunt.registerTask('report', ['makeReport']);
+    //without coveralls
+    grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'copy:instrument', 'protractor_coverage:local', 'copy:copyBack', 'makeReport']);
+    grunt.registerTask('report', ['makeReport']);
 
-  //with coveralls
-  // grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'copy:instrument', 'protractor_coverage:local', 'copy:copyBack', 'makeReport', 'coveralls']);
+    //with coveralls
+    // grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'copy:instrument', 'protractor_coverage:local', 'copy:copyBack', 'makeReport', 'coveralls']);
 
-  grunt.registerTask('test', ['coverage']);
+    grunt.registerTask('test', ['coverage']);
 };
