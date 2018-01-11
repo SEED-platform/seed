48e53cd1bae34efa91aead397e9e43d601aa103e
Ryan McCuaig <ryan@ryanmccuaig.net>
2018-01-11 11:08:48 -0800

    Migrates in new pint columns for old float columns
    To reduce risk on making things units-aware, we created new columns for
    the EUIs and areas that had the suffix `_pint`, and left the old ones in
    place. This commit, via renaming, archives the old EUI/area columns with
    the suffix `_orig`, and takes over the column names for the new
    unit-aware Quantity columns.
    
    This commit also provides a migration that simply copies the original
    EUI/area data from the `_orig` Float columns to the corresponding new
    Quantity column. This works because the implicit units of the
    unit-unaware Float EUIs was `kBtu/ft**2/year` and these are the explicit
    base units of the EUI Quantity columns. (Similarly for areas with
    `ft**2`).  So the database representations are equivalent and any new
    capabilities are being provided at the application/Django layer.
    
    The columns technically could have been converted in place, but this
    swap approach provides us a clean way to back out in case of problems:
    remove the Quantity-based code and columns, and provide a migration that
    removes the `_orig` suffix from the archived columns.

--- seed/models/properties.py
+++ seed/models/properties.py
@@ -141,11 +141,11 @@ class PropertyState(models.Model):
     # Tax IDs are often stuck here.
     use_description = models.CharField(max_length=255, null=True, blank=True)
 
-    gross_floor_area = models.FloatField(null=True, blank=True)
+    gross_floor_area_orig = models.FloatField(null=True, blank=True)
     year_built = models.IntegerField(null=True, blank=True)
     recent_sale_date = models.DateTimeField(null=True, blank=True)
-    conditioned_floor_area = models.FloatField(null=True, blank=True)
-    occupied_floor_area = models.FloatField(null=True, blank=True)
+    conditioned_floor_area_orig = models.FloatField(null=True, blank=True)
+    occupied_floor_area_orig = models.FloatField(null=True, blank=True)
 
     # Normalize eventually on owner/address table
     owner = models.CharField(max_length=255, null=True, blank=True)
@@ -163,12 +163,19 @@ class PropertyState(models.Model):
     # reported fields. Ideally would have the ability to provide the same field from
     # multiple data sources. For example, site EUI (portfolio manager), site EUI (calculated),
     # site EUI (modeled 8/4/2017).
-    site_eui = models.FloatField(null=True, blank=True)
-    site_eui_weather_normalized = models.FloatField(null=True, blank=True)
-    site_eui_modeled = models.FloatField(null=True, blank=True)
-    source_eui = models.FloatField(null=True, blank=True)
-    source_eui_weather_normalized = models.FloatField(null=True, blank=True)
-    source_eui_modeled = models.FloatField(null=True, blank=True)
+    #
+    # note: `*_orig` are all the unit-unaware original fields in the property
+    # state, which have been superceded by unit-aware Quantity fields. The old
+    # ones are left in place via the rename from eg. site_eui -> site_eui_orig
+    # with their original data intact until we're sure things are OK with the
+    # new columns. At that point (probably 2.4 release) these can be safely
+    # deleted and removed with a migration.
+    site_eui_orig = models.FloatField(null=True, blank=True)
+    site_eui_weather_normalized_orig = models.FloatField(null=True, blank=True)
+    site_eui_modeled_orig = models.FloatField(null=True, blank=True)
+    source_eui_orig = models.FloatField(null=True, blank=True)
+    source_eui_weather_normalized_orig = models.FloatField(null=True, blank=True)
+    source_eui_modeled_orig = models.FloatField(null=True, blank=True)
 
     energy_alerts = models.TextField(null=True, blank=True)
     space_alerts = models.TextField(null=True, blank=True)
@@ -181,21 +188,15 @@ class PropertyState(models.Model):
                                          null=True)
     analysis_state_message = models.TextField(null=True)
 
-    # extra columns for pint interpretation base units in database will
-    # continue to be imperial these will become the canonical columns in
-    # future, with the old ones above to be culled once OGBS merges the metric
-    # units work (scheduled for late 2017)
-
-    # TODO: eventually need to add these fields to the coparent SQL query below.
-    gross_floor_area_pint = QuantityField('ft**2', null=True, blank=True)
-    conditioned_floor_area_pint = QuantityField('ft**2', null=True, blank=True)
-    occupied_floor_area_pint = QuantityField('ft**2', null=True, blank=True)
-    site_eui_pint = QuantityField('kBtu/ft**2/year', null=True, blank=True)
-    source_eui_weather_normalized_pint = QuantityField('kBtu/ft**2/year', null=True, blank=True)
-    site_eui_modeled_pint = QuantityField('kBtu/ft**2/year', null=True, blank=True)
-    site_eui_weather_normalized_pint = QuantityField('kBtu/ft**2/year', null=True, blank=True)
-    source_eui_pint = QuantityField('kBtu/ft**2/year', null=True, blank=True)
-    source_eui_modeled_pint = QuantityField('kBtu/ft**2/year', null=True, blank=True)
+    gross_floor_area = QuantityField('ft**2', null=True, blank=True)
+    conditioned_floor_area = QuantityField('ft**2', null=True, blank=True)
+    occupied_floor_area = QuantityField('ft**2', null=True, blank=True)
+    site_eui = QuantityField('kBtu/ft**2/year', null=True, blank=True)
+    source_eui_weather_normalized = QuantityField('kBtu/ft**2/year', null=True, blank=True)
+    site_eui_modeled = QuantityField('kBtu/ft**2/year', null=True, blank=True)
+    site_eui_weather_normalized = QuantityField('kBtu/ft**2/year', null=True, blank=True)
+    source_eui = QuantityField('kBtu/ft**2/year', null=True, blank=True)
+    source_eui_modeled = QuantityField('kBtu/ft**2/year', null=True, blank=True)
 
     extra_data = JSONField(default=dict, blank=True)
     measures = models.ManyToManyField('Measure', through='PropertyMeasure')
