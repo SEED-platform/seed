cfaf55f88a542ac9793670f18a6590bebc2e53c9
Nicholas Long <nicholas.long@nrel.gov>
2018-03-13 13:17:38 -0600

    pep8

--- docs/scripts/export_issues_to_csv.py
+++ docs/scripts/export_issues_to_csv.py
@@ -1,9 +1,9 @@
 # pip install --pre github3.py
 
-import github3
-import csv
-import json
 import argparse
+import csv
+
+import github3
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-k', '--github-key', required=True, help='Github API Key')
@@ -16,34 +16,36 @@ gh = github3.login(token=args.github_key)
 repo = gh.repository('SEED-platform', 'seed')
 print(repo)
 
-
 # Initialize some data
-header = [ "Order", "Title", "Category", "Priority", "Impact", "Estimate", "Weighted Priority", "Note", "Github ID", "Type/Labels", "Github URL", "Pivotal URL"  ]
+header = ["Order", "Title", "Category", "Priority", "Impact", "Estimate", "Weighted Priority", "Note", "Github ID",
+          "Type/Labels", "Github URL", "Pivotal URL"]
 lines = []
 ids_added = []
 
+
 def add_issue_to_csv(issue):
+    # import json
     # print(json.dumps(issue.as_dict(), indent=2))
     print("Adding Issue %s : %s" % (issue.number, issue.title))
     labels = [l.name for l in issue.labels()]
     ids_added.append(issue.number)
     line = []
-    line.append(len(lines)) # Order
-    line.append(issue.title) # Title
-    line.append("") # category
+    line.append(len(lines))  # Order
+    line.append(issue.title)  # Title
+    line.append("")  # category
     # nrel priority
     if 'P-1' in labels:
-        line.append(1) 
+        line.append(1)
     elif 'P-2' in labels:
         line.append(2)
     elif 'P-3' in labels:
         line.append(3)
     else:
         line.append("")
-    
+
     # nrel impact
     if 'Impact-1' in labels:
-        line.append(1) 
+        line.append(1)
     elif 'Impact-2' in labels:
         line.append(2)
     elif 'Impact-3' in labels:
@@ -53,7 +55,7 @@ def add_issue_to_csv(issue):
 
     # estimate
     if '1 Point' in labels:
-        line.append(1) 
+        line.append(1)
     elif '2 Points' in labels:
         line.append(2)
     elif '3 Points' in labels:
@@ -66,14 +68,15 @@ def add_issue_to_csv(issue):
         line.append("")
     # line.append("") # lbnl priority
     # line.append("") # lbnl impact
-    line.append("") # weighted impact
-    line.append("") # Notes
-    line.append(issue.number) # Github ID
-    line.append(",".join(labels)) # Type / Labels
-    line.append(issue.html_url) # Github URL
-    line.append("") # Pivotal URL
+    line.append("")  # weighted impact
+    line.append("")  # Notes
+    line.append(issue.number)  # Github ID
+    line.append(",".join(labels))  # Type / Labels
+    line.append(issue.html_url)  # Github URL
+    line.append("")  # Pivotal URL
 
     lines.append(line)
+
 
 if args.csv:
     print("There are %s open issues" % repo.open_issues_count)
@@ -82,11 +85,11 @@ if args.csv:
     for issue in repo.issues(state='open', labels='P-1'):
         add_issue_to_csv(issue)
 
-    print("Finding P-2 Issues") 
+    print("Finding P-2 Issues")
     for issue in repo.issues(state='open', labels='P-2'):
         add_issue_to_csv(issue)
 
-    print("Finding P-3 Issues") 
+    print("Finding P-3 Issues")
     for issue in repo.issues(state='open', labels='P-3'):
         add_issue_to_csv(issue)
 
@@ -99,6 +102,6 @@ if args.csv:
     with open('seed_issues.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerow(header)
-            
+
         for line in lines:
             writer.writerow(line)
