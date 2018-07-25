4af08ef4ab5d6191a7bb85df1f9ef346e61a7f2d
Edwin Lee <leeed2001@gmail.com>
2018-07-24 21:10:42 -0500

    Add functional tests for PM integration

--- seed/tests/test_portfoliomanager.py
+++ seed/tests/test_portfoliomanager.py
@@ -103,23 +103,85 @@ class PortfolioManagerTemplateListViewTestsFailure(TestCase):
         self.assertIn('Check credentials.', data['message'])
 
 
-skip_pm_tests = skipIf(
-    os.environ.get('SEED_ESPM_UN', False) or os.environ.get('SEED_ESPM_PW', False),
-    'Cannot run expect-pass ESPM unit tests without SEED_ESPM_UN and SEED_ESPM_PW in environment'
+PM_UN = 'SEED_PM_UN'
+PM_PW = 'SEED_PM_PW'
+pm_skip_test_check = skipIf(
+    not os.environ.get(PM_UN, False) and not os.environ.get(PM_PW, False),
+    'Cannot run "expect-pass" PM unit tests without %s and %s in environment' % (PM_UN, PM_PW)
 )
 
 
 class PortfolioManagerTemplateListViewTestsSuccess(TestCase):
 
     def setUp(self):
-        pass
+        user_details = {
+            'username': 'test_user@demo.com',
+            'password': 'test_pass',
+            'email': 'test_user@demo.com',
+            'first_name': 'Johnny',
+            'last_name': 'Energy',
+        }
+        self.user = User.objects.create_user(**user_details)
+        self.org, self.org_user, _ = create_organization(self.user)
+        self.client.login(**user_details)
 
-    @skip_pm_tests
-    def test_a(self):
-        print("Running test_a")
+    @pm_skip_test_check
+    def test_template_views(self):
+        # if we get into this test, the PM_UN and PM_PW variables should be available
+        # we'll still check of course
+        pm_un = os.environ.get(PM_UN, False)
+        pm_pw = os.environ.get(PM_PW, False)
+        if not pm_un or not pm_pw:
+            self.fail('Somehow PM test was initiated without %s or %s in the environment' % (PM_UN, PM_PW))
+
+        # so now we'll make the call out to PM
+        resp = self.client.post(
+            reverse_lazy('api:v2.1:portfolio_manager-template-list'),
+            json.dumps({'username': pm_un, 'password': pm_pw}),
+            content_type='application/json',
+        )
+        # this kinda gets a little fragile here.
+        # We can't really guarantee that the test account over on ESPM will stay exactly like it is the whole time
+        # But we can try to at least test the "form" of the response to make sure it is what we expect
+        # And if we ever break the stuff over on ESPM it should be clear what went wrong
+
+        # at a minimum, we should have a successful login and response
+        self.assertEqual(200, resp.status_code)
+
+        # the body should come as json; if not, this will fail to parse I presume and fail this test
+        body = resp.json()
+
+        # body should represent the successful process
+        self.assertTrue(body['status'])
+
+        # templates should be present, and be a list
+        self.assertIn('templates', body)
+        self.assertIsInstance(body['templates'], list)
+
+        # every object in that list should be a dictionary that contains a bunch of expected keys
+        for row in body['templates']:
+            for expected_key in ['name', 'display_name', 'z_seed_child_row']:
+                self.assertIn(expected_key, row)
+
+            # if it is a child (data request) row, the display name should be formatted
+            # it is possible that a parent row could have the same "indentation", and that's fine, we don't assert there
+            if row['z_seed_child_row']:
+                self.assertEquals(u'  -  ', row['display_name'][0:5])
 
 
-class PortfolioManagerReportGenerationViewTests(TestCase):
+class PortfolioManagerReportGenerationViewTestsFailure(TestCase):
+
+    def setUp(self):
+        user_details = {
+            'username': 'test_user@demo.com',
+            'password': 'test_pass',
+            'email': 'test_user@demo.com',
+            'first_name': 'Johnny',
+            'last_name': 'Energy',
+        }
+        self.user = User.objects.create_user(**user_details)
+        self.org, self.org_user, _ = create_organization(self.user)
+        self.client.login(**user_details)
 
     def test_report_interface_no_username(self):
         resp = self.client.post(
@@ -192,3 +254,97 @@ class PortfolioManagerReportGenerationViewTests(TestCase):
         self.assertIn('message', data)
         self.assertEqual('error', data['status'])
         self.assertIn('Check credentials.', data['message'])
+
+
+class PortfolioManagerReportGenerationViewTestsSuccess(TestCase):
+
+    def setUp(self):
+        user_details = {
+            'username': 'test_user@demo.com',
+            'password': 'test_pass',
+            'email': 'test_user@demo.com',
+            'first_name': 'Johnny',
+            'last_name': 'Energy',
+        }
+        self.user = User.objects.create_user(**user_details)
+        self.org, self.org_user, _ = create_organization(self.user)
+        self.client.login(**user_details)
+
+        # if we get into this test, the PM_UN and PM_PW variables should be available
+        # we'll still check of course
+        self.pm_un = os.environ.get(PM_UN, False)
+        self.pm_pw = os.environ.get(PM_PW, False)
+        if not self.pm_un or not self.pm_pw:
+            self.fail('Somehow PM test was initiated without %s or %s in the environment' % (PM_UN, PM_PW))
+
+    @pm_skip_test_check
+    def test_report_generation_parent_template(self):
+
+        parent_template = {
+            u'display_name': u'SEED City Test Report',
+            u'name': u'SEED City Test Report',
+            u'id': 1103344,
+            u'z_seed_child_row': False,
+            u'type': 0,
+            u'children': [],
+            u'pending': 0
+        }
+
+        # so now we'll call out to PM to get a parent template report
+        resp = self.client.post(
+            reverse_lazy('api:v2.1:portfolio_manager-report'),
+            json.dumps({'username': self.pm_un, 'password': self.pm_pw, 'template': parent_template}),
+            content_type='application/json',
+        )
+
+        # as usual, the first thing to test is really the status code of the response
+        self.assertEquals(200, resp.status_code)
+
+        # and we expect a json blob to come back
+        body = resp.json()
+
+        # the status flag should be successful
+        self.assertEquals('success', body['status'])
+
+        # we expect a list of properties to come back
+        self.assertIn('properties', body)
+        self.assertIsInstance(body['properties'], list)
+
+        # then for each property, we expect some keys to come back, but if it has the property id, that should suffice
+        for prop in body['properties']:
+            self.assertIn('property_id', prop)
+
+    @pm_skip_test_check
+    def test_report_generation_empty_child_template(self):
+
+        child_template = {
+            u'display_name': u'  -  Data Request:SEED City Test Report April 24 2018',
+            u'name': u'Data Request:SEED City Test Report April 24 2018',
+            u'id': 2097417,
+            u'subtype': 2,
+            u'z_seed_child_row': True,
+            u'hasChildrenRows': False,
+            u'type': 1,
+            u'children': [],
+            u'pending': 0
+        }
+
+        # so now we'll call out to PM to get a child template report
+        resp = self.client.post(
+            reverse_lazy('api:v2.1:portfolio_manager-report'),
+            json.dumps({'username': self.pm_un, 'password': self.pm_pw, 'template': child_template}),
+            content_type='application/json',
+        )
+
+        # this child template is empty over on PM, so it comes back as a 400
+        self.assertEquals(400, resp.status_code)
+
+        # still, we expect a json blob to come back
+        body = resp.json()
+
+        # the status flag should be error
+        self.assertEquals('error', body['status'])
+
+        # in this case, we expect a meaningful error message
+        self.assertIn('message', body)
+        self.assertIn('empty', body['message'])
