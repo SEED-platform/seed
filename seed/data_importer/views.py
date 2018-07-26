ef016bea714b41f7646a1d6216b8c498aa4384d4
Nicholas Long <nicholas.long@nrel.gov>
2018-07-25 21:21:10 -0600

    use get_parent org for list settings and column mappings

--- seed/data_importer/views.py
+++ seed/data_importer/views.py
@@ -1801,6 +1801,7 @@ class ImportFileViewSet(viewsets.ViewSet):
         membership = OrganizationUser.objects.select_related('organization') \
             .get(organization_id=organization_id, user=request.user)
         organization = membership.organization
+        parent_org = organization.get_parent()
 
         import_file = ImportFile.objects.get(
             pk=pk,
@@ -1808,8 +1809,8 @@ class ImportFileViewSet(viewsets.ViewSet):
         )
 
         # Get a list of the database fields in a list, these are the db columns and the extra_data columns
-        property_columns = Column.retrieve_mapping_columns(organization_id, 'property')
-        taxlot_columns = Column.retrieve_mapping_columns(organization_id, 'taxlot')
+        property_columns = Column.retrieve_mapping_columns(parent_org.pk, 'property')
+        taxlot_columns = Column.retrieve_mapping_columns(parent_org.pk, 'taxlot')
 
         # If this is a portfolio manager file, then load in the PM mappings and if the column_mappings
         # are not in the original mappings, default to PM
@@ -1820,7 +1821,7 @@ class ImportFileViewSet(viewsets.ViewSet):
                 import_file.first_row_columns,
                 Column.retrieve_all_by_tuple(organization_id),
                 previous_mapping=get_column_mapping,
-                map_args=[organization],
+                map_args=[parent_org],
                 default_mappings=pm_mappings,
                 thresh=80
             )
@@ -1828,9 +1829,9 @@ class ImportFileViewSet(viewsets.ViewSet):
             # All other input types
             suggested_mappings = mapper.build_column_mapping(
                 import_file.first_row_columns,
-                Column.retrieve_all_by_tuple(organization_id),
+                Column.retrieve_all_by_tuple(parent_org.pk),
                 previous_mapping=get_column_mapping,
-                map_args=[organization],
+                map_args=[parent_org],
                 thresh=80  # percentage match that we require. 80% is random value for now.
             )
             # replace None with empty string for column names and PropertyState for tables
--- seed/utils/organizations.py
+++ seed/utils/organizations.py
@@ -5,6 +5,7 @@
 :author
 """
 
+from seed.lib.superperms.orgs.exceptions import TooManyNestedOrgs
 from seed.lib.superperms.orgs.models import (
     Organization,
     OrganizationUser,
@@ -15,12 +16,13 @@ from seed.models.data_quality import DataQualityCheck
 
 
 def create_organization(user=None, org_name='', *args, **kwargs):
-    """Helper script to create a user/org relationship from scratch.
+    """
+    Helper script to create a user/org relationship from scratch. This is heavily used and
+    creates the default labels, columns, and data quality rules when a new organization is created
 
     :param user: user inst.
     :param org_name: str, name of Organization we'd like to create.
     :param (optional) kwargs: 'role', int; 'status', str.
-
     """
     from seed.models import StatusLabel as Label
     organization_user = None
@@ -55,3 +57,22 @@ def create_organization(user=None, org_name='', *args, **kwargs):
     DataQualityCheck.retrieve(organization.id)
 
     return organization, organization_user, user_added
+
+
+def create_suborganization(user, current_org, suborg_name=''):
+    # Create the suborg manually to prevent the generation of the default columns, labels, and data
+    # quality checks
+    sub_org = Organization.objects.create(
+        name=suborg_name
+    )
+    OrganizationUser.objects.get_or_create(user=user, organization=sub_org)
+
+    sub_org.parent_org = current_org
+
+    try:
+        sub_org.save()
+    except TooManyNestedOrgs:
+        sub_org.delete()
+        return False, 'Tried to create child of a child organization.'
+
+    return True, sub_org
