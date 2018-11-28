93547150f2a29b91c172679190d355e2ebcd8ec5
Nicholas Long <nicholas.long@nrel.gov>
2018-11-27 17:48:35 -0700

    Do not use docker-compose file to launch container, simply look for container by id.

--- docker/backup_database.sh
+++ docker/backup_database.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # This backup script creates nightly database and media file backups of SEED when SEED is running
-# in a docker container.
+# in a docker container. The name of the container running the database is hardcoded to look for
+# *db-postgres*. This may cause an issue if several docker applications are running on the same
+# system. Also, the location of the backups is hardcoded to ~/seed-backups.
 
 # To create nightly backups, add the following to your crontab
-# 0 0 * * * /home/ubuntu/prj/seed/docker/backup_database.sh <docker-compose-filename> <db_name> <db_username>
+# 0 0 * * * /home/ubuntu/prj/seed/docker/backup_database.sh <db_name> <db_username>
 
-DC_FILENAME=$1
-DB_NAME=$2
-DB_USERNAME=$3
+DB_NAME=$1
+DB_USERNAME=$2
 
 function file_name(){
     echo ${BACKUP_DIR}/${DB_NAME}_$(date '+%Y%m%d_%H%M%S').dump
@@ -18,8 +19,8 @@ function media_file_name(){
     echo /backup/dir/${DB_NAME}_media_$(date '+%Y%m%d_%H%M%S').tgz
 }
 
-if [[ (-z ${DB_NAME}) || (-z ${DB_USERNAME}) || (-z ${DC_FILENAME}) ]] ; then
-    echo "Expecting command to be of form ./backup_database.sh <docker-compose-filename> <db_name> <db_username>"
+if [[ (-z ${DB_NAME}) || (-z ${DB_USERNAME}) ]] ; then
+    echo "Expecting command to be of form ./backup_database.sh <db_name> <db_username>"
     exit 1
 fi
 
@@ -29,8 +30,9 @@ mkdir -p ${BACKUP_DIR}
 
 # db_password is set from the environment variables in docker-compose. The docker stack must
 # be running for this command to work.
-echo "docker-compose -f ${DC_FILENAME} exec $(docker ps -f "name=db-postgres" --format "{{.ID}}") pg_dump -U ${DB_USERNAME} -Fc ${DB_NAME} > $(file_name)"
-docker-compose -f ${DC_FILENAME} exec $(docker ps -f "name=db-postgres" --format "{{.ID}}") pg_dump -U ${DB_USERNAME} -Fc ${DB_NAME} > $(file_name)
+echo "docker exec $(docker ps -f "name=db-postgres" --format "{{.ID}}") pg_dump -U ${DB_USERNAME} -Fc ${DB_NAME} > $(file_name)"
+docker exec $(docker ps -f "name=db-postgres" --format "{{.ID}}") pg_dump -U ${DB_USERNAME} -Fc ${DB_NAME} > $(file_name)
+
 
 # Backup the media directory (uploads, especially buildingsync). In docker-land this is
 # just a container volume, so create a new container with the volume attached and tar it up.
