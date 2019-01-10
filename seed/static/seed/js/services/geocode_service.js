e013bd35c84e8df6bbcdcedbbfc433aa0c8fe8e0
Adrian Lara <adrian@devetry.com>
2019-01-10 16:46:30 -0700

    Remove trailing comma in geocode service

--- seed/static/seed/js/services/geocode_service.js
+++ seed/static/seed/js/services/geocode_service.js
@@ -2,20 +2,18 @@
 angular.module('BE.seed.service.geocode', [])
   .factory('geocode_service', [
     '$http',
-    function(
-      $http,
-    ) {
-        var geocode_factory = {};
+    function($http) {
+      var geocode_factory = {};
 
-        geocode_factory.geocode_by_ids = function (property_state_ids, taxlot_state_ids) {
-          return $http.post('/api/v2/geocode/geocode_by_ids/', {
-            'property_ids': property_state_ids,
-            'taxlot_ids': taxlot_state_ids,
-          }).then(function (response) {
-            return response
-          });
-        };
+      geocode_factory.geocode_by_ids = function (property_state_ids, taxlot_state_ids) {
+        return $http.post('/api/v2/geocode/geocode_by_ids/', {
+          'property_ids': property_state_ids,
+          'taxlot_ids': taxlot_state_ids,
+        }).then(function (response) {
+          return response
+        });
+      };
 
-        return geocode_factory;
-      }
+      return geocode_factory;
+    }
 ])
