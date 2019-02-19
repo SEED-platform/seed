3b21bd6aae7947ca38a83d8e6068b64b6368d48c
Nicholas Long <nicholas.long@nrel.gov>
2019-02-19 10:18:12 -0700

    release 2.4.2 update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='seed',
-    version='2.4.1',
+    version='2.4.2',
     packages=find_packages(),
     url='',
     license='revised BSD',
