3652bca392e88ca9f5c01b01de1c4a5189d3a309
Adrian Lara <adrian@devetry.com>
2019-03-07 14:03:36 -0700

    Users can update display meter unit preferences in the org. settings page

--- seed/static/seed/js/services/meters_service.js
+++ seed/static/seed/js/services/meters_service.js
@@ -13,6 +13,12 @@ angular.module('BE.seed.service.meters', [])
         });
       };
 
+      meters_factory.valid_energy_types_units = function () {
+        return $http.get('/api/v2/meters/valid_types_units/').then(function (response) {
+          return response.data;
+        });
+      };
+
       return meters_factory;
     }
   ]);
