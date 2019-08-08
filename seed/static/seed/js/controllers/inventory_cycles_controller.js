c90862a181ba152ec0792a22cdf9f74ca5bee20b
Adrian Lara <adrian@devetry.com>
2019-08-08 12:54:47 -0600

    YOY inventory list improvements including - settings profile changeable

--- seed/static/seed/js/controllers/inventory_cycles_controller.js
+++ seed/static/seed/js/controllers/inventory_cycles_controller.js
@@ -50,13 +50,19 @@ angular.module('BE.seed.controller.inventory_cycles', [])
     ) {
       spinner_utility.show();
       $scope.inventory_type = $stateParams.inventory_type;
+      $scope.profiles = profiles;
+      $scope.currentProfile = current_profile;
+      $scope.all_columns = all_columns
 
-      // matching_criteria_columns identified here to (always) pin left on table
-      if ($scope.inventory_type == "properties") {
-        $scope.matching_criteria_columns = matching_criteria_columns.PropertyState;
-      } else {
-        $scope.matching_criteria_columns = matching_criteria_columns.TaxLotState;
-      }
+      // set up i18n
+      //
+      // let angular-translate be in charge ... need
+      // to feed the language-only part of its $translate setting into
+      // ui-grid's i18nService
+      var stripRegion = function (languageTag) {
+        return _.first(languageTag.split('_'));
+      };
+      i18nService.setCurrentLang(stripRegion($translate.proposedLanguage() || $translate.use()));
 
       // Establish all cycle options and initially included cycles
       $scope.included_cycle_ids = [];
@@ -70,7 +76,7 @@ angular.module('BE.seed.controller.inventory_cycles', [])
         };
       });
 
-      // Checks for changes and refreshes grid objects if necessary
+      // Checks for cycle selection changes and refreshes inventory if necessary
       $scope.cycle_selection_toggled = function (is_open) {
         if (!is_open) {
           var updated_selections = _.map(_.filter($scope.cycle_options, ['selected', true]), 'value');
@@ -82,8 +88,8 @@ angular.module('BE.seed.controller.inventory_cycles', [])
       };
 
       // Takes raw cycle-partitioned records and returns array of cycle-aware records
-      $scope.reformat_records = function(refreshed_inventory) {
-        return _.reduce(refreshed_inventory, function(all_records, records, cycle_id) {
+      $scope.format_records = function(raw_inventory) {
+        return _.reduce(raw_inventory, function(all_records, records, cycle_id) {
           var cycle = _.find($scope.cycle_options, { value: parseInt(cycle_id) });
           _.forEach(records, function(record) {
             record.cycle_name = cycle.label;
@@ -93,107 +99,129 @@ angular.module('BE.seed.controller.inventory_cycles', [])
         }, []);
       };
 
+      // Set initial table data
+      $scope.data = $scope.format_records(inventory);
+
+      // Refreshes inventory by making API call
       $scope.refresh_objects = function() {
         spinner_utility.show();
         if ($scope.inventory_type == "properties") {
           inventory_service.properties_cycle($scope.currentProfile.id, $scope.included_cycle_ids).then(function(refreshed_inventory) {
-            $scope.data = $scope.reformat_records(refreshed_inventory);
+            $scope.data = $scope.format_records(refreshed_inventory);
             spinner_utility.hide();
           });
         } else {
           // inventory_service.taxlots_cycle($scope.currentProfile.id, $scope.included_cycle_ids).then(function(refreshed_inventory) {
-          //   $scope.data = $scope.reformat_records(refreshed_inventory);
+          //   $scope.data = $scope.format_records(refreshed_inventory);
           //   spinner_utility.hide();
           // });
         }
       };
 
-      $scope.data = $scope.reformat_records(inventory);
+      // On profile change, refreshes objects and rebuild columns
+      $scope.profile_change = function() {
+        inventory_service.save_last_profile($scope.currentProfile.id, $scope.inventory_type);
+        $scope.refresh_objects();
 
-      // set up i18n
-      //
-      // let angular-translate be in charge ... need
-      // to feed the language-only part of its $translate setting into
-      // ui-grid's i18nService
-      var stripRegion = function (languageTag) {
-        return _.first(languageTag.split('_'));
+        // uiGrid doesn't recognize complete columnDefs swap unless it's removed and refreshed and notified for each
+        $scope.gridOptions.columnDefs = [];
+        $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.COLUMN)
+
+        $scope.build_columns();
+
+        $scope.gridOptions.columnDefs = $scope.columns;
+        $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.COLUMN)
       };
-      i18nService.setCurrentLang(stripRegion($translate.proposedLanguage() || $translate.use()));
 
-      // List Settings Profile
-      $scope.profiles = profiles;
-      $scope.currentProfile = current_profile;
-
-      if ($scope.currentProfile) {
-        $scope.columns = [];
-        _.forEach($scope.currentProfile.columns, function (col) {
-          var foundCol = _.find(all_columns, {id: col.id});
-          if (foundCol) {
-            foundCol.pinnedLeft = col.pinned;
-            $scope.columns.push(foundCol);
-          }
-        });
-      } else {
-        // No profiles exist
-        $scope.columns = _.reject(all_columns, 'is_extra_data');
-      }
-
-      // Build out columnDefs
-      var matching_field_value = function(aggregation, fieldValue) {
+      // Agg function returning last value of matching criteria field (all should be the same if they match)
+      $scope.matching_field_value = function(aggregation, fieldValue) {
         aggregation.value = fieldValue;
       };
 
-      var column_def_defaults = {
-        headerCellFilter: 'translate',
-        minWidth: 75,
-        width: 150
+      // matching_criteria_columns identified here to pin left on table
+      if ($scope.inventory_type == "properties") {
+        $scope.matching_criteria_columns = matching_criteria_columns.PropertyState;
+      } else {
+        $scope.matching_criteria_columns = matching_criteria_columns.TaxLotState;
+      }
+
+      // Builds columns with profile, default, and grouping settings
+      $scope.build_columns = function() {
+        $scope.columns = [];
+
+        // Profile Settings
+        if ($scope.currentProfile) {
+          _.forEach($scope.currentProfile.columns, function (col) {
+            var foundCol = _.find($scope.all_columns, {id: col.id});
+            if (foundCol) {
+              foundCol.pinnedLeft = col.pinned;
+              $scope.columns.push(foundCol);
+            }
+          });
+        } else {
+          // No profiles exist
+          $scope.columns = _.reject($scope.all_columns, 'is_extra_data');
+        }
+
+        // Default Settings
+        var column_def_defaults = {
+          headerCellFilter: 'translate',
+          minWidth: 75,
+          width: 150
+        };
+
+        _.map($scope.columns, function (col) {
+          var options = {};
+          if (col.data_type === 'datetime') {
+            options.cellFilter = 'date:\'yyyy-MM-dd h:mm a\'';
+          } // else if (['eui', 'interger'].includes(col.data_type)) {
+          //   options.cellTemplate = '<div ng-if="row.groupHeader" class="ui-grid-cell-contents" tooltip-append-to-body="true" tooltip-popup-delay="500" title="TOOLTIP">[tooltip-placeholder?]</div>' +
+          //     '<div ng-if="!row.groupHeader" class="ui-grid-cell-contents">{{COL_FIELD CUSTOM_FILTERS}}</div>'
+          // }
+
+          // For matching criteria values, always pin left and show values in aggregate rows.
+          if ($scope.matching_criteria_columns.includes(col.column_name)) {
+            col.pinnedLeft = true;
+            options.customTreeAggregationFn = $scope.matching_field_value;
+          }
+          return _.defaults(col, options, column_def_defaults);
+        });
+
+        // Grouping Settings
+        $scope.columns.unshift(
+          {
+            displayName: 'Linking ID',
+            grouping: { groupPriority: 0 },
+            name: 'id',
+            sort: { priority: 0, direction: 'desc' },
+            pinnedLeft: true,
+            visible: false,
+            minWidth: 75,
+            width: 150
+          },
+          {
+            name: "cycle_name",
+            displayName: "Cycle",
+            pinnedLeft: true,
+            treeAggregationType: uiGridGroupingConstants.aggregation.COUNT,
+            minWidth: 75,
+            width: 150
+          },
+        )
       };
 
-      _.map($scope.columns, function (col) {
-        var options = {};
-        if (col.data_type === 'datetime') {
-          options.cellFilter = 'date:\'yyyy-MM-dd h:mm a\'';
-        } // else if (['eui', 'interger'].includes(col.data_type)) {
-        //   options.cellTemplate = '<div ng-if="row.groupHeader" class="ui-grid-cell-contents" tooltip-append-to-body="true" tooltip-popup-delay="500" title="TOOLTIP">[tooltip-placeholder?]</div>' +
-        //     '<div ng-if="!row.groupHeader" class="ui-grid-cell-contents">{{COL_FIELD CUSTOM_FILTERS}}</div>'
-        // }
-
-        if ($scope.matching_criteria_columns.includes(col.column_name)) {
-          col.pinnedLeft = true; // always overrides what's given
-          options.customTreeAggregationFn = matching_field_value;
-        }
-        return _.defaults(col, options, column_def_defaults);
-      });
-
-      $scope.columns.unshift(
-        {
-          displayName: 'Linking ID',
-          grouping: { groupPriority: 0 },
-          name: 'id',
-          sort: { priority: 0, direction: 'desc' },
-          pinnedLeft: true,
-          visible: false,
-          minWidth: 75,
-          width: 150
-        },
-        {
-          name: "cycle_name",
-          displayName: "Cycle",
-          pinnedLeft: true,
-          treeAggregationType: uiGridGroupingConstants.aggregation.COUNT,
-          minWidth: 75,
-          width: 150
-        },
-      )
+      $scope.build_columns();
 
       $scope.gridOptions = {
         data: 'data',
         columnDefs: $scope.columns,
         enableColumnResizing: true,
+        onRegisterApi: function (gridApi) {
+          $scope.gridApi = gridApi;
+        },
       };
 
-      // console.log("currentProfile", $scope.currentProfile);
-      // console.log("current_profile", current_profile);
+      // console.log("profiles", $scope.profiles);
       // console.log("data", $scope.data);
       // console.log("incoming cycles", cycles.cycles);
       // console.log("cycle_options", $scope.cycle_options);
