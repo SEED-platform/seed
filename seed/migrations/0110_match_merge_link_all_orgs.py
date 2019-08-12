f101dd6a539fd4331c91eca0c9f395fd393c7390
Adrian Lara <adrian@devetry.com>
2019-08-11 21:32:52 -0600

    Shift match_merge_link across orgs migration

--- seed/migrations/0110_match_merge_link_all_orgs.py
+++ seed/migrations/0110_match_merge_link_all_orgs.py
@@ -15,7 +15,7 @@ def forwards(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('seed', '0108_auto_20190730_1212'),
+        ('seed', '0109_auto_20190730_1212'),
     ]
 
     operations = [
