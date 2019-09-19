63b78fad8963beb74f8a0ba259454f16c69df87f
Alex Swindler <Alex.Swindler@nrel.gov>
2019-09-19 14:53:37 -0600

    Revert "Removed unused `is_staff` property", it's a necessary part of the default Django authorization.
    This reverts commit f83ecbe826982236fb4f12167bbad0fe2e3e8d37.

--- seed/landing/models.py
+++ seed/landing/models.py
@@ -46,6 +46,10 @@ class SEEDUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
     is_active = models.BooleanField(
         _('active'), default=True,
         help_text=_('Designates whether this user should be treated as '
