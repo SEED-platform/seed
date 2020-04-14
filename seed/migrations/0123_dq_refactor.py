99a5f30ef5716071e0249dbb2ccd41526e27655f
lin <llin@nrel.gov>
2020-04-14 11:45:34 -0600

    Separate condition check into multiple lines; migrations with debug printouts

--- seed/migrations/0123_dq_refactor.py
+++ seed/migrations/0123_dq_refactor.py
@@ -10,14 +10,16 @@ def forwards(apps, schema_editor):
     # r_condition = []
     for r in rule.values():
         if r['min'] is None and r['max'] is None:
-            if r['required']: r['condition'] = 'required'
-            else: r['condition'] = 'not_null'
+            if (r['required']):
+                r['condition'] = 'required'
+            else:
+                r['condition'] = 'not_null'
         else:
-            if r['data_type'] == 1: r['condition'] = 'range'
-            else: r['condition'] = 'include'
-
+            if (r['data_type'] == 1):
+                r['condition'] = 'range'
+            else:
+                r['condition'] = 'include'
         # if (r['condition'] != ''): r_condition.append(r['condition'])
-
     # print('\nAll rules included? ', len(r_condition) == Rule.objects.count())
 
 
