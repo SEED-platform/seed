ce50da36483a3e9957b39b66116041dae27cc5b4
Justin Belanger <justindbelanger@outlook.com>
2020-04-14 17:24:30 -0700

    Added SMTP-related env vars to docker config

--- config/settings/docker.py
+++ config/settings/docker.py
@@ -14,7 +14,9 @@ ENV_VARS = ['POSTGRES_DB', 'POSTGRES_PORT', 'POSTGRES_USER', 'POSTGRES_PASSWORD'
 # The optional vars will set the SERVER_EMAIL information as needed
 OPTIONAL_ENV_VARS = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_SES_REGION_NAME',
                      'AWS_SES_REGION_ENDPOINT', 'SERVER_EMAIL', 'SENTRY_JS_DSN', 'SENTRY_RAVEN_DSN',
-                     'REDIS_PASSWORD', 'DJANGO_EMAIL_BACKEND']
+                     'REDIS_PASSWORD', 'DJANGO_EMAIL_BACKEND',
+                     'EMAIL_HOST', 'EMAIL_PORT', 'EMAIL_HOST_USER',
+                     'EMAIL_HOST_PASSWORD', 'EMAIL_USE_TLS', 'EMAIL_USE_SSL']
 
 for loc in ENV_VARS + OPTIONAL_ENV_VARS:
     locals()[loc] = os.environ.get(loc)
