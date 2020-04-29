f20aad98a9478329f331c0620116e44fa38e7603
aviveiros11 <viveiros.austin@gmail.com>
2020-04-29 12:24:12 -0600

    working on Label endpoint conversion to v3

--- seed/api/v3/urls.py
+++ seed/api/v3/urls.py
@@ -5,8 +5,12 @@ from rest_framework import routers
 
 from seed.views.v3.data_quality import DataQualityViews
 
+from seed.views.v3.labels import LabelViewSet
+
 api_v3_router = routers.DefaultRouter()
 api_v3_router.register(r'data_quality_checks', DataQualityViews, base_name='data_quality_checks')
+
+api_v3_router.register(r'labels', LabelViewSet, base_name='labels')
 
 urlpatterns = [
     url(r'^', include(api_v3_router.urls)),
