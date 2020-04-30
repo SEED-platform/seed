a07d778e43199b338690147f8fd9e852d2c3678c
lin <llin@nrel.gov>
2020-04-30 10:13:11 -0600

    Migration modification on data_type 1, not 1, and null.

--- seed/migrations/0125_dq_refactor.py
+++ seed/migrations/0125_dq_refactor.py
@@ -4,20 +4,20 @@ from django.db import migrations, models
 
 
 def forwards(apps, schema_editor):
-    Rule = apps.get_model('seed', 'Rule')
-    # Total 1226 rows/rules;
-    # ====================================================================================================================================== #
-    # data_type is not null (1055) ---> data_type = 1 (171) ---> text_match is null (107) ---> not_null or required?                         #
-    #                               |                       |--> test_match not null (64) ---> text_match != '' (63): include                #
-    #                               |                                                     |--> text_match = '' (1) --> not_null or required? #
-    #                               |                                                                                                        #
-    #                               |-> data_type != 1 (884) ---> min and max are null (32) ---> not_null or required?                       #
-    #                                                        |--> min or max not null (852): range                                           #
-    #                                                                                                                                        #
-    # data_type is null (171) --------> min and max are null (170) ---> text_match is null (170) ---> not_null or required?                  #
-    #                         |                                    |--> text_match not null (0) ----> include                                #
-    #                         |-------> min or max not null (1) ---> range                                                                   #
-    # ====================================================================================================================================== #
+    Rule = apps.get_model('seed', 'Rule')  # Total 1226
+    # =========================================================================================================== #
+    # data_type not null ---> 1 ---> text_match null: not_null/require/''                                         #
+    #                    |      |--> test_match not null ---> text_match != '': include                           #
+    #                    |                               |--> text_match == '': not_null/required/''              #
+    #                    |                                                                                        #
+    #                    |--> != 1 ---> min & max null: not_null/required/''                                      #
+    #                              |--> min / max not null: range                                                 #
+    #                                                                                                             #
+    # data_type null -------> min & max null ---> text_match null: not_null/required/''                           #
+    #                |                       |--> text_match not null ---> text_match == '': not_null/required/'' #
+    #                |                                                |--> text_match != '': include              #
+    #                |--> min / max not null ---> range                                                           #
+    # =========================================================================================================== #
     Rule.objects.filter(data_type=1, text_match=None, not_null=True).update(condition='not_null')  # TODO: ?
     Rule.objects.filter(data_type=1, text_match=None, required=True, not_null=False).update(condition='required')
     Rule.objects.filter(data_type=1, text_match=None, required=False, not_null=False).update(condition='')
@@ -26,15 +26,22 @@ def forwards(apps, schema_editor):
     Rule.objects.filter(data_type=1, text_match='').filter(required=False, not_null=False).update(condition='')
     Rule.objects.filter(data_type=1).exclude(text_match=None).exclude(text_match='').update(condition='include')
 
-    Rule.objects.exclude(data_type=1).filter(min=None, max=None, not_null=True).update(condition='not_null')  # TODO: ?
-    Rule.objects.exclude(data_type=1).filter(min=None, max=None, required=True, not_null=False).update(condition='required')
-    Rule.objects.exclude(data_type=1).filter(min=None, max=None, required=False, not_null=False).update(condition='')
-    Rule.objects.exclude(data_type=1, min=None, max=None).update(condition='range')
+    Rule.objects.exclude(data_type=1).exclude(data_type=None).filter(min=None, max=None, not_null=True).update(condition='not_null')  # TODO: ?
+    Rule.objects.exclude(data_type=1).exclude(data_type=None).filter(min=None, max=None, required=True, not_null=False).update(condition='required')
+    Rule.objects.exclude(data_type=1).exclude(data_type=None).filter(min=None, max=None, required=False, not_null=False).update(condition='')
+    Rule.objects.filter(data_type=0).exclude(min=None, max=None).update(condition='range')
+    Rule.objects.filter(data_type=2).exclude(min=None, max=None).update(condition='range')
+    Rule.objects.filter(data_type=3).exclude(min=None, max=None).update(condition='range')
+    Rule.objects.filter(data_type=4).exclude(min=None, max=None).update(condition='range')
+    Rule.objects.filter(data_type=5).exclude(min=None, max=None).update(condition='range')
 
     Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, required=True, not_null=False).update(condition='required')
-    Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, required=False, not_null=False).update(condition='')
-    Rule.objects.filter(data_type=None, min=None, max=None).exclude(text_match=None).update(condition='include')
+    Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, required=False, not_null=False).update(condition='')  # TODO: ?
+    Rule.objects.filter(data_type=None, min=None, max=None, text_match='', not_null=True).update(condition='not_null')
+    Rule.objects.filter(data_type=None, min=None, max=None, text_match='', required=True, not_null=False).update(condition='required')
+    Rule.objects.filter(data_type=None, min=None, max=None, text_match='', required=False, not_null=False).update(condition='')
+    Rule.objects.filter(data_type=None, min=None, max=None).exclude(text_match=None).exclude(text_match='').update(condition='include')
     Rule.objects.filter(data_type=None).exclude(min=None, max=None).update(condition='range')
 
 
