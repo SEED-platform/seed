928d3f33f83d1c258c8a87f1800c984f8b5912d4
lin <llin@nrel.gov>
2020-05-04 13:49:05 -0600

    Removed test that reports null value as an error under rules that check for ranges, not value existence (not_null).

--- seed/data_importer/tests/integration/test_footprints_import.py
+++ seed/data_importer/tests/integration/test_footprints_import.py
@@ -179,12 +179,12 @@ class TestDemoV2(DataMappingBaseTestCase):
         pdq = DataQualityCheck.retrieve(self.org.id)
         pdq.check_data('PropertyState', [property_1, property_2, property_3])
         match = False
-        for result in pdq.results[property_1.id]['data_quality_results']:
-            if result['detailed_message'] == "Conditioned Floor Area is null":
-                match = True
+        # for result in pdq.results[property_1.id]['data_quality_results']:
+        #     if result['detailed_message'] == "Conditioned Floor Area is null":
+        #         match = True
         for result in pdq.results[property_2.id]['data_quality_results']:
             if result['detailed_message'] == "'{}' is not a valid geometry".format(invalid_property_footprint_string):
-                match &= True
+                match = True
         for result in pdq.results[property_3.id]['data_quality_results']:
             if result['detailed_message'] == "'123' is not a valid geometry":
                 match &= True
