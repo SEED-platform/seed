726ebf3d7e894860b5a6834b16eab854da3eaa39
lin <llin@nrel.gov>
2020-05-05 17:00:55 -0600

    Migration changed text rule with not_null condition to show null string

--- seed/migrations/0125_dq_refactor.py
+++ seed/migrations/0125_dq_refactor.py
@@ -27,12 +27,12 @@ def forwards(apps, schema_editor):
     #     min or max not null: range -> not_null: (+)not_null;
     #                                -> !not_null -> required: (+)required;
 
-    def create_condition(rule, condition):
+    def create_condition(rule, condition, text_match):
         Rule.objects.create(name=rule['name'], description=rule['description'], table_name=rule['table_name'], field=rule['field'],
                             enabled=rule['enabled'], data_type=rule['data_type'], rule_type=rule['rule_type'], min=rule['min'],
                             max=rule['max'], severity=rule['severity'], units=rule['units'],
                             data_quality_check_id=rule['data_quality_check_id'], status_label_id=rule['status_label_id'],
-                            text_match=rule['text_match'], condition=condition)
+                            text_match=text_match, condition=condition)
 
     Rule.objects.filter(data_type=1, text_match=None, not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type=1, text_match=None, required=True, not_null=False).update(condition='required')
@@ -43,10 +43,10 @@ def forwards(apps, schema_editor):
     Rule.objects.filter(data_type=1).exclude(text_match=None).exclude(text_match='').update(condition='include')
 
     for rule in Rule.objects.filter(data_type=1).exclude(text_match=None).exclude(text_match='').filter(not_null=True).values():
-        create_condition(rule, 'not_null')
+        create_condition(rule, 'not_null', None)
 
     for rule in Rule.objects.filter(data_type=1).exclude(text_match=None).exclude(text_match='').filter(required=True, not_null=False).values():
-        create_condition(rule, 'required')
+        create_condition(rule, 'required', rule['text_match'])
 
     Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5], min=None, max=None, not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5], min=None, max=None, required=True, not_null=False).update(condition='required')
@@ -54,10 +54,10 @@ def forwards(apps, schema_editor):
     Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5]).exclude(min=None, max=None).update(condition='range')
 
     for rule in Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5]).exclude(min=None, max=None).filter(not_null=True).values():
-        create_condition(rule, 'not_null')
+        create_condition(rule, 'not_null', rule['text_match'])
 
     for rule in Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5]).exclude(min=None, max=None).filter(required=True, not_null=False).values():
-        create_condition(rule, 'required')
+        create_condition(rule, 'required', rule['text_match'])
 
     Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, required=True, not_null=False).update(condition='required')
@@ -68,17 +68,17 @@ def forwards(apps, schema_editor):
 
     Rule.objects.filter(data_type=None).exclude(text_match='').exclude(text_match=None).update(condition='include')
     for rule in Rule.objects.exclude(text_match='').exclude(text_match=None).filter(data_type=None, not_null=True).values():
-        create_condition(rule, 'not_null')
+        create_condition(rule, 'not_null', None)
 
     for rule in Rule.objects.exclude(text_match='').exclude(text_match=None).filter(data_type=None, required=True, not_null=False).values():
-        create_condition(rule, 'required')
+        create_condition(rule, 'required', rule['text_match'])
 
     Rule.objects.filter(data_type=None).exclude(min=None, max=None).update(condition='range')
     for rule in Rule.objects.filter(data_type=None, not_null=True).exclude(min=None, max=None).values():
-        create_condition(rule, 'not_null')
+        create_condition(rule, 'not_null', rule['text_match'])
 
     for rule in Rule.objects.filter(data_type=None, required=True, not_null=False).exclude(min=None, max=None).values():
-        create_condition(rule, 'required')
+        create_condition(rule, 'required', rule['text_match'])
 
 
 class Migration(migrations.Migration):
