d46b21f86c9dee2734d14772f50c950a1a5bf608
lin <llin@nrel.gov>
2020-05-05 21:46:55 -0600

    Get_or_create rull for not_null or required condition in case duplicates

--- seed/migrations/0125_dq_refactor.py
+++ seed/migrations/0125_dq_refactor.py
@@ -27,58 +27,58 @@ def forwards(apps, schema_editor):
     #     min or max not null: range -> not_null: (+)not_null;
     #                                -> !not_null -> required: (+)required;
 
-    def create_condition(rule, condition, text_match):
-        Rule.objects.create(name=rule['name'], description=rule['description'], table_name=rule['table_name'], field=rule['field'],
-                            enabled=rule['enabled'], data_type=rule['data_type'], rule_type=rule['rule_type'], min=rule['min'],
-                            max=rule['max'], severity=rule['severity'], units=rule['units'],
-                            data_quality_check_id=rule['data_quality_check_id'], status_label_id=rule['status_label_id'],
-                            text_match=text_match, condition=condition)
+    def create_rule(rule, condition, min, max, text_match):
+        Rule.objects.get_or_create(name=rule['name'], description=rule['description'], table_name=rule['table_name'],
+                                   field=rule['field'], enabled=rule['enabled'], data_type=rule['data_type'],
+                                   rule_type=rule['rule_type'], min=min, max=max, units=rule['units'],
+                                   data_quality_check_id=rule['data_quality_check_id'],
+                                   text_match=text_match, condition=condition)
 
     Rule.objects.filter(data_type=1, text_match=None, not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type=1, text_match=None, required=True, not_null=False).update(condition='required')
-    Rule.objects.filter(data_type=1, text_match=None, required=False, not_null=False).update(condition='')
+    Rule.objects.filter(data_type=1, text_match=None, required=False, not_null=False).update(condition='', enabled=False)
     Rule.objects.filter(data_type=1, text_match='').filter(not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type=1, text_match='').filter(required=True, not_null=False).update(condition='required')
-    Rule.objects.filter(data_type=1, text_match='').filter(required=False, not_null=False).update(condition='')
+    Rule.objects.filter(data_type=1, text_match='').filter(required=False, not_null=False).update(condition='', enabled=False)
     Rule.objects.filter(data_type=1).exclude(text_match=None).exclude(text_match='').update(condition='include')
 
     for rule in Rule.objects.filter(data_type=1).exclude(text_match=None).exclude(text_match='').filter(not_null=True).values():
-        create_condition(rule, 'not_null', None)
+        create_rule(rule, 'not_null', rule['min'], rule['max'], None)
 
     for rule in Rule.objects.filter(data_type=1).exclude(text_match=None).exclude(text_match='').filter(required=True, not_null=False).values():
-        create_condition(rule, 'required', rule['text_match'])
+        create_rule(rule, 'required', rule['min'], rule['max'], rule['text_match'])
 
     Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5], min=None, max=None, not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5], min=None, max=None, required=True, not_null=False).update(condition='required')
-    Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5], min=None, max=None, required=False, not_null=False).update(condition='')
+    Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5], min=None, max=None, required=False, not_null=False).update(condition='', enabled=False)
     Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5]).exclude(min=None, max=None).update(condition='range')
 
     for rule in Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5]).exclude(min=None, max=None).filter(not_null=True).values():
-        create_condition(rule, 'not_null', rule['text_match'])
+        create_rule(rule, 'not_null', None, None, rule['text_match'])
 
     for rule in Rule.objects.filter(data_type__in=[0, 2, 3, 4, 5]).exclude(min=None, max=None).filter(required=True, not_null=False).values():
-        create_condition(rule, 'required', rule['text_match'])
+        create_rule(rule, 'required', rule['min'], rule['max'], rule['text_match'])
 
     Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, required=True, not_null=False).update(condition='required')
-    Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, required=False, not_null=False).update(condition='')
+    Rule.objects.filter(data_type=None, min=None, max=None, text_match=None, required=False, not_null=False).update(condition='', enabled=False)
     Rule.objects.filter(data_type=None, min=None, max=None, text_match='', not_null=True).update(condition='not_null')
     Rule.objects.filter(data_type=None, min=None, max=None, text_match='', required=True, not_null=False).update(condition='required')
-    Rule.objects.filter(data_type=None, min=None, max=None, text_match='', required=False, not_null=False).update(condition='')
+    Rule.objects.filter(data_type=None, min=None, max=None, text_match='', required=False, not_null=False).update(condition='', enabled=False)
 
     Rule.objects.filter(data_type=None).exclude(text_match='').exclude(text_match=None).update(condition='include')
     for rule in Rule.objects.exclude(text_match='').exclude(text_match=None).filter(data_type=None, not_null=True).values():
-        create_condition(rule, 'not_null', None)
+        create_rule(rule, 'not_null', rule['min'], rule['max'], None)
 
     for rule in Rule.objects.exclude(text_match='').exclude(text_match=None).filter(data_type=None, required=True, not_null=False).values():
-        create_condition(rule, 'required', rule['text_match'])
+        create_rule(rule, 'required', rule['min'], rule['max'], rule['text_match'])
 
     Rule.objects.filter(data_type=None).exclude(min=None, max=None).update(condition='range')
     for rule in Rule.objects.filter(data_type=None, not_null=True).exclude(min=None, max=None).values():
-        create_condition(rule, 'not_null', rule['text_match'])
+        create_rule(rule, 'not_null', None, None, rule['text_match'])
 
     for rule in Rule.objects.filter(data_type=None, required=True, not_null=False).exclude(min=None, max=None).values():
-        create_condition(rule, 'required', rule['text_match'])
+        create_rule(rule, 'required', rule['min'], rule['max'], rule['text_match'])
 
 
 class Migration(migrations.Migration):
