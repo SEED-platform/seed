dd98d2478d8165841186ca4732398b799af06fcf
aviveiros11 <viveiros.austin@gmail.com>
2020-05-19 12:58:02 -0600

    moved to post and added properties/taxlots to labels endpoint

--- seed/api/v3/urls.py
+++ seed/api/v3/urls.py
@@ -5,16 +5,27 @@ from rest_framework import routers
 
 from seed.views.v3.data_quality import DataQualityViews
 from seed.views.v3.datasets import DatasetViewSet
-
 from seed.views.v3.labels import LabelViewSet
+from seed.views.v3.labels import UpdateInventoryLabelsAPIView
 
 api_v3_router = routers.DefaultRouter()
 api_v3_router.register(r'data_quality_checks', DataQualityViews, base_name='data_quality_checks')
 api_v3_router.register(r'datasets', DatasetViewSet, base_name='datasets')
-
-
 api_v3_router.register(r'labels', LabelViewSet, base_name='labels')
+
 
 urlpatterns = [
     url(r'^', include(api_v3_router.urls)),
+    url(
+        r'labels-property/$',
+        UpdateInventoryLabelsAPIView.as_view(),
+        {'inventory_type': 'property'},
+        name="property-labels",
+    ),
+    url(
+        r'labels-taxlot/$',
+        UpdateInventoryLabelsAPIView.as_view(),
+        {'inventory_type': 'taxlot'},
+        name="taxlot-labels",
+    ),
 ]
