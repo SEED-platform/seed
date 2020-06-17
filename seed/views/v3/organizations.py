bfc2e848302687bd5e0b4d1ea4bb224c7de301a8
Ted Summer <ted.summer2@gmail.com>
2020-06-17 10:34:17 -0600

    refactor(v3/organizations): move get_property_report_data and get_aggregatted_... to v3

--- seed/views/v3/organizations.py
+++ seed/views/v3/organizations.py
@@ -5,6 +5,7 @@
 """
 
 import logging
+from collections import defaultdict
 from io import BytesIO
 from random import randint
 
@@ -37,6 +38,7 @@ from seed.serializers.pint import apply_display_unit_preferences
 from seed.utils.api import api_endpoint_class
 from seed.utils.api_schema import AutoSchemaHelper
 from seed.utils.cache import get_cache_raw, set_cache_raw
+from seed.utils.generic import median, round_down_hundred_thousand
 from seed.utils.match import (matching_criteria_column_names,
                               whole_org_match_merge_link)
 from seed.utils.organizations import (create_organization,
@@ -1036,6 +1038,186 @@ class OrganizationViewSet(viewsets.ViewSet):
             }
             results.append(result)
         return results
+
+    def get_property_report_data(self, request):
+        campus_only = request.query_params.get('campus_only', False)
+        params = {}
+        missing_params = []
+        error = ''
+        for param in ['x_var', 'y_var', 'organization_id', 'start', 'end']:
+            val = request.query_params.get(param, None)
+            if not val:
+                missing_params.append(param)
+            else:
+                params[param] = val
+        if missing_params:
+            error = "{} Missing params: {}".format(
+                error, ", ".join(missing_params)
+            )
+        if error:
+            status_code = status.HTTP_400_BAD_REQUEST
+            result = {'status': 'error', 'message': error}
+        else:
+            cycles = self.get_cycles(params['start'], params['end'])
+            data = self.get_raw_report_data(
+                params['organization_id'], cycles,
+                params['x_var'], params['y_var'], campus_only
+            )
+            for datum in data:
+                if datum['property_counts']['num_properties_w-data'] != 0:
+                    break
+            property_counts = []
+            chart_data = []
+            for datum in data:
+                property_counts.append(datum['property_counts'])
+                chart_data.extend(datum['chart_data'])
+            data = {
+                'property_counts': property_counts,
+                'chart_data': chart_data,
+            }
+            result = {'status': 'success', 'data': data}
+            status_code = status.HTTP_200_OK
+        return Response(result, status=status_code)
+
+    def get_aggregated_property_report_data(self, request):
+        campus_only = request.query_params.get('campus_only', False)
+        valid_y_values = ['gross_floor_area', 'use_description', 'year_built']
+        params = {}
+        missing_params = []
+        empty = True
+        error = ''
+        for param in ['x_var', 'y_var', 'organization_id', 'start', 'end']:
+            val = request.query_params.get(param, None)
+            if not val:
+                missing_params.append(param)
+            elif param == 'y_var' and val not in valid_y_values:
+                error = "{} {} is not a valid value for {}.".format(
+                    error, val, param
+                )
+            else:
+                params[param] = val
+        if missing_params:
+            error = "{} Missing params: {}".format(
+                error, ", ".join(missing_params)
+            )
+        if error:
+            status_code = status.HTTP_400_BAD_REQUEST
+            result = {'status': 'error', 'message': error}
+        else:
+            cycles = self.get_cycles(params['start'], params['end'])
+            x_var = params['x_var']
+            y_var = params['y_var']
+            data = self.get_raw_report_data(
+                params['organization_id'], cycles, x_var, y_var,
+                campus_only
+            )
+            for datum in data:
+                if datum['property_counts']['num_properties_w-data'] != 0:
+                    empty = False
+                    break
+            if empty:
+                result = {'status': 'error', 'message': 'No data found'}
+                status_code = status.HTTP_404_NOT_FOUND
+        if not empty or not error:
+            chart_data = []
+            property_counts = []
+            for datum in data:
+                buildings = datum['chart_data']
+                yr_e = datum['property_counts']['yr_e']
+                chart_data.extend(self.aggregate_data(yr_e, y_var, buildings)),
+                property_counts.append(datum['property_counts'])
+            # Send back to client
+            aggregated_data = {
+                'chart_data': chart_data,
+                'property_counts': property_counts
+            }
+            result = {
+                'status': 'success',
+                'aggregated_data': aggregated_data,
+            }
+            status_code = status.HTTP_200_OK
+        return Response(result, status=status_code)
+
+    def aggregate_data(self, yr_e, y_var, buildings):
+        aggregation_method = {
+            'use_description': self.aggregate_use_description,
+            'year_built': self.aggregate_year_built,
+            'gross_floor_area': self.aggregate_gross_floor_area,
+
+
+        }
+        return aggregation_method[y_var](yr_e, buildings)
+
+    def aggregate_use_description(self, yr_e, buildings):
+        # Group buildings in this year_ending group into uses
+        chart_data = []
+        grouped_uses = defaultdict(list)
+        for b in buildings:
+            grouped_uses[str(b['y']).lower()].append(b)
+
+        # Now iterate over use groups to make each chart item
+        for use, buildings_in_uses in grouped_uses.items():
+            chart_data.append({
+                'x': median([b['x'] for b in buildings_in_uses]),
+                'y': use.capitalize(),
+                'yr_e': yr_e
+            })
+        return chart_data
+
+    def aggregate_year_built(self, yr_e, buildings):
+        # Group buildings in this year_ending group into decades
+        chart_data = []
+        grouped_decades = defaultdict(list)
+        for b in buildings:
+            grouped_decades['%s0' % str(b['y'])[:-1]].append(b)
+
+        # Now iterate over decade groups to make each chart item
+        for decade, buildings_in_decade in grouped_decades.items():
+            chart_data.append({
+                'x': median(
+                    [b['x'] for b in buildings_in_decade]
+                ),
+                'y': '%s-%s' % (decade, '%s9' % str(decade)[:-1]),  # 1990-1999
+                'yr_e': yr_e
+            })
+        return chart_data
+
+    def aggregate_gross_floor_area(self, yr_e, buildings):
+        chart_data = []
+        y_display_map = {
+            0: '0-99k',
+            100000: '100-199k',
+            200000: '200k-299k',
+            300000: '300k-399k',
+            400000: '400-499k',
+            500000: '500-599k',
+            600000: '600-699k',
+            700000: '700-799k',
+            800000: '800-899k',
+            900000: '900-999k',
+            1000000: 'over 1,000k',
+        }
+        max_bin = max(y_display_map)
+
+        # Group buildings in this year_ending group into ranges
+        grouped_ranges = defaultdict(list)
+        for b in buildings:
+            area = b['y']
+            # make sure anything greater than the biggest bin gets put in
+            # the biggest bin
+            range_bin = min(max_bin, round_down_hundred_thousand(area))
+            grouped_ranges[range_bin].append(b)
+
+        # Now iterate over range groups to make each chart item
+        for range_floor, buildings_in_range in grouped_ranges.items():
+            chart_data.append({
+                'x': median(
+                    [b['x'] for b in buildings_in_range]
+                ),
+                'y': y_display_map[range_floor],
+                'yr_e': yr_e
+            })
+        return chart_data
 
     @swagger_auto_schema(
         manual_parameters=[
