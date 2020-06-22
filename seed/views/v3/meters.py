e39bdc906046d36b2af88b66e7449eab9bd9a834
Ted Summer <ted.summer2@gmail.com>
2020-06-22 17:49:04 -0600

    chore(v3/meters): add comments

--- seed/views/v3/meters.py
+++ seed/views/v3/meters.py
@@ -15,6 +15,15 @@ class MeterViewSet(viewsets.ViewSet):
     @ajax_request_class
     @action(detail=False, methods=['GET'])
     def valid_types_units(self, request):
+        """
+        Returns the valid type for units.
+
+        The valid type and unit combinations are built from US Thermal Conversion
+        values. As of this writing, the valid combinations are the same as for
+        Canadian conversions, even though the actual factors may differ between
+        the two.
+        (https://portfoliomanager.energystar.gov/pdf/reference/Thermal%20Conversions.pdf)
+        """
         return {
             type: list(units.keys())
             for type, units
