34738ab76a92217aa3e5930802abca7c5d4d5053
Ted Summer <ted.summer2@gmail.com>
2020-06-22 19:01:04 -0600

    feat(v3/labels_inventories): move UpdateInventoryLabels view to v3

--- seed/api/v3/urls.py
+++ seed/api/v3/urls.py
@@ -11,9 +11,10 @@ from seed.views.v3.cycles import CycleViewSet
 from seed.views.v3.data_quality_checks import DataQualityCheckViewSet
 from seed.views.v3.data_quality_check_rules import DataQualityCheckRuleViewSet
 from seed.views.v3.datasets import DatasetViewSet
-from seed.views.v3.labels import LabelViewSet
 from seed.views.v3.import_files import ImportFileViewSet
 from seed.views.v3.meters import MeterViewSet
+from seed.views.v3.labels import LabelViewSet
+from seed.views.v3.labels_inventories import LabelInventoryViewSet
 from seed.views.v3.organizations import OrganizationViewSet
 from seed.views.v3.properties import PropertyViewSet
 from seed.views.v3.taxlots import TaxlotViewSet
@@ -39,4 +40,14 @@ data_quality_checks_router.register(r'rules', DataQualityCheckRuleViewSet, base_
 urlpatterns = [
     url(r'^', include(api_v3_router.urls)),
     url(r'^', include(data_quality_checks_router.urls)),
+    url(
+        r'^labels_property/$',
+        LabelInventoryViewSet.as_view(),
+        {'inventory_type': 'property'},
+    ),
+    url(
+        r'^labels_taxlot/$',
+        LabelInventoryViewSet.as_view(),
+        {'inventory_type': 'taxlot'},
+    ),
 ]
