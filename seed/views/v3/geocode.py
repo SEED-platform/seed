c7a3414815b141a9830cb75caed65e1cf716d777
Adrian Lara <adrian@devetry.com>
2020-06-29 13:35:21 -0600

    API v3 geocode - geocode_by_ids shift to View IDs.

--- seed/views/v3/geocode.py
+++ seed/views/v3/geocode.py
@@ -1,6 +1,11 @@
 # !/usr/bin/env python
 # encoding: utf-8
 
+from django.db.models import Subquery
+from django.http import JsonResponse
+
+from drf_yasg.utils import swagger_auto_schema
+
 from rest_framework import viewsets
 from rest_framework.decorators import action
 
@@ -9,32 +14,49 @@ from seed.decorators import ajax_request_class
 from seed.lib.superperms.orgs.decorators import has_perm_class
 from seed.lib.superperms.orgs.models import Organization
 
-from seed.models.properties import PropertyState
-from seed.models.tax_lots import TaxLotState
-
+from seed.models.properties import PropertyState, PropertyView
+from seed.models.tax_lots import TaxLotState, TaxLotView
 from seed.utils.api import api_endpoint_class
+from seed.utils.api_schema import AutoSchemaHelper
 from seed.utils.geocode import geocode_buildings
 
 
 class GeocodeViewSet(viewsets.ViewSet):
 
+    @swagger_auto_schema(
+        manual_parameters=[AutoSchemaHelper.query_org_id_field()],
+        request_body=AutoSchemaHelper.schema_factory(
+            {
+                'property_view_ids': ['integer'],
+                'taxlot_view_ids': ['integer'],
+            },
+            description='IDs by inventory type for records to be geocoded.'
+        )
+    )
     @api_endpoint_class
     @ajax_request_class
     @has_perm_class('can_modify_data')
     @action(detail=False, methods=['POST'])
     def geocode_by_ids(self, request):
         body = dict(request.data)
-        property_ids = body.get('property_ids')
-        taxlot_ids = body.get('taxlot_ids')
+        property_view_ids = body.get('property_view_ids')
+        taxlot_view_ids = body.get('taxlot_view_ids')
 
-        if property_ids:
-            properties = PropertyState.objects.filter(id__in=property_ids)
+        if property_view_ids:
+            property_views = PropertyView.objects.filter(id__in=property_view_ids)
+            properties = PropertyState.objects.filter(
+                id__in=Subquery(property_views.values('state_id'))
+            )
             geocode_buildings(properties)
 
-        if taxlot_ids:
-            taxlots = TaxLotState.objects.filter(id__in=taxlot_ids)
+        if taxlot_view_ids:
+            taxlot_views = TaxLotView.objects.filter(id__in=taxlot_view_ids)
+            taxlots = TaxLotState.objects.filter(
+                id__in=Subquery(taxlot_views.values('state_id'))
+            )
             geocode_buildings(taxlots)
 
+        return JsonResponse({'status': 'success'})
     @ajax_request_class
     @action(detail=False, methods=['POST'])
     def confidence_summary(self, request):
