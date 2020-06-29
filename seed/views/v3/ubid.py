d4efee151457a8946e9c78f1512f7d36efc1b2b5
Adrian Lara <adrian@devetry.com>
2020-06-29 15:06:44 -0600

    API v3 ubid - decode_results shift to View IDs and add perm

--- seed/views/v3/ubid.py
+++ seed/views/v3/ubid.py
@@ -1,6 +1,9 @@
 # !/usr/bin/env python
 # encoding: utf-8
 
+from django.db.models import Subquery
+from django.http import JsonResponse
+
 from drf_yasg.utils import swagger_auto_schema
 
 from rest_framework import viewsets
@@ -8,8 +11,8 @@ from rest_framework.decorators import action
 
 from seed.decorators import ajax_request_class
 from seed.lib.superperms.orgs.decorators import has_perm_class
-from seed.models.properties import PropertyState
-from seed.models.tax_lots import TaxLotState
+from seed.models.properties import PropertyState, PropertyView
+from seed.models.tax_lots import TaxLotState, TaxLotView
 from seed.utils.api import api_endpoint_class
 from seed.utils.api_schema import AutoSchemaHelper
 from seed.utils.ubid import decode_unique_ids
@@ -51,7 +54,18 @@ class UbidViewSet(viewsets.ViewSet):
 
         return JsonResponse({'status': 'success'})
 
+    @swagger_auto_schema(
+        manual_parameters=[AutoSchemaHelper.query_org_id_field()],
+        request_body=AutoSchemaHelper.schema_factory(
+            {
+                'property_view_ids': ['integer'],
+                'taxlot_view_ids': ['integer'],
+            },
+            description='IDs by inventory type for records to be used in building a UBID decoding summary.'
+        )
+    )
     @ajax_request_class
+    @has_perm_class('can_view_data')
     @action(detail=False, methods=['POST'])
     def decode_results(self, request):
         body = dict(request.data)
@@ -62,25 +76,29 @@ class UbidViewSet(viewsets.ViewSet):
         ulid_unpopulated = 0
         ulid_successfully_decoded = 0
         ulid_not_decoded = 0
-        property_ids = body.get('property_ids')
-        taxlot_ids = body.get('taxlot_ids')
-        if property_ids:
-            properties = PropertyState.objects.filter(id__in=property_ids)
-            ubid_unpopulated = len(properties.filter(ubid__isnull=True))
-            ubid_successfully_decoded = len(properties.filter(ubid__isnull=False,
+        property_view_ids = body.get('property_view_ids')
+        taxlot_view_ids = body.get('taxlot_view_ids')
+        if property_view_ids:
+            property_views = PropertyView.objects.filter(id__in=property_view_ids)
+            property_states = PropertyState.objects.filter(id__in=Subquery(property_views.values('state_id')))
+
+            ubid_unpopulated = len(property_states.filter(ubid__isnull=True))
+            ubid_successfully_decoded = len(property_states.filter(ubid__isnull=False,
                                                               bounding_box__isnull=False,
                                                               centroid__isnull=False))
             # for ubid_not_decoded, bounding_box could be populated from a GeoJSON import
-            ubid_not_decoded = len(properties.filter(ubid__isnull=False, centroid__isnull=True))
+            ubid_not_decoded = len(property_states.filter(ubid__isnull=False, centroid__isnull=True))
 
-        if taxlot_ids:
-            taxlots = TaxLotState.objects.filter(id__in=taxlot_ids)
-            ulid_unpopulated = len(taxlots.filter(ulid__isnull=True))
-            ulid_successfully_decoded = len(taxlots.filter(ulid__isnull=False,
+        if taxlot_view_ids:
+            taxlot_views = TaxLotView.objects.filter(id__in=taxlot_view_ids)
+            taxlot_states = TaxLotState.objects.filter(id__in=Subquery(taxlot_views.values('state_id')))
+
+            ulid_unpopulated = len(taxlot_states.filter(ulid__isnull=True))
+            ulid_successfully_decoded = len(taxlot_states.filter(ulid__isnull=False,
                                                            bounding_box__isnull=False,
                                                            centroid__isnull=False))
             # for ulid_not_decoded, bounding_box could be populated from a GeoJSON import
-            ulid_not_decoded = len(taxlots.filter(ulid__isnull=False, centroid__isnull=True))
+            ulid_not_decoded = len(taxlot_states.filter(ulid__isnull=False, centroid__isnull=True))
 
         result = {
             "ubid_unpopulated": ubid_unpopulated,
