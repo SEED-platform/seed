f7d54560f0b5bb1a759a6be9c0e75d61a05b382b
Adrian Lara <adrian@devetry.com>
2020-06-29 13:37:29 -0600

    API v3 geocode - confidence_summary uses View IDs

--- seed/views/v3/geocode.py
+++ seed/views/v3/geocode.py
@@ -57,59 +57,74 @@ class GeocodeViewSet(viewsets.ViewSet):
             geocode_buildings(taxlots)
 
         return JsonResponse({'status': 'success'})
+
+    @swagger_auto_schema(
+        manual_parameters=[AutoSchemaHelper.query_org_id_field()],
+        request_body=AutoSchemaHelper.schema_factory(
+            {
+                'property_view_ids': ['integer'],
+                'taxlot_view_ids': ['integer'],
+            },
+            description='IDs by inventory type for records to be used in building a geocoding summary.'
+        )
+    )
+    @api_endpoint_class
     @ajax_request_class
+    @has_perm_class('can_view_data')
     @action(detail=False, methods=['POST'])
     def confidence_summary(self, request):
         body = dict(request.data)
-        property_ids = body.get('property_ids')
-        tax_lot_ids = body.get('tax_lot_ids')
+        property_view_ids = body.get('property_view_ids')
+        taxlot_view_ids = body.get('taxlot_view_ids')
 
         result = {}
 
-        if property_ids:
+        if property_view_ids:
+            property_views = PropertyView.objects.filter(id__in=property_view_ids)
             result["properties"] = {
                 'not_geocoded': len(PropertyState.objects.filter(
-                    id__in=property_ids,
+                    id__in=Subquery(property_views.values('state_id')),
                     geocoding_confidence__isnull=True
                 )),
                 'high_confidence': len(PropertyState.objects.filter(
-                    id__in=property_ids,
+                    id__in=Subquery(property_views.values('state_id')),
                     geocoding_confidence__startswith='High'
                 )),
                 'low_confidence': len(PropertyState.objects.filter(
-                    id__in=property_ids,
+                    id__in=Subquery(property_views.values('state_id')),
                     geocoding_confidence__startswith='Low'
                 )),
                 'manual': len(PropertyState.objects.filter(
-                    id__in=property_ids,
+                    id__in=Subquery(property_views.values('state_id')),
                     geocoding_confidence='Manually geocoded (N/A)'
                 )),
                 'missing_address_components': len(PropertyState.objects.filter(
-                    id__in=property_ids,
+                    id__in=Subquery(property_views.values('state_id')),
                     geocoding_confidence='Missing address components (N/A)'
                 )),
             }
 
-        if tax_lot_ids:
+        if taxlot_view_ids:
+            taxlot_views = TaxLotView.objects.filter(id__in=taxlot_view_ids)
             result["tax_lots"] = {
                 'not_geocoded': len(TaxLotState.objects.filter(
-                    id__in=tax_lot_ids,
+                    id__in=Subquery(taxlot_views.values('state_id')),
                     geocoding_confidence__isnull=True
                 )),
                 'high_confidence': len(TaxLotState.objects.filter(
-                    id__in=tax_lot_ids,
+                    id__in=Subquery(taxlot_views.values('state_id')),
                     geocoding_confidence__startswith='High'
                 )),
                 'low_confidence': len(TaxLotState.objects.filter(
-                    id__in=tax_lot_ids,
+                    id__in=Subquery(taxlot_views.values('state_id')),
                     geocoding_confidence__startswith='Low'
                 )),
                 'manual': len(TaxLotState.objects.filter(
-                    id__in=tax_lot_ids,
+                    id__in=Subquery(taxlot_views.values('state_id')),
                     geocoding_confidence='Manually geocoded (N/A)'
                 )),
                 'missing_address_components': len(TaxLotState.objects.filter(
-                    id__in=tax_lot_ids,
+                    id__in=Subquery(taxlot_views.values('state_id')),
                     geocoding_confidence='Missing address components (N/A)'
                 )),
             }
