ea555dfa7ccf34f4e103f78e3c8fdb87045625c8
Adrian Lara <adrian@devetry.com>
2020-07-02 15:49:42 -0600

    API v3 property scenarios perms update

--- seed/views/v3/property_scenarios.py
+++ seed/views/v3/property_scenarios.py
@@ -4,6 +4,8 @@
 :copyright (c) 2014 - 2020, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.  # NOQA
 :author
 """
+from django.utils.decorators import method_decorator
+
 from rest_framework.parsers import JSONParser, FormParser
 from rest_framework.renderers import JSONRenderer
 
@@ -14,8 +16,11 @@ from seed.serializers.scenarios import ScenarioSerializer
 from seed.utils.viewsets import (
     SEEDOrgReadOnlyModelViewSet
 )
+from seed.utils.api_schema import swagger_auto_schema_org_query_param
 
 
+@method_decorator(name='list', decorator=swagger_auto_schema_org_query_param)
+@method_decorator(name='retrieve', decorator=swagger_auto_schema_org_query_param)
 class PropertyScenarioViewSet(SEEDOrgReadOnlyModelViewSet):
     """
     API View for Scenarios. This only includes retrieve and list for now.
@@ -27,6 +32,8 @@ class PropertyScenarioViewSet(SEEDOrgReadOnlyModelViewSet):
     orgfilter = 'property_state__organization_id'
 
     def get_queryset(self):
+        # Authorization is partially implicit in that users can't try to query
+        # on an org_id for an Organization that they are not a member of.
         org_id = self.get_organization(self.request)
         property_view_id = self.kwargs.get('property_pk')
 
