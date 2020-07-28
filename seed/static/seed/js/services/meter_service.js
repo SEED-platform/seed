7f7c3bbd2200fa9e4d47da3dc457d50fee09bfd8
Ted Summer <ted.summer2@gmail.com>
2020-07-28 12:21:53 -0600

    refactor(meter_service): use v3 api for meter usage

--- seed/static/seed/js/services/meter_service.js
+++ seed/static/seed/js/services/meter_service.js
@@ -15,11 +15,13 @@ angular.module('BE.seed.service.meter', [])
 
       meter_factory.property_meter_usage = function (property_view_id, organization_id, interval, excluded_meter_ids) {
         if (_.isUndefined(excluded_meter_ids)) excluded_meter_ids = [];
-        return $http.post('/api/v2/meters/property_meter_usage/', {
-          property_view_id: property_view_id,
-          interval: interval,
-          excluded_meter_ids: excluded_meter_ids
-        }).then(function (response) {
+        return $http.post(
+          '/api/v3/properties/' + property_view_id + '/meter_usage/?organization_id=' + organization_id,
+          {
+            interval: interval,
+            excluded_meter_ids: excluded_meter_ids
+          }
+        ).then(function (response) {
           return response.data;
         });
       };
