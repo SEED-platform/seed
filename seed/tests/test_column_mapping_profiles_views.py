e8a90b2b028d715f6485a75586e973e1efb7ca61
aviveiros11 <viveiros.austin@gmail.com>
2020-07-31 11:42:58 -0400

    API v3 ColumnMappingProfile - update tests

--- seed/tests/test_column_mapping_profiles_views.py
+++ seed/tests/test_column_mapping_profiles_views.py
@@ -26,7 +26,7 @@ class ColumnMappingPresetViewsCore(DataMappingBaseTestCase):
         }
         self.client.login(**user_details)
 
-    def test_list_profile_endpoint(self):
+    def test_filter_profile_endpoint(self):
         preset_info = {
             "name": 'test_preset_1',
             "mappings": [
@@ -119,7 +119,7 @@ class ColumnMappingPresetViewsCore(DataMappingBaseTestCase):
         self.assertEqual(1, ColumnMappingPreset.objects.filter(name='changed_preset_name').count())
 
     def test_create_profile_endpoint(self):
-        url = reverse('api:v2:column_mapping_presets-list') + '?organization_id=' + str(self.org.id)
+        url = reverse('api:v3:column_mapping_presets-create') + '?organization_id=' + str(self.org.id)
 
         preset_info = dumps({
             "name": 'test_preset_1',
