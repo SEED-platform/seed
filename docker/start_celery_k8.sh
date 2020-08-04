076215ffb4745fd2967329669f72813298794db9
aviveiros11 <viveiros.austin@gmail.com>
2020-08-04 11:15:53 -0400

    pushing test image from PR and update charts

---
+++ docker/start_celery_k8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /seed
+
+echo "Waiting for postgres to start"
+/usr/local/wait-for-it.sh --strict -t 0 db-postgres:5432
+
+echo "Waiting for redis to start"
+/usr/local/wait-for-it.sh --strict -t 0 db-redis:6379
+
+echo "Waiting for web to start"
+/usr/local/wait-for-it.sh --strict -t 0 web-stage:80
+
+# check if the number of workers is set in the env
+if [ -z ${NUMBER_OF_WORKERS} ]; then
+    echo "var is unset"
+    # Set the number of workers to half the number of cores on the machine
+    export NUMBER_OF_WORKERS=$(($(nproc) / 2))
+    export NUMBER_OF_WORKERS=$(($NUMBER_OF_WORKERS>1?$NUMBER_OF_WORKERS:1))
+fi
+
+echo "Number of workers will be set to: $NUMBER_OF_WORKERS"
+celery -A seed worker -l info -c $NUMBER_OF_WORKERS --maxtasksperchild 1000 --uid 1000 --events
