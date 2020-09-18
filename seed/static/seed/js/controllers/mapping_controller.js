49de3eba9d8b86f85a9e0c7cd77d426ad8a07d85
lin <llin@nrel.gov>
2020-09-18 11:29:12 -0600

    added cycle id and test in mapping_controller.spec.js

--- seed/static/seed/js/controllers/mapping_controller.js
+++ seed/static/seed/js/controllers/mapping_controller.js
@@ -250,9 +250,9 @@ angular.module('BE.seed.controller.mapping', [])
       $scope.review_mappings = false;
       $scope.show_mapped_buildings = false;
 
-      $scope.validCycle = _.find(cycles.cycles, {id: $scope.import_file.cycle});
-      $scope.isValidCycle = Boolean($scope.validCycle);
-      $scope.cycleName = $scope.validCycle.name;
+      var validCycle = _.find(cycles.cycles, {id: $scope.import_file.cycle});
+      $scope.isValidCycle = Boolean(validCycle);
+      $scope.cycleName = validCycle.name;
 
       $scope.mappingBuildingSync = $scope.import_file.source_type === 'BuildingSync Raw';
 
