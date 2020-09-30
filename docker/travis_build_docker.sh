343f29ecb52f4d0b30efaa05974f9cac4abe803f
Austin <31779424+aviveiros11@users.noreply.github.com>
2020-09-30 14:37:00 -0600

    Update travis_build_docker.sh

--- docker/travis_build_docker.sh
+++ docker/travis_build_docker.sh
@@ -10,14 +10,14 @@ elif [ "${TRAVIS_EVENT_TYPE}" == "push" ]; then
     IMAGETAG=${TRAVIS_BRANCH}
 fi
 
-if [ "${IMAGETAG}" == "master" ] || [ "${IMAGETAG}" == "develop" ] && [ "${TRAVIS_PULL_REQUEST}" == "true" ]; then
+if [ "${IMAGETAG}" == "master" ] || [ "${IMAGETAG}" == "develop" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     docker-compose build --pull
     docker login -u $DOCKER_USER -p $DOCKER_PASS
 
     echo "Tagging image as $IMAGETAG"
     docker tag seedplatform/seed seedplatform/seed:$IMAGETAG
     docker push seedplatform/seed:$IMAGETAG
-elif [ "${IMAGETAG}" != "skip" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+elif [ "${IMAGETAG}" != "skip" ] && [ "${TRAVIS_PULL_REQUEST}" == "true" ]; then
     docker-compose build --pull
     docker login -u $DOCKER_USER -p $DOCKER_PASS
 
