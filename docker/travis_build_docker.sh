5fb86f66d17196e25f320a002667f8f22e440708
aviveiro <viveiros.austin@gmail.com>
2020-10-06 08:33:26 -0600

    removed new travis logic and cleaned up charts

--- docker/travis_build_docker.sh
+++ docker/travis_build_docker.sh
@@ -6,17 +6,6 @@ if [ "${TRAVIS_BRANCH}" == "develop" ]; then
 elif [ "${TRAVIS_BRANCH}" == "master" ]; then
     # Retrieve the version number from package.json
     IMAGETAG=$( sed -n 's/.*"version": "\(.*\)",/\1/p' package.json )
-elif [ "${TRAVIS_EVENT_TYPE}" == "push" ]; then
-    IMAGETAG=${TRAVIS_BRANCH}
-fi
-
-if [ "${IMAGETAG}" == "master" ] || [ "${IMAGETAG}" == "develop" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    docker-compose build --pull
-    docker login -u $DOCKER_USER -p $DOCKER_PASS
-
-    echo "Tagging image as $IMAGETAG"
-    docker tag seedplatform/seed seedplatform/seed:$IMAGETAG
-    docker push seedplatform/seed:$IMAGETAG
 elif [ "${IMAGETAG}" != "skip" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     docker-compose build --pull
     docker login -u $DOCKER_USER -p $DOCKER_PASS
@@ -24,7 +13,6 @@ elif [ "${IMAGETAG}" != "skip" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; the
     echo "Tagging PR image as $IMAGETAG"
     docker tag seedplatform/seed seedplatform/seed:$IMAGETAG
     docker push seedplatform/seed:$IMAGETAG
-
 else
     echo "Not on a deployable branch or a valid pull request"
 fi
