a01e4bddacb31a8db701ed14a2fe522be638a16a
Adrian Lara <adrian@devetry.com>
2020-11-04 12:36:13 -0500

    feat(analysis): create model and migration

--- seed/models/__init__.py
+++ seed/models/__init__.py
@@ -7,6 +7,7 @@
 """
 
 # Import all the models in this folder
+from .analyses import *  # noqa
 from .cycles import *  # noqa
 from .models import *  # noqa
 from .tax_lot_properties import *  # noqa
