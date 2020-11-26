63bcfb94b43ded004c00d2ea7e15a9da4cbc14a0
Ryo <kyle.schulz@devetry.com>
2020-11-25 18:00:41 -0700

    feat: added related analysis serializers

---
+++ seed/serializers/analysis_input_files.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+# encoding: utf-8
+"""
+:copyright (c) 2014 - 2020, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.  # NOQA
+:author
+"""
+from rest_framework import serializers
+
+from seed.models import AnalysisInputFile
+
+class AnalysisInputFileSerializer(serializers.ModelSerializer):
+    content_type = serializers.SerializerMethodField('get_readable_content_type')
+
+    class Meta:
+        model = AnalysisInputFile
+        fields = '__all__'
+
+    def get_readable_content_type(self, obj):
+        return AnalysisInputFile.CONTENT_TYPES[next((i for i, v in enumerate(AnalysisInputFile.CONTENT_TYPES) if v[0] == obj.content_type), None)][1]
