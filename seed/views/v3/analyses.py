13e9c6f5d7576126364c2861ccbbfeb2d1ade582
Ryo <kyle.schulz@devetry.com>
2020-12-10 18:00:27 -0700

    feat: initial endpoint for analysis property views

--- seed/views/v3/analyses.py
+++ seed/views/v3/analyses.py
@@ -7,12 +7,14 @@
 from drf_yasg.utils import swagger_auto_schema
 from django.http import JsonResponse
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.status import HTTP_409_CONFLICT
 
 from seed.decorators import ajax_request_class, require_organization_id_class
 from seed.lib.superperms.orgs.decorators import has_perm_class
-from seed.models import Analysis
+from seed.models import Analysis, AnalysisPropertyView
 from seed.serializers.analyses import AnalysisSerializer
+from seed.serializers.analysis_property_views import AnalysisPropertyViewSerializer
 from seed.utils.api import api_endpoint_class
 from seed.utils.api_schema import AutoSchemaHelper
 
@@ -71,4 +73,58 @@ class AnalysisViewSet(viewsets.ViewSet):
         return JsonResponse({
             'status': 'success',
             'analysis': serialized_analysis
+        })
+
+class AnalysisPropertyViewViewSet(viewsets.ViewSet):
+    serializer_class = AnalysisSerializer
+    model = Analysis
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            AutoSchemaHelper.query_org_id_field(True),
+            AutoSchemaHelper.query_integer_field('property_id', False, 'Property ID')
+        ]
+    )
+    @require_organization_id_class
+    @api_endpoint_class
+    @ajax_request_class
+    @has_perm_class('requires_member')
+    def list(self, request, analysis_pk):
+        organization_id = int(request.query_params.get('organization_id', 0))
+        try:
+            views_queryset = AnalysisPropertyView.objects.filter(analysis=analysis_pk, analysis__organization_id=organization_id)
+        except AnalysisPropertyView.DoesNotExist:
+            return JsonResponse({
+                'status': 'error',
+                'message': "Requested analysis doesn't exist in this organization."
+            }, status=HTTP_409_CONFLICT)
+        views = []
+        for view in views_queryset:
+            serialized_view = AnalysisPropertyViewSerializer(view).data
+            views.append(serialized_view)
+
+        return JsonResponse({
+            'status': 'success',
+            'views': views
+        })
+
+    @swagger_auto_schema(manual_parameters=[AutoSchemaHelper.query_org_id_field(True)])
+    @require_organization_id_class
+    @api_endpoint_class
+    @ajax_request_class
+    @has_perm_class('requires_member')
+    def retrieve(self, request, analysis_pk, pk):
+        organization_id = int(request.query_params.get('organization_id', 0))
+        try:
+            view = AnalysisPropertyView.objects.get(id=pk, analysis=analysis_pk, analysis__organization_id=organization_id)
+        except AnalysisPropertyView.DoesNotExist:
+            return JsonResponse({
+                'status': 'error',
+                'message': "Requested analysis property view doesn't exist in this organization and/or analysis."
+            }, status=HTTP_409_CONFLICT)
+        serialized_view = AnalysisPropertyViewSerializer(view).data
+
+        return JsonResponse({
+            'status': 'success',
+            'view': serialized_view
         })
