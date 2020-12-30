c3355fd3086cfdd7399ef51f4c9cd0a5655f55ac
Ted Summer <ted.summer2@gmail.com>
2020-12-30 14:28:21 -0500

    feat(views): add analysis stop endpoint

--- seed/views/v3/analyses.py
+++ seed/views/v3/analyses.py
@@ -163,3 +163,24 @@ class AnalysisViewSet(viewsets.ViewSet):
                 'status': 'error',
                 'message': str(e)
             }, status=HTTP_409_CONFLICT)
+
+    @swagger_auto_schema(manual_parameters=[AutoSchemaHelper.query_org_id_field()])
+    @require_organization_id_class
+    @api_endpoint_class
+    @ajax_request_class
+    @has_perm_class('requires_member')
+    @action(detail=True, methods=['post'])
+    def stop(self, request, pk):
+        organization_id = int(request.query_params.get('organization_id', 0))
+        try:
+            analysis = Analysis.objects.get(id=pk, organization_id=organization_id)
+            pipeline = AnalysisPipeline.factory(analysis)
+            pipeline.stop()
+            return JsonResponse({
+                'status': 'success',
+            })
+        except Analysis.DoesNotExist:
+            return JsonResponse({
+                'status': 'error',
+                'message': 'Requested analysis doesn\'t exist in this organization.'
+            }, status=HTTP_409_CONFLICT)
