efdbefe21dc8fb11148f928bffa3bb10020676ab
Ryo <kyle.schulz@devetry.com>
2021-01-26 16:20:12 -0700

    feat: added property_display_field to org model

--- seed/views/v3/organizations.py
+++ seed/views/v3/organizations.py
@@ -107,6 +107,7 @@ def _dict_org(request, organizations):
             'created': o.created.strftime('%Y-%m-%d') if o.created else '',
             'mapquest_api_key': o.mapquest_api_key or '',
             'geocoding_enabled': o.geocoding_enabled,
+            'property_display_field': o.property_display_field,
             'display_meter_units': o.display_meter_units,
             'thermal_conversion_assumption': o.thermal_conversion_assumption,
             'comstock_enabled': o.comstock_enabled,
@@ -515,6 +516,11 @@ class OrganizationViewSet(viewsets.ViewSet):
         geocoding_enabled = posted_org.get('geocoding_enabled', True)
         if geocoding_enabled != org.geocoding_enabled:
             org.geocoding_enabled = geocoding_enabled
+
+        # Update property_display_field option
+        property_display_field = posted_org.get('property_display_field', True)
+        if property_display_field != org.property_display_field:
+            org.property_display_field = property_display_field
 
         comstock_enabled = posted_org.get('comstock_enabled', False)
         if comstock_enabled != org.comstock_enabled:
@@ -1310,3 +1316,14 @@ class OrganizationViewSet(viewsets.ViewSet):
         org = Organization.objects.get(id=pk)
 
         return org.geocoding_enabled
+
+    @has_perm_class('requires_member')
+    @ajax_request_class
+    @action(detail=True, methods=['GET'])
+    def property_display_field(self, request, pk=None):
+        """
+        Returns the organization's property_display_field setting
+        """
+        org = Organization.objects.get(id=pk)
+
+        return org.property_display_field
