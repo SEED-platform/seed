d06ca04e5fe533c8904ab9ef8ec6316de7846577
Alex Swindler <Alex.Swindler@nrel.gov>
2021-03-16 03:01:56 -0600

    style: add tz to maintenance timestamp

--- docker/maintenance.sh
+++ docker/maintenance.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == 'on' ]
 then
-  sed -e 's|<small></small>|<small>'"$(date '+%-m/%-d %H:%M')"'</small>|g' /seed/docker/maintenance.html > /seed/collected_static/maintenance.html
+  sed -e 's|<small></small>|<small>'"$(date '+%-m/%-d %-H:%M %Z')"'</small>|g' /seed/docker/maintenance.html > /seed/collected_static/maintenance.html
   echo '...maintenance is on...';
 elif [ "$1" == 'off' ]
 then
