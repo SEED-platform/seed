78f354e5f06560f10db28aef0db6221d63104a03
Ted Summer <ted.summer2@gmail.com>
2021-05-25 10:00:05 -0400

    refactor: move serializer CustomChoicesField to utils

---
+++ seed/serializers/utils.py
@@ -0,0 +1,26 @@
+# !/usr/bin/env python
+# encoding: utf-8
+"""
+:copyright (c) 2014 - 2021, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.  # NOQA
+:author
+"""
+from rest_framework import serializers
+
+
+class CustomChoicesField(serializers.ChoiceField):
+    """Use this choices field when a model uses choices fields and you'd like
+    the display value to be serialized rather than the coded value
+    """
+    def to_representation(self, obj):
+        if obj == '' and self.allow_blank:
+            return obj
+        return self._choices[obj]
+
+    def to_internal_value(self, data):
+        if data == '' and self.allow_blank:
+            return ''
+
+        for key, val in self._choices.items():
+            if val == data:
+                return key
+        self.fail('invalid_choice', input=data)
