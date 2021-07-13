6a90e295c1757ff8eac2b014b19a2de4b9ae1305
Ted Summer <ted.summer2@gmail.com>
2021-07-13 15:08:37 -0600

    test: use assertRaisesRegex
    assertRaisesRegexp is deprecated

--- seed/tests/test_columns.py
+++ seed/tests/test_columns.py
@@ -200,7 +200,7 @@ class TestColumns(TestCase):
 
     def test_save_column_mapping_by_file_exception(self):
         self.mapping_import_file = os.path.abspath("./no-file.csv")
-        with self.assertRaisesRegexp(Exception, "Mapping file does not exist: .*/no-file.csv"):
+        with self.assertRaisesRegex(Exception, "Mapping file does not exist: .*/no-file.csv"):
             Column.create_mappings_from_file(self.mapping_import_file, self.fake_org,
                                              self.fake_user)
 
@@ -1012,7 +1012,7 @@ class TestColumnsByInventory(TestCase):
             is_extra_data=True
         )
 
-        with self.assertRaisesRegexp(Exception, 'Duplicate name'):
+        with self.assertRaisesRegex(Exception, 'Duplicate name'):
             Column.retrieve_all(self.fake_org.pk, 'property', False)
 
     def test_column_retrieve_schema(self):
