bf3a41e0cbff39ce1c85fd3c3a52ce4986f859f0
Ted Summer <ted.summer2@gmail.com>
2021-07-28 19:49:24 -0600

    test: fix analysis pipeline test

--- seed/tests/test_analysis_pipelines.py
+++ seed/tests/test_analysis_pipelines.py
@@ -46,7 +46,7 @@ from seed.analysis_pipelines.pipeline import (
     task_create_analysis_property_views,
     analysis_pipeline_task
 )
-from seed.analysis_pipelines.better import _build_better_input
+from seed.analysis_pipelines.better.buildingsync import _build_better_input
 from seed.analysis_pipelines.bsyncr import _build_bsyncr_input, BsyncrPipeline, _parse_analysis_property_view_id, \
     PREMISES_ID_NAME
 from seed.building_sync.building_sync import BuildingSync
