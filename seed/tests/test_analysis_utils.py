b0120098357c1907fa5357b00b29056f60ec779d
Ted Summer <ted.summer2@gmail.com>
2021-10-07 09:32:22 -0600

    test: add tests for calendarization functions

--- seed/tests/test_analysis_utils.py
+++ seed/tests/test_analysis_utils.py
@@ -12,6 +12,8 @@ from seed.analysis_pipelines.utils import (
     SimpleMeterReading,
     _split_reading,
     interpolate_monthly_readings,
+    calendarize_meter_readings,
+    calendarize_and_extrapolate_meter_readings,
 )
 
 
@@ -108,6 +110,256 @@ class TestAnalysisUtils(TestCase):
         ]
 
         self.assertListEqual(expected, readings)
+
+    def test_split_reading_doesnt_return_extra_reading_when_reading_ends_on_month_start(self):
+        # This test that the function treats end dates as non-inclusive
+        # E.g. If a reading ends on February 1 at midnight, it will not return
+        # an additional reading for the month of February-March
+        # -- Setup
+        reading = SimpleMeterReading(
+            dt(2021, 1, 1),
+            dt(2021, 2, 1),  # This is February 1 at midnight
+            100
+        )
+
+        # -- Act
+        readings = _split_reading(reading)
+
+        # -- Assert
+        self.assertListEqual([reading], readings)
+
+    def test_split_reading_works_when_snap_is_disabled(self):
+        # -- Setup
+        # this reading starts in January, covers February, and ends in March
+        # More specifically, this reading includes:
+        # January:  17 days (~28%)
+        # February: 28 days (~47%)
+        # March:    15 days (~24%)
+        # Total:    60 days (100%)
+        original_start = dt(2021, 1, 15)
+        original_end = dt(2021, 3, 15)
+        reading = SimpleMeterReading(
+            original_start,
+            original_end,
+            100
+        )
+
+        # -- Act
+        # NOTE: we set snap to false, we expect the first and last readings to
+        # keep their original start and end (respectrively)
+        readings = _split_reading(reading, snap_intervals=False)
+
+        # -- Assert
+        expected = [
+            SimpleMeterReading(
+                original_start,  # start_time should be the same!
+                dt(2021, 2, 1),
+                28.8135593220339
+            ),
+            SimpleMeterReading(
+                dt(2021, 2, 1),
+                dt(2021, 3, 1),
+                47.45762711864407
+            ),
+            SimpleMeterReading(
+                dt(2021, 3, 1),
+                original_end,  # end_time should be the same!
+                23.728813559322035
+            )
+        ]
+
+        self.assertListEqual(expected, readings)
+
+    def test_calendarize_meter_readings_works_when_readings_already_calendarized(self):
+        # -- Setup
+        jan, feb, mar, apr = dt(2021, 1, 1), dt(2021, 2, 1), dt(2021, 3, 1), dt(2021, 4, 1)
+        original_readings = [
+            SimpleMeterReading(jan, feb, 1),
+            SimpleMeterReading(feb, mar, 2),
+            SimpleMeterReading(mar, apr, 3),
+        ]
+
+        # -- Act
+        result = calendarize_meter_readings(original_readings)
+
+        # -- Assert
+        # result should be the same as original readings
+        self.assertListEqual(original_readings, result)
+
+    def test_calendarize_meter_readings_works_when_readings_arent_sorted(self):
+        # -- Setup
+        jan, feb, mar, apr = dt(2021, 1, 1), dt(2021, 2, 1), dt(2021, 3, 1), dt(2021, 4, 1)
+        original_readings = [
+            SimpleMeterReading(mar, apr, 3),
+            SimpleMeterReading(jan, feb, 1),
+            SimpleMeterReading(feb, mar, 2),
+        ]
+
+        # -- Act
+        result = calendarize_meter_readings(original_readings)
+
+        # -- Assert
+        expected = [
+            SimpleMeterReading(jan, feb, 1),
+            SimpleMeterReading(feb, mar, 2),
+            SimpleMeterReading(mar, apr, 3),
+        ]
+        self.assertListEqual(expected, result)
+
+    def test_calendarize_meter_readings_works_when_readings_overlap(self):
+        # -- Setup
+        jan, feb, mar, apr = dt(2021, 1, 1), dt(2021, 2, 1), dt(2021, 3, 1), dt(2021, 4, 1)
+        original_readings = [
+            SimpleMeterReading(jan, feb, 1),
+            SimpleMeterReading(feb, mar, 2),
+            SimpleMeterReading(mar, apr, 3),
+            # overlapping months
+            SimpleMeterReading(jan, feb, 1),
+            SimpleMeterReading(feb, mar, 2),
+            SimpleMeterReading(mar, apr, 3),
+        ]
+
+        # -- Act
+        result = calendarize_meter_readings(original_readings)
+
+        # -- Assert
+        expected = [
+            SimpleMeterReading(jan, feb, 2),
+            SimpleMeterReading(feb, mar, 4),
+            SimpleMeterReading(mar, apr, 6),
+        ]
+        self.assertListEqual(expected, result)
+
+    def test_calendarize_and_extrapolate_meter_readings_correctly_extrapolates(self):
+        # -- Setup
+        # There are 28 days in February, so 14 days is 50% of the month
+        original_readings = [
+            SimpleMeterReading(
+                dt(2021, 2, 1),
+                dt(2021, 2, 15),
+                1
+            ),
+        ]
+
+        # -- Act
+        result = calendarize_and_extrapolate_meter_readings(original_readings)
+
+        # -- Assert
+        expected = [
+            SimpleMeterReading(
+                dt(2021, 2, 1),
+                dt(2021, 3, 1),
+                2,  # since the original reading covered 1/2 of month, we'd expect the month total to be doubled
+            )
+        ]
+        self.assertListEqual(expected, result)
+
+    def test_calendarize_and_extrapolate_meter_readings_works_when_readings_are_consecutive(self):
+        # -- Setup
+        # These readings cover the full month of February
+        original_readings = [
+            SimpleMeterReading(
+                dt(2021, 2, 1),
+                dt(2021, 2, 15),
+                1
+            ),
+            SimpleMeterReading(
+                dt(2021, 2, 15),
+                dt(2021, 3, 1),
+                1
+            ),
+        ]
+
+        # -- Act
+        result = calendarize_and_extrapolate_meter_readings(original_readings)
+
+        # -- Assert
+        expected = [
+            SimpleMeterReading(
+                dt(2021, 2, 1),
+                dt(2021, 3, 1),
+                2,  # since the original reading covered 1/2 of month, we'd expect the month total to be doubled
+            )
+        ]
+        self.assertListEqual(expected, result)
+
+    def test_calendarize_and_extrapolate_meter_readings_filters_months_below_coverage_threshold(self):
+        # -- Setup
+        original_readings = [
+            # January readings only cover the 1st through the 6th
+            # This is ~19% of the month (6 / 31)
+            SimpleMeterReading(
+                dt(2021, 1, 1),
+                dt(2021, 1, 3),
+                1
+            ),
+            SimpleMeterReading(
+                dt(2021, 1, 3),
+                dt(2021, 1, 5),
+                1
+            ),
+            SimpleMeterReading(
+                dt(2021, 1, 5),
+                dt(2021, 1, 7),
+                1
+            ),
+            # February Readings cover the 1st through the 14th
+            # This is 50% of the month (14 / 28)
+            # Note that the _number_ of readings shouldn't matter -- it's about
+            # monthly _coverage_
+            SimpleMeterReading(
+                dt(2021, 2, 1),
+                dt(2021, 2, 3),
+                1
+            ),
+            SimpleMeterReading(
+                dt(2021, 2, 3),
+                dt(2021, 2, 8),
+                1
+            ),
+            SimpleMeterReading(
+                dt(2021, 2, 8),
+                dt(2021, 2, 12),
+                1
+            ),
+            SimpleMeterReading(
+                dt(2021, 2, 12),
+                dt(2021, 2, 15),
+                1
+            ),            
+        ]
+
+        # -- Act
+        coverage_threshold = 0.5
+        result = calendarize_and_extrapolate_meter_readings(original_readings, coverage_threshold)
+
+        # -- Assert
+        expected = [
+            # Note that January isn't included here b/c the coverage for that month
+            # is below our given threshold
+            SimpleMeterReading(
+                dt(2021, 2, 1),
+                dt(2021, 3, 1),
+                8,  # original total for Feb was 4; since original coverage was 50%, double that is 8
+            )
+        ]
+        self.assertListEqual(expected, result)
+
+    def test_calendarize_and_extrapolate_meter_readings_extrapolates_readings_when_months_are_incomplete(self):
+        # -- Setup
+        jan, feb, mar, apr = dt(2021, 1, 1), dt(2021, 2, 1), dt(2021, 3, 1), dt(2021, 4, 1)
+        original_readings = [
+            SimpleMeterReading(jan, feb, 1),
+            SimpleMeterReading(feb, mar, 2),
+            SimpleMeterReading(mar, apr, 3),
+        ]
+
+        # -- Act
+        result = calendarize_and_extrapolate_meter_readings(original_readings)
+
+        # -- Assert
+        # result should be the same as original readings
+        self.assertListEqual(original_readings, result)
 
     def test_interpolate_works_when_one_month_missing(self):
         # -- Setup
