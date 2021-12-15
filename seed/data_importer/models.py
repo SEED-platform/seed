bb1b2203c58ff2eef9cc352f2e94155d3eb049e6
Ted Summer <ted.summer2@gmail.com>
2021-12-15 12:29:51 -0700

    refactor(ImportRecord): remove unused property method

--- seed/data_importer/models.py
+++ seed/data_importer/models.py
@@ -534,11 +534,6 @@ class ImportRecord(NotDeletableModel):
     def SUMMARY_ANALYSIS_QUEUED_KEY(cls, pk):
         return 'SUMMARY_ANALYSIS_QUEUED%s' % pk
 
-    @property
-    def form(self, data=None):
-        from seed.data_importer import ImportRecordForm
-        return ImportRecordForm(data, instance=self)
-
     def prefixed_pk(self, pk, max_len_before_prefix=(SOURCE_FACILITY_ID_MAX_LEN - len('IMP1234-'))):
         """This is a total hack to support prefixing until source_facility_id
         is turned into a proper pk.  Prefixes a given pk with the import_record"""
