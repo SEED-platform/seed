34358459669aa2f138672f51295c9584237dc72c
haneslinger <heslinge@nrel.gov>
2021-12-28 12:12:39 -0600

    Format

--- seed/landing/urls.py
+++ seed/landing/urls.py
@@ -56,6 +56,5 @@ if settings.INCLUDE_ACCT_REG:
     urlpatterns += [
         re_path(r'^accounts/create/$', create_account, name='create_account'),
         re_path(r'^account_activation_sent/$', account_activation_sent, name='account_activation_sent'),
-        re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            activate, name='activate')
+        re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', activate, name='activate')
     ]
