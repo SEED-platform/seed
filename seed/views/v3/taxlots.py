476682d83b0081112facc312ce1f25015481864e
Ross Perry <perryr16@gmail.com>
2022-01-12 15:18:53 -0700

    refactor: move components to util

--- seed/views/v3/taxlots.py
+++ seed/views/v3/taxlots.py
@@ -37,6 +37,7 @@ from seed.utils.properties import (get_changed_fields,
                                    pair_unpair_property_taxlot,
                                    update_result_with_master)
 from seed.utils.taxlots import taxlots_across_cycles
+from seed.utils.filter_state import _get_filtered_results
 
 ErrorState = namedtuple('ErrorState', ['status_code', 'message'])
 
@@ -68,124 +69,6 @@ class TaxlotViewSet(viewsets.ViewSet, OrgMixin, ProfileIdMixin):
         super_organization = self.get_organization(request)
         # TODO: refactor to avoid passing request here
         return get_labels(request, labels, super_organization, 'taxlot_view')
-
-    def _get_filtered_results(self, request, profile_id):
-        page = request.query_params.get('page', 1)
-        per_page = request.query_params.get('per_page', 1)
-        org_id = self.get_organization(request)
-        cycle_id = request.query_params.get('cycle')
-        # check if there is a query paramater for the profile_id. If so, then use that one
-        profile_id = request.query_params.get('profile_id', profile_id)
-        if not org_id:
-            return JsonResponse(
-                {'status': 'error', 'message': 'Need to pass organization_id as query parameter'},
-                status=status.HTTP_400_BAD_REQUEST)
-        org = Organization.objects.get(id=org_id)
-
-        if cycle_id:
-            cycle = Cycle.objects.get(organization_id=org_id, pk=cycle_id)
-        else:
-            cycle = Cycle.objects.filter(organization_id=org_id).order_by('name')
-            if cycle:
-                cycle = cycle.first()
-            else:
-                return JsonResponse({
-                    'status': 'error',
-                    'message': 'Could not locate cycle',
-                    'pagination': {
-                        'total': 0
-                    },
-                    'cycle_id': None,
-                    'results': []
-                })
-
-        taxlot_views_list = (
-            TaxLotView.objects.select_related('taxlot', 'state', 'cycle')
-            .filter(taxlot__organization_id=org_id, cycle=cycle)
-        )
-
-        # Retrieve all the columns that are in the db for this organization
-        columns_from_database = Column.retrieve_all(org_id, 'taxlot', False)
-        try:
-            filters, order_by = build_view_filters_and_sorts(request.query_params, columns_from_database)
-        except FilterException as e:
-            return JsonResponse(
-                {
-                    'status': 'error',
-                    'message': f'Error filtering: {str(e)}'
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        taxlot_views_list = taxlot_views_list.filter(filters).order_by(*order_by)
-
-        # Return taxlot views limited to the 'taxlot_view_ids' list.  Otherwise, if selected is empty, return all
-        if 'taxlot_view_ids' in request.data and request.data['taxlot_view_ids']:
-            taxlot_views_list = taxlot_views_list.filter(id__in=request.data['taxlot_view_ids'])
-
-        paginator = Paginator(taxlot_views_list, per_page)
-
-        try:
-            taxlot_views = paginator.page(page)
-            page = int(page)
-        except PageNotAnInteger:
-            taxlot_views = paginator.page(1)
-            page = 1
-        except EmptyPage:
-            taxlot_views = paginator.page(paginator.num_pages)
-            page = paginator.num_pages
-
-        # This uses an old method of returning the show_columns. There is a new method that
-        # is preferred in v2.1 API with the ProfileIdMixin.
-        if profile_id is None:
-            show_columns = None
-        elif profile_id == -1:
-            show_columns = list(Column.objects.filter(
-                organization_id=org_id
-            ).values_list('id', flat=True))
-        else:
-            try:
-                profile = ColumnListProfile.objects.get(
-                    organization=org,
-                    id=profile_id,
-                    profile_location=VIEW_LIST,
-                    inventory_type=VIEW_LIST_TAXLOT
-                )
-                show_columns = list(ColumnListProfileColumn.objects.filter(
-                    column_list_profile_id=profile.id
-                ).values_list('column_id', flat=True))
-            except ColumnListProfile.DoesNotExist:
-                show_columns = None
-
-        include_related = (
-            str(request.query_params.get('include_related', 'true')).lower() == 'true'
-        )
-        related_results = TaxLotProperty.serialize(
-            taxlot_views,
-            show_columns,
-            columns_from_database,
-            include_related,
-        )
-
-        # collapse units here so we're only doing the last page; we're already a
-        # realized list by now and not a lazy queryset
-        unit_collapsed_results = [apply_display_unit_preferences(org, x) for x in related_results]
-
-        response = {
-            'pagination': {
-                'page': page,
-                'start': paginator.page(page).start_index(),
-                'end': paginator.page(page).end_index(),
-                'num_pages': paginator.num_pages,
-                'has_next': paginator.page(page).has_next(),
-                'has_previous': paginator.page(page).has_previous(),
-                'total': paginator.count
-            },
-            'cycle_id': cycle.id,
-            'results': unit_collapsed_results
-        }
-
-        return JsonResponse(response)
 
     @swagger_auto_schema(
         manual_parameters=[
@@ -224,7 +107,7 @@ class TaxlotViewSet(viewsets.ViewSet, OrgMixin, ProfileIdMixin):
         """
         List all the properties
         """
-        return self._get_filtered_results(request, profile_id=-1)
+        return _get_filtered_results(request, 'taxlot', profile_id=-1)
 
     @swagger_auto_schema(
         request_body=AutoSchemaHelper.schema_factory(
@@ -315,7 +198,7 @@ class TaxlotViewSet(viewsets.ViewSet, OrgMixin, ProfileIdMixin):
             else:
                 profile_id = request.data['profile_id']
 
-        return self._get_filtered_results(request, profile_id=profile_id)
+        return _get_filtered_results(request, 'taxlot', profile_id=profile_id)
 
     @swagger_auto_schema(
         manual_parameters=[
