291c5fec7e72181f5bfcf41b96de3ed1df458bdc
Ross Perry <perryr16@gmail.com>
2022-01-24 12:31:12 -0700

    refactor: remove unused config setting

--- seed/celery.py
+++ seed/celery.py
@@ -33,7 +33,6 @@ class Celery(celery.Celery):
 
 app = Celery('seed',
              backend='redis://db-redis:6379/',
-             broker='redis://'
              )
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.SEED_CORE_APPS + ('seed.analysis_pipelines',))
