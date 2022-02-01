d75d3186c3ba236491c7605f043dc2f45d2926eb
Nicholas Long <1907354+nllong@users.noreply.github.com>
2022-01-31 20:10:29 -0700

    Update seed/data_importer/managers.py
    Co-authored-by: Ted Summer <ted.summer2@gmail.com>

--- seed/data_importer/managers.py
+++ seed/data_importer/managers.py
@@ -14,5 +14,6 @@ class NotDeletedManager(models.Manager):
         return super().get_queryset(*args, **kwargs).exclude(deleted=True)
 
     def get_all(self, *args, **kwargs):
-        """method to actually return all fields so we can do database/filesystem cleanup"""
+        """Method to return ALL ImportFiles, including the ones where `deleted == True` which are normally excluded.
+        This is used for database/filesystem cleanup."""
         return super().get_queryset(*args, **kwargs)
