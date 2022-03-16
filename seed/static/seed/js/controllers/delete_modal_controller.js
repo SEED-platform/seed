0af44dd7ec0b187c148bc57021fadcf482fae71b
Ryo <kyle.schulz@devetry.com>
2022-03-15 20:31:54 -0600

    feat: delete analyses when property deleted

--- seed/static/seed/js/controllers/delete_modal_controller.js
+++ seed/static/seed/js/controllers/delete_modal_controller.js
@@ -8,12 +8,34 @@ angular.module('BE.seed.controller.delete_modal', [])
     '$q',
     '$uibModalInstance',
     'inventory_service',
+    'analyses_service',
     'property_view_ids',
     'taxlot_view_ids',
-    function ($scope, $q, $uibModalInstance, inventory_service, property_view_ids, taxlot_view_ids) {
+    function ($scope, $q, $uibModalInstance, inventory_service, analyses_service, property_view_ids, taxlot_view_ids) {
       $scope.property_view_ids = _.uniq(property_view_ids);
       $scope.taxlot_view_ids = _.uniq(taxlot_view_ids);
       $scope.delete_state = 'delete';
+      $scope.delete_analyses = true;
+      $scope.delete_batch_analyses = false;
+
+
+      let analysis_ids = [];
+      let batch_analysis_ids = [];
+      for (i in $scope.property_view_ids) {
+        inventory_service.get_property($scope.property_view_ids[i]).then(function (results) {
+          analyses_service.get_analyses_for_canonical_property(results.property.id).then(function (results) {
+            for (i in results.analyses) {
+              if (results.analyses[i].number_of_analysis_property_views > 1) {
+                batch_analysis_ids.push(results.analyses[i].id);
+              } else {
+                analysis_ids.push(results.analyses[i].id);
+              }
+            }
+            $scope.analysis_ids = _.uniq(analysis_ids);
+            $scope.batch_analysis_ids = _.uniq(batch_analysis_ids);
+          });
+        });
+      }
 
       $scope.delete_inventory = function () {
         $scope.delete_state = 'prepare';
@@ -22,6 +44,8 @@ angular.module('BE.seed.controller.delete_modal', [])
 
         if ($scope.property_view_ids.length) promises.push(inventory_service.delete_property_states($scope.property_view_ids));
         if ($scope.taxlot_view_ids.length) promises.push(inventory_service.delete_taxlot_states($scope.taxlot_view_ids));
+        if ($scope.delete_analyses) for (i in $scope.analysis_ids) promises.push(analyses_service.delete_analysis($scope.analysis_ids[i]));
+        if ($scope.delete_batch_analyses) for (i in $scope.batch_analysis_ids) promises.push(analyses_service.delete_analysis($scope.batch_analysis_ids[i]));
 
         return $q.all(promises).then(function (results) {
           $scope.deletedProperties = 0;
