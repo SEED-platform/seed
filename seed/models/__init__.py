3b938bab2ce65085ec69cc2322c61a710337cb9d
Nicholas Long <nicholas.long@nrel.gov>
2022-05-08 10:01:55 -0600

    do not sort models init

--- seed/models/__init__.py
+++ seed/models/__init__.py
@@ -5,37 +5,45 @@
 :copyright (c) 2014 - 2022, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.
 :author
 """
+# Do not isort these since order is important
+# The precommit call has a skip for this file.
 
 # Import all the models in this folder
 from .analyses import *  # noqa
-from .analysis_input_files import *  # noqa
-from .analysis_messages import *  # noqa
-from .analysis_output_files import *  # noqa
-from .analysis_property_views import *  # noqa
+from .cycles import *  # noqa
+from .derived_columns import *  # noqa
+from .models import *  # noqa
+from .tax_lot_properties import *  # noqa
+from .properties import *  # noqa
+from .tax_lots import *  # noqa
+from .columns import *  # noqa
+from .column_mappings import *  # noqa
+from .column_mapping_profiles import *  # noqa
+from .column_list_profiles import *  # noqa
+from .column_list_profile_columns import *  # noqa
 from .auditlog import *  # noqa
+from .measures import *  # noqa
+from .scenarios import *  # noqa
+from .meters import *  # noqa
+from .sensors import *  # noqa
+from .simulations import *  # noqa
 from .building_file import *  # noqa
-from .certification import (  # noqa
+from .inventory_document import *  # noqa
+from .notes import *  # noqa
+from .analysis_property_views import *  # noqa
+from .analysis_input_files import *  # noqa
+from .analysis_output_files import *  # noqa
+from .analysis_messages import *  # noqa
+from .postoffice import *  # noqa
+
+from .certification import (    # noqa
     GreenAssessment,
     GreenAssessmentProperty,
     GreenAssessmentURL
 )
-from .column_list_profile_columns import *  # noqa
-from .column_list_profiles import *  # noqa
-from .column_mapping_profiles import *  # noqa
-from .column_mappings import *  # noqa
-from .columns import *  # noqa
-from .cycles import *  # noqa
-from .derived_columns import *  # noqa
-from .inventory_document import *  # noqa
-from .measures import *  # noqa
-from .meters import *  # noqa
-from .models import *  # noqa
-from .notes import *  # noqa
-from .postoffice import *  # noqa
-from .projects import Project, ProjectPropertyView, ProjectTaxLotView  # noqa
-from .properties import *  # noqa
-from .scenarios import *  # noqa
-from .sensors import *  # noqa
-from .simulations import *  # noqa
-from .tax_lot_properties import *  # noqa
-from .tax_lots import *  # noqa
+
+from .projects import (     # noqa
+    Project,
+    ProjectPropertyView,
+    ProjectTaxLotView,
+)
