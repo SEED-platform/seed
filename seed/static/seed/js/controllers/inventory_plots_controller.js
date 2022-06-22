ee313c35db5ead0e22d28b814daf279c619c8b7c
Hannah Eslinger <heslinge@nrel.gov>
2022-06-21 18:02:06 -0600

    WIP

--- seed/static/seed/js/controllers/inventory_plots_controller.js
+++ seed/static/seed/js/controllers/inventory_plots_controller.js
@@ -65,46 +65,127 @@ angular.module('BE.seed.controller.inventory_plots', [])
       $scope.inventory_type = $stateParams.inventory_type;
       var lastCycleId = inventory_service.get_last_cycle();
       $scope.cycle = {
-        selected_cycle: _.find(cycles.cycles, {id: lastCycleId}) || _.first(cycles.cycles),
+        selected_cycle: _.find(cycles.cycles, { id: lastCycleId }) || _.first(cycles.cycles),
         cycles: cycles.cycles
       };
 
-      var canvas = document.getElementById("myChart");
-      var ctx = canvas.getContext("2d");
-      var myChart = new Chart(ctx, {
-        type: 'scatter',
-        data: {
-          datasets: [{
-            data: [],
-            backgroundColor: "rgba(0,255,255,0.7)",
-          }],
-        },
-        options: {
-          plugins: {
-            tooltip: {
-              callbacks: {
-                label: function (ctx) {
-                  let label = ctx.dataset.labels[ctx.dataIndex];
-                  label += " (" + ctx.parsed.x + ", " + ctx.parsed.y + ")";
-                  return label
+      var createChart = function (elementId, xAxisKey, yAxisKey, onHover) {
+        var canvas = document.getElementById(elementId);
+        var ctx = canvas.getContext("2d");
+
+        return new Chart(ctx, {
+          type: 'scatter',
+          data: {
+            datasets: [{
+              data: [],
+              backgroundColor: "cyan",
+              hoverBackgroundColor: 'red',
+              borderColor: "black",
+            }],
+          },
+          options: {
+            scales: {
+              x: {
+                display: true,
+                title: {
+                  display: true,
+                  text: xAxisKey,
+                },
+              },
+              y: {
+                display: true,
+                title: {
+                  display: true,
+                  text: yAxisKey
                 }
               }
-            }
+            },
+            plugins: {
+              zoom: {
+                limits: {
+                  x: { min: 'original', max: 'original', minRange: 50 },
+                  y: { min: 'original', max: 'original', minRange: 50 }
+                },
+                pan: {
+                  enabled: true,
+                  mode: 'xy',
+                },
+                zoom: {
+                  wheel: {
+                    enabled: true,
+                  },
+                  mode: 'xy',
+                },
+              },
+              legend: {
+                display: false,
+              },
+              tooltip: {
+                callbacks: {
+                  label: function (ctx) {
+                    let label = ctx.dataset.labels[ctx.dataIndex];
+                    label += " (" + ctx.parsed.x + ", " + ctx.parsed.y + ")";
+                    return label
+                  }
+                }
+              }
+            },
+            parsing: {
+              xAxisKey: xAxisKey,
+              yAxisKey: yAxisKey
+            },
+            onClick: (evt) => {
+              var activePoints = evt.chart.getActiveElements(evt);
+
+              if (activePoints[0]) {
+                activePoint = $scope.data[activePoints[0]["index"]]
+                window.location.href = '/app/#/' + $scope.inventory_type + '/' + activePoint["id"];
+              }
+            },
+            onHover: (evt) => {
+              var activePoints = evt.chart.getActiveElements(evt);
+              onHover(activePoints);
+            },
           },
-        },
-      });
+        });
+      }
 
-      canvas.onclick = function (evt) {
-        var activePoints = myChart.getActiveElements(evt);
-
+      function hoverOnAllCharts(activePoints) {
         if (activePoints[0]) {
-          activePoint = $scope.data[activePoints[0]["index"]]
-          console.log(activePoint);
-          window.location.href = '/app/#/' + $scope.inventory_type + '/' + activePoint["id"];
+          var index = activePoints[0]["index"]
+          for (const [chartName, chart] of Object.entries(charts)) {
+            chart.setActiveElements([
+              {
+                datasetIndex: 0,
+                index: index,
+              }
+            ])
+            chart.update()
+          }
+        } else {
+          for (const [chartName, chart] of Object.entries(charts)) {
+            chart.setActiveElements([]);
+            chart.update()
+          }
         }
-      };
+      }
 
-      $scope.update_chart = function () {
+      charts = {
+        myChart1: createChart(
+          elementId = "myChart1",
+          xAxisKey = 'site_eui_56',
+          yAxisKey = 'gross_floor_area_36',
+          onHover = hoverOnAllCharts,
+        ),
+        myChart2: createChart(
+          elementId = "myChart2",
+          xAxisKey = 'id',
+          yAxisKey = 'gross_floor_area_36',
+          onHover = hoverOnAllCharts,
+        ),
+      }
+
+      $scope.update_charts = function () {
         spinner_utility.show();
         fetch().then(function (data) {
           if (data.status === 'error') {
@@ -115,26 +196,19 @@ angular.module('BE.seed.controller.inventory_plots', [])
           }
 
           $scope.data = data.results
-          populate_chart(data.results);
+          populate_charts(data.results);
           spinner_utility.hide();
         });
       };
 
-      var populate_chart = function (data) {
-        labels = []
-        chart_data = []
+      var populate_charts = function (data) {
+        labels = data.map(property => property["property_name_22"]);
 
-        data.forEach(property => {
-          chart_data.push({
-            "x": property["total_ghg_emissions_intensity_71"],
-            "y": property["site_eui_56"]
-          })
-          labels.push(property["pm_property_id_1"])
-        });
-
-        myChart.data.datasets[0].data = chart_data
-        myChart.data.datasets[0].labels = labels
-        myChart.update();
+        for (const [chartName, chart] of Object.entries(charts)) {
+          chart.data.datasets[0].data = data
+          chart.data.datasets[0].labels = labels
+          chart.update();
+        }
       }
 
       var fetch = function () {
@@ -149,6 +223,7 @@ angular.module('BE.seed.controller.inventory_plots', [])
           page = 1,
           per_page = undefined,
           cycle = $scope.cycle.selected_cycle,
+          include_related = false
         ).then(function (data) {
           return data;
         });
@@ -157,9 +232,9 @@ angular.module('BE.seed.controller.inventory_plots', [])
       $scope.update_cycle = function (cycle) {
         inventory_service.save_last_cycle(cycle.id);
         $scope.cycle.selected_cycle = cycle;
-        $scope.update_chart();
+        $scope.update_charts();
       };
 
-      $scope.update_chart();
+      $scope.update_charts();
     }
   ]);
