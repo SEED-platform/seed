6612ad3e057cd84b5c4ce740cc4750e0020fa111
Hannah Eslinger <heslinge@nrel.gov>
2022-06-23 13:59:58 -0600

    Create FilterGroup

--- seed/views/v3/filter_group.py
+++ seed/views/v3/filter_group.py
@@ -23,7 +23,6 @@ from seed.models import (
     PropertyView,
     TaxLotView
 )
-from seed.search import build_view_filters_and_sorts
 from seed.utils.api import OrgMixin
 from seed.utils.api_schema import swagger_auto_schema_org_query_param
 
@@ -74,32 +73,3 @@ class FilterGroupViewSet(viewsets.ViewSet, OrgMixin):
             "inventory_type": VIEW_LIST_INVENTORY_TYPE[filter_group.inventory_type][1],
             "query_dict": filter_group.query_dict,
         })
-
-    def get_inventory(filter_group: FilterGroup):
-        inventory_type = filter_group.inventory_type
-        org_id = filter_group.organization.id
-
-        if inventory_type == 'property':
-            views_list = (
-                PropertyView.objects.select_related('property', 'state', 'cycle')
-                .filter(property__organization_id=org_id)
-            )
-        elif inventory_type == 'taxlot':
-            views_list = (
-                TaxLotView.objects.select_related('taxlot', 'state', 'cycle')
-                .filter(taxlot__organization_id=org_id)
-            )
-
-        # Retrieve all the columns that are in the db for this organization
-        columns_from_database = Column.retrieve_all(
-            org_id=org_id,
-            inventory_type=inventory_type,
-            only_used=False,
-        )
-
-        filters, annotations, order_by = build_view_filters_and_sorts(
-            filter_group.get_query_dict(),
-            columns_from_database
-        )
-
-        return views_list.annotate(**annotations).filter(filters).order_by(*order_by)
