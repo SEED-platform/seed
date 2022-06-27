56efa8faa6afb8e3bdf53a30296fb9da48b2ed89
Ryo <kyle.schulz@devetry.com>
2022-06-27 16:57:54 -0600

    fix: added new files

---
+++ seed/static/seed/js/controllers/at_token_modal_controller.js
@@ -0,0 +1,55 @@
+/**
+ * :copyright (c) 2014 - 2022, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.
+ * :author
+ *
+ * Controller for the Audit Template token modal.
+ */
+angular.module('BE.seed.controller.at_token_modal', []).controller('at_token_modal_controller', [
+  '$scope',
+  '$log',
+  '$uibModalInstance',
+  'Notification',
+  'audit_template_service',
+  'organization',
+  function (
+    $scope,
+    $log,
+    $uibModalInstance,
+    Notification,
+    audit_template_service,
+    organization
+  ) {
+    $scope.org = organization;
+    $scope.waiting_for_server = false;
+    $scope.fields = {
+      org_token: $scope.org.at_organization_token,
+      email: null,
+      password: null
+    };
+
+    $scope.submitForm = function (form) {
+      if (form.$invalid) {
+        return;
+      }
+      $scope.waiting_for_server = true;
+
+      return audit_template_service.get_api_token(
+        $scope.org.id, $scope.fields.org_token, $scope.fields.email, $scope.fields.password
+      ).then(result => {
+        if (result.success) {
+          Notification.primary('Audit Template API Token generated.');
+          $uibModalInstance.close(result.data);
+        } else {
+          Notification.error('Failed to generate Audit Template API Token: ' + result.message);
+          $log.error('Failed to generate Audit Template API Token.', result.message);
+        }
+        $scope.waiting_for_server = false;
+      });
+    };
+
+    $scope.cancel = function () {
+      //don't do anything, just close modal.
+      $uibModalInstance.dismiss('cancel');
+    };
+
+  }]);
---
+++ seed/static/seed/js/services/audit_template_service.js
@@ -0,0 +1,26 @@
+angular.module('BE.seed.service.audit_template', [])
+  .factory('audit_template_service', [
+    '$http',
+    '$log',
+    function (
+      $http,
+      $log
+    ) {
+
+      const get_api_token = function (org_id, org_token, email, password) {
+        return $http.get([
+          '/api/v3/audit_template/get_api_token/?organization_id=', org_id,
+          '&organization_token=', org_token,
+          '&email=', email,
+          '&password=', password
+        ].join('')).then(function (response) {
+          return response.data;
+        });
+      };
+
+      const analyses_factory = {
+        get_api_token: get_api_token
+      };
+
+      return analyses_factory;
+    }]);
