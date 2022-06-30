0e197c166542c068e3d26826e90b93495fc01b08
Ross Perry <perryr16@gmail.com>
2022-06-30 16:09:55 -0600

    crud functionality on data agg modal

--- seed/static/seed/js/controllers/data_aggregation_modal_controller.js
+++ seed/static/seed/js/controllers/data_aggregation_modal_controller.js
@@ -14,43 +14,118 @@ angular.module('BE.seed.controller.data_aggregation_modal', []).controller('data
             $uibModalInstance,
             all_columns,
             data_aggregation_service,
-            organization
-
+            organization,
         ) {
             $scope.all_columns = all_columns;
             $scope.organization = organization;
-            $scope.crud_selection = 'create';
-            $scope.new_data_aggregation = {
+            $scope.crud_selection = 'overview';
+            $scope.data_aggregation = {
                 'name': null,
                 'type': 'Average',
                 'column': null,
             };
             $scope.data_aggregation_type_options = ['Average', 'Count', 'Max', 'Min', 'Sum'];
 
-            $scope.validate_new_data_aggregation = function() {
-                return Object.values($scope.new_data_aggregation).every(Boolean)
+            $scope.validate_data_aggregation = () => {
+                return Object.values($scope.data_aggregation).every(Boolean)
             };
 
-            $scope.crud_select = function(crud_option) {
+            $scope.crud_select = (crud_option) => {
                 $scope.crud_selection = crud_option
             };
 
-            $scope.create = function() {
-                let { name, type, column } = $scope.new_data_aggregation
-                column = column.id
-                res = data_aggregation_service.create_data_aggregation($scope.organization.id, {name, type, column})
-                console.log('res', res)
+            $scope.capitalize = (str) => {
+                return str[0].toUpperCase() + str.slice(1);
             };
 
-            $scope.cancel = function () {
+            $scope.create_data_aggregation = () => {
+                let { name, type, column } = $scope.data_aggregation
+                column = column.id
+                data_aggregation_service.create_data_aggregation($scope.organization.id, {name, type, column})
+                    .then(response => {
+                        console.log(response)
+                        $scope.crud_select('post_action')
+                        get_data_aggregations()
+                        if (response.status == 'success') {
+                            $scope.message = `Successfully created Data Aggregation id:${response.data_aggregation.id} name: ${response.data_aggregation.name}`
+
+                    } else {
+                            $scope.message = `Error`
+                        }
+                    })
+            };
+
+            $scope.confirm_delete_data_aggregation = (data_aggregation_id) => {
+                data_aggregation_service.get_data_aggregation($scope.organization.id, data_aggregation_id)
+                .then(response => {
+                    $scope.data_aggregation_to_delete = response.data_aggregation
+                })
+                $scope.crud_select('delete')
+                
+            }
+
+            $scope.delete_data_aggregation = (data_aggregation_id) => {
+                console.log('delete data agg', data_aggregation_id)
+                data_aggregation_service.delete_data_aggregation($scope.organization.id, data_aggregation_id)
+                    .then(response => {
+                        console.log(response)
+                        $scope.crud_select('post_action')
+                        get_data_aggregations()
+                        if (response.status == 'success') {
+                            $scope.message = 'Successfully Deleted Data Aggregation'
+                        } else {
+                            $scope.message = `Error`
+                        }
+                    })
+            }
+
+            $scope.edit_data_aggregation = (data_aggregation_id) => {
+                data_aggregation_service.get_data_aggregation($scope.organization.id, data_aggregation_id)
+                    .then(response => {
+                        $scope.data_aggregation = response.data_aggregation
+                    })
+                $scope.crud_select('update')
+            }
+
+            $scope.update_data_aggregation = (data_aggregation_id) => {
+                console.log('delete data agg', data_aggregation_id)
+                let { name, type, column } = $scope.data_aggregation
+                column = column.id
+                data_aggregation_service.update_data_aggregation($scope.organization.id, data_aggregation_id, {name, type, column})
+                    .then(response => {
+                        console.log(response)
+                        $scope.crud_select('post_action')
+                        get_data_aggregations()
+                        if (response.status == 'success') {
+                            $scope.message = `Successfully Updated Data Aggregation id:${response.data_aggregation.id} name: ${response.data_aggregation.name}`
+                        } else {
+                            $scope.message = `Error`
+                        }
+                    })
+
+            }
+
+
+            $scope.cancel =  () => {
                 $uibModalInstance.dismiss();
             };
 
-            const init = function() {
+            const get_data_aggregations = () => {
+                data_aggregation_service.get_data_aggregations($scope.organization.id)
+                    .then(data => {
+                        console.log('all data agg', data)
+                        data.message.forEach(da => da.column_id_name = da.id + ' / ' + da.name)
+                        $scope.data_aggregations = data.message
+
+                    })
+            }
+
+            const init = () => {
                 const x = 10
+                get_data_aggregations()
                 console.log('init modal')
             };
-            
+
             init();
         }
     ]);
--- seed/static/seed/js/services/data_aggregation_service.js
+++ seed/static/seed/js/services/data_aggregation_service.js
@@ -7,14 +7,59 @@ angular.module('BE.seed.service.data_aggregation', []).factory('data_aggregation
     function ($http) {
         const data_aggregation_factory = {};
 
-        data_aggregation_factory.create_data_aggregation = function (organization_id, data) {
+        data_aggregation_factory.create_data_aggregation = (organization_id, data) => {
             console.log('create data', data)
             return $http({
                 url: '/api/v3/data_aggregations/',
                 method: 'POST',
                 params: {organization_id},
                 data: data,
-            }).then(function (response) {
+            }).then(response => {
+                return response.data
+            })
+        }
+
+        data_aggregation_factory.get_data_aggregations = (organization_id) => {
+            console.log('get_data_aggs')
+            return $http({
+                url: '/api/v3/data_aggregations/',
+                method: 'GET',
+                params: {organization_id},
+            }).then(response => {
+                return response.data
+            })
+        }
+
+        data_aggregation_factory.get_data_aggregation = (organization_id, data_aggregation_id) => {
+            console.log('get_data_agg', data_aggregation_id)
+            return $http({
+                url: `/api/v3/data_aggregations/${data_aggregation_id}/`,
+                method: 'GET',
+                params: {organization_id},
+            }).then(response => {
+                return response.data
+            })
+        }
+
+        data_aggregation_factory.delete_data_aggregation = (organization_id, data_aggregation_id) => {
+            console.log('delete data agg', data_aggregation_id)
+            return $http({
+                url: `/api/v3/data_aggregations/${data_aggregation_id}/`,
+                method: 'DELETE',
+                params: { organization_id }
+            }).then(response => {
+                return response.data
+            })
+        }
+
+        data_aggregation_factory.update_data_aggregation = (organization_id, data_aggregation_id, data) => {
+            console.log('update data agg', data_aggregation_id)
+            return $http({
+                url: `/api/v3/data_aggregations/${data_aggregation_id}/`,
+                method: 'PUT',
+                params: {organization_id},
+                data: data
+            }).then(response => {
                 return response.data
             })
         }
