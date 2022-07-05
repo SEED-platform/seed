afc09c4c710e4e0577fc9d5b76071d3fac6fd69e
Ross Perry <perryr16@gmail.com>
2022-07-05 14:41:57 -0600

    evaluate extra data fields

--- seed/models/data_aggregations.py
+++ seed/models/data_aggregations.py
@@ -6,8 +6,9 @@
 """
 from django.db import models
 from django.db.models import Avg, Count, Max, Min, Sum
-from seed.models import Column
+from seed.models import Column, PropertyState
 from seed.lib.superperms.orgs.models import Organization
+import logging
 
 class DataAggregation(models.Model):
     name = models.CharField(max_length=255)
@@ -30,4 +31,43 @@ class DataAggregation(models.Model):
     type = models.IntegerField(choices=AGGREGATION_TYPES)
 
     def evaluate(self):
-        return 123
+        column = self.column
+        
+        if column.is_extra_data:
+            return self.evaluate_extra_data()
+        elif column.derived_column:
+            return {"value": 'derived column', "units": None}
+        else: 
+            type_lookup = {0: Avg, 1: Count, 2: Max, 3: Min, 4: Sum}
+            # PropertyState must be associated with the current org AND a valid PropertyView
+            aggregation = PropertyState.objects.filter(organization=self.organization.id, propertyview__isnull=False).aggregate(value=type_lookup[self.type](column.column_name))
+
+            if aggregation.get('value'):
+                value = aggregation['value']
+                if type(value) is int or type(value) is float:
+                    return {"value": round(value,2), "units": None}
+
+
+                return {"value": round(value.m, 2), "units": "{:P~}".format(value.u)}
+
+            return 'failed'
+
+    def evaluate_extra_data(self):
+        extra_data_col = 'extra_data__' + self.column.column_name
+        q_set = PropertyState.objects.filter(organization=self.organization.id, propertyview__isnull=False).values(extra_data_col)
+        values = []
+        logging.error('>>> q_set %s', q_set)
+        for val in list(q_set):
+            try: 
+                values.append(float(val[extra_data_col]))
+            except (ValueError, TypeError):
+                pass
+
+        if values:
+            type_to_aggregate = {0: sum(values)/len(values), 1: len(values), 2: max(values), 3: min(values), 4: sum(values)}
+            logging.error('>>> values %s', values)
+            return {"value": type_to_aggregate[self.type], "units": None}
+        
+        return {"value": 'def extra data', "units": None}
+
+
