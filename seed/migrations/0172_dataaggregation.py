8bab932333f2644a4c03d3f6d388812fdb3bcccc
Ross Perry <perryr16@gmail.com>
2022-07-08 09:12:45 -0600

    update migrations

--- seed/migrations/0172_dataaggregation.py
+++ seed/migrations/0172_dataaggregation.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('orgs', '0020_rename_display_significant_figures_organization_display_decimal_places'),
-        ('seed', '0170_column_derived_column'),
+        ('seed', '0171_update_bae_assets'),
     ]
 
     operations = [
