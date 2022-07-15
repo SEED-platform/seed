159becb7dd85d7bf7fe152695d2b9d5377c0f8d4
Alex Chapin <alex.chapin@nrel.gov>
2022-07-15 16:43:12 -0400

    #2885 reverted some files back to dev version

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -26,10 +26,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'seed',
-        'USER': 'seed',
-        'PASSWORD': 'GDEus3fasd1askj89QkAldjfX',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '',
     },
 }
 
