ed636ed34b41b5ee9e5529fe416c2b4ae2591764
loydbanks <lonwabo@bitcast.co.za>
2022-07-27 10:28:13 +0000

    upload media to s3

--- config/settings/common.py
+++ config/settings/common.py
@@ -16,7 +16,7 @@ from seed.serializers.celery import CeleryDatetimeSerializer
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-PROTOCOL = os.environ.get('PROTOCOL', 'https')
+# PROTOCOL = os.environ.get('PROTOCOL', 'https')
 
 SESSION_COOKIE_DOMAIN = None
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
@@ -105,6 +105,7 @@ DJANGO_CORE_APPS = (
     'oauth2_jwt_provider',
     'crispy_forms',  # needed to squash warnings around collectstatic with rest_framework
     'post_office',
+    'storages'
 )
 
 SEED_CORE_APPS = (
@@ -162,6 +163,27 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 AWS_QUERYSTRING_AUTH = False
+
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
+if USE_S3:
+    print("credentials", os.getenv('AWS_ACCESS_KEY_ID'), 
+                         os.getenv('AWS_SECRET_ACCESS_KEY'),
+                         os.getenv('AWS_STORAGE_BUCKET_NAME'),
+                         os.getenv('AWS_S3_REGION_NAME'))
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    print("custom domain",  f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com")
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+    # s3 media settings
+    MEDIA_ROOT = "media"
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # django-longer-username-and-email
 REQUIRE_UNIQUE_EMAIL = False
--- seed/views/v3/uploads.py
+++ seed/views/v3/uploads.py
@@ -6,7 +6,7 @@ import os
 import pint
 import xlrd
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import default_storage
 from django.http import JsonResponse
 from drf_yasg.utils import no_body, swagger_auto_schema
 from past.builtins import basestring
@@ -28,7 +28,7 @@ def get_upload_path(filename):
     path = os.path.join(settings.MEDIA_ROOT, "uploads", filename)
 
     # Get a unique filename using the get_available_name method in FileSystemStorage
-    s = FileSystemStorage()
+    s = default_storage
     return s.get_available_name(path)
 
 
@@ -95,10 +95,6 @@ class UploadViewSet(viewsets.ViewSet, OrgMixin):
         filename = the_file.name
         path = get_upload_path(filename)
 
-        # verify the directory exists
-        if not os.path.exists(os.path.dirname(path)):
-            os.makedirs(os.path.dirname(path))
-
         extension = the_file.name.split(".")[-1]
         if extension == "xlsx" or extension == "xls":
             workbook = xlrd.open_workbook(file_contents=the_file.read())
@@ -119,7 +115,7 @@ class UploadViewSet(viewsets.ViewSet, OrgMixin):
                 })
 
         # save the file
-        with open(path, 'wb+') as temp_file:
+        with default_storage.open(path, mode='wb+') as temp_file:
             for chunk in the_file.chunks():
                 temp_file.write(chunk)
         org_id = self.get_organization(request)
@@ -228,13 +224,9 @@ class UploadViewSet(viewsets.ViewSet, OrgMixin):
         # create a folder to keep pm_import files
         path = os.path.join(settings.MEDIA_ROOT, "uploads", "pm_imports", file_name)
 
-        # Get a unique filename using the get_available_name method in FileSystemStorage
-        s = FileSystemStorage()
+        # Get a unique filename using the get_available_name method in default_storage
+        s = default_storage
         path = s.get_available_name(path)
-
-        # verify the directory exists
-        if not os.path.exists(os.path.dirname(path)):
-            os.makedirs(os.path.dirname(path))
 
         # This list should cover the core keys coming from PM, ensuring that they map easily
         # We will also look for keys not in this list and just map them to themselves
