b7b3c24b18c269a38b85ea145fca1da515ab5570
Ryo <kyle.schulz@devetry.com>
2022-08-02 14:48:04 -0600

    fix: disabled filtering and sorting on related columns

--- seed/static/seed/js/controllers/inventory_list_controller.js
+++ seed/static/seed/js/controllers/inventory_list_controller.js
@@ -751,6 +751,19 @@ angular.module('BE.seed.controller.inventory_list', [])
         });
       }
 
+      // disable sorting and filtering on related data until the backend can filter/sort over two models
+      for (i in $scope.columns) {
+        let column = $scope.columns[i];
+        if (column['related']) {
+          column['enableSorting'] = false;
+          title = "Filtering disabled for taxlot columns on the property list."
+          if ($scope.inventory_type == 'property') {
+            title = "Filtering disabled for property columns on the taxlot list."
+          }
+          column['filterHeaderTemplate'] = '<div class="ui-grid-filter-container"><input type="text" title="' + title + '" class="ui-grid-filter-input" disabled=disabled />'
+        }
+      }
+
       var findColumn = _.memoize(function (name) {
         return _.find(all_columns, {name: name});
       });
