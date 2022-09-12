183c6341e2356c121f51d172e869506aeb86d99a
Ryo <kyle.schulz@devetry.com>
2022-09-12 13:55:47 -0600

    fix: tox

--- seed/utils/search.py
+++ seed/utils/search.py
@@ -4,8 +4,8 @@
 :copyright (c) 2014 - 2022, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.
 :author 'Piper Merriam <pmerriam@quickleft.com'
 """
-import operator
 import logging
+import operator
 import re
 from dataclasses import dataclass
 from datetime import datetime
