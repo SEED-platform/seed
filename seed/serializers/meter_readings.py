f66506888b810c688fed05639d7d5fa8b304668e
Nicholas Long <1907354+nllong@users.noreply.github.com>
2023-01-29 17:46:55 -0700

    Add bulk meter reading API for faster creation (#3787)
    * add bulk meter reading serializer for imports
    * fix validation on bulk_create method
    * make single writing naive too

--- seed/serializers/meter_readings.py
+++ seed/serializers/meter_readings.py
@@ -4,23 +4,67 @@
 :copyright (c) 2014 - 2022, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.
 :author
 """
+import dateutil.parser
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from django.utils.timezone import make_aware
+from pytz import timezone
 from rest_framework import serializers
 
+from config.settings.common import TIME_ZONE
 from seed.models import MeterReading
+
+# import logging
+# _log = logging.getLogger(__name__)
+
+
+class MeterReadingBulkCreateUpdateSerializer(serializers.ListSerializer):
+    def to_internal_value(self, data):
+        for datum in data:
+            datum['start_time'] = make_aware(dateutil.parser.parse(
+                datum['start_time']), timezone=timezone(TIME_ZONE))
+            datum['end_time'] = make_aware(dateutil.parser.parse(
+                datum['end_time']), timezone=timezone(TIME_ZONE))
+        return data
+
+    def create(self, validated_data):
+        meter_data = [MeterReading(**item) for item in validated_data]
+
+        try:
+            # Presently update_conflicts are not supported in Django 3.x.
+            # An older github patch has happened, but we need to evaluate
+            # it: https://github.com/martinphellwig/django-query-signals/commit/a3ed59614287f1ef9e7398d325a8bbcc11bf0b3c.
+            # For now, if there is a conflict with the date/times, then it will
+            # ignore the conflict and not update the reading values.
+            #   update_conflicts=True,
+            #   update_fields=['reading', 'source_unit', 'conversion_factor'],
+            result = MeterReading.objects.bulk_create(meter_data, ignore_conflicts=True)
+        except IntegrityError as e:
+            raise ValidationError(e)
+
+        return result
 
 
 class MeterReadingSerializer(serializers.ModelSerializer):
-    # type = ChoiceField(choices=Meter.ENERGY_TYPES, required=True)
-    # alias = serializers.CharField(required=False, allow_blank=True)
-    # source = ChoiceField(choices=Meter.SOURCES)
-    # source_id = serializers.CharField(required=False, allow_blank=True)
-    # scenario_id = serializers.IntegerField(required=False, allow_null=True)
-    # scenario_name = serializers.CharField(required=False, allow_blank=True)
-    # # meter_readings = serializers.StringRelatedField(many=True)
+    def to_internal_value(self, data):
+        data['start_time'] = make_aware(dateutil.parser.parse(
+            data['start_time']), timezone=timezone(TIME_ZONE))
+        data['end_time'] = make_aware(dateutil.parser.parse(
+            data['end_time']), timezone=timezone(TIME_ZONE))
+        return data
+
+    def create(self, validated_data):
+        instance = MeterReading(**validated_data)
+
+        if isinstance(self._kwargs["data"], dict):
+            instance.save()
+
+        return instance
 
     class Meta:
         model = MeterReading
         exclude = ('meter', )
+        list_serializer_class = MeterReadingBulkCreateUpdateSerializer
 
     def to_representation(self, obj):
         result = super().to_representation(obj)
