7b2925305d64593dba2f58a35abeedac73efb0d7
Ross Perry <perryr16@gmail.com>
2023-02-01 11:05:48 -0700

    revert

--- config/settings/docker_dev.py
+++ config/settings/docker_dev.py
@@ -116,7 +116,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
         },
     },
 }
