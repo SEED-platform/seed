23c99aed3af321af5c826d031237a2d457edc987
Nicholas Long <nicholas.long@nrel.gov>
2023-03-08 21:47:26 -0700

    legal updates

--- seed/urls.py
+++ seed/urls.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # encoding: utf-8
 """
-SEED Platform (TM), (c) Alliance for Sustainable Energy, LLC, and other contributors.
+SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
 See also https://github.com/seed-platform/seed/main/LICENSE.md
 """
 
