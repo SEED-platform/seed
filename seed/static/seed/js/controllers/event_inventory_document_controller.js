ccb735e800fd1f109c2e1485fc06670c8b5ed176
Alex Chapin <alex.chapin@nrel.gov>
2023-03-15 08:15:50 -0400

    pre-commit fixes

---
+++ seed/static/seed/js/controllers/event_inventory_document_controller.js
@@ -0,0 +1,20 @@
+/*
+ * :copyright (c) 2014 - 2022, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Department of Energy) and contributors. All rights reserved.
+ * :author
+ */
+
+angular.module('BE.seed.controller.event_inventory_document', [])
+    .controller('event_inventory_document_controller', [
+        '$scope',
+        '$state',
+        'analyses_service',
+        'inventory_service',
+        function (
+            $scope,
+            $state,
+            analyses_service,
+            inventory_service,
+        ) {
+
+        }
+    ])
