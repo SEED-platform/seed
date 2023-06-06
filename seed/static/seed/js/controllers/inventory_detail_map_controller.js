4e5bbd30cad84da3a02c6ce4e32b49281b6bd5e8
Ross Perry <perryr16@gmail.com>
2023-06-06 10:42:22 -0600

    return if no preferred ubid

--- seed/static/seed/js/controllers/inventory_detail_map_controller.js
+++ seed/static/seed/js/controllers/inventory_detail_map_controller.js
@@ -74,215 +74,217 @@ angular.module('BE.seed.controller.inventory_detail_map', [])
             // Do not map if there is no preferred ubid.
             if (!$scope.item_state.ubid) {
                 return
-            }
-
-            // store a mapping of layers z-index and visibility
-            $scope.layers = {};
-            $scope.layers.base_layer = { zIndex: 0, visible: 1 };
-            if ($scope.inventory_type === 'properties') {
-                $scope.layers.points_layer = { zIndex: 2, visible: 1 };
-                $scope.layers.building_bb_layer = { zIndex: 3, visible: 1 };
-                $scope.layers.building_centroid_layer = { zIndex: 4, visible: 1 };
-                $scope.layers.taxlot_bb_layer = { zIndex: 5, visible: 0 };
-                $scope.layers.taxlot_centroid_layer = { zIndex: 6, visible: 0 };
             } else {
-                // taxlots
-                $scope.layers.points_layer = { zIndex: 2, visible: 1 };
-                $scope.layers.building_bb_layer = { zIndex: 3, visible: 0 };
-                $scope.layers.building_centroid_layer = { zIndex: 4, visible: 0 };
-                $scope.layers.taxlot_bb_layer = { zIndex: 5, visible: 1 };
-                $scope.layers.taxlot_centroid_layer = { zIndex: 6, visible: 1 };
-            }
 
-            // Map
-            var base_layer = new ol.layer.Tile({
-                source: new ol.source.Stamen({
-                    layer: 'terrain'
-                }),
-                zIndex: $scope.layers.base_layer.zIndex // Note: This is used for layer toggling.
-            });
-
-            // This uses the bounding box instead of the lat/long. See var BuildingPoint function in inventory_map_controller for reference
-            var buildingBoundingBox = function () {
-                var format = new ol.format.WKT();
-
-                var feature = format.readFeature($scope.bounding_box, {
-                    dataProjection: 'EPSG:4326',
-                    featureProjection: 'EPSG:3857'
-                });
-
-                feature.setProperties($scope.item_state);
-                return feature;
-            }
-
-            var buildingSources = function () {
-                var features = [buildingBoundingBox()];
-                return new ol.source.Vector({ features: features });
-            };
-
-            // Define building UBID bounding box
-            var buildingBB = function () {
-                var format = new ol.format.WKT();
-
-                var feature = format.readFeature($scope.bounding_box, {
-                    dataProjection: 'EPSG:4326',
-                    featureProjection: 'EPSG:3857'
-                });
-                feature.setProperties($scope.item_state);
-                return feature;
-            };
-
-            // Define building UBID centroid box
-            var buildingCentroid = function () {
-                var format = new ol.format.WKT();
-
-                var feature = format.readFeature($scope.centroid, {
-                    dataProjection: 'EPSG:4326',
-                    featureProjection: 'EPSG:3857'
-                });
-                feature.setProperties($scope.item_state);
-                return feature;
-            };
-
-            var buildingBBSources = function () {
-                var features = [buildingBB()];
-                return new ol.source.Vector({ features: features });
-            };
-
-            var buildingCentroidSources = function () {
-                var features = [buildingCentroid()];
-                return new ol.source.Vector({ features: features });
-            };
-
-            // Define taxlot UBID bounding box
-            var taxlotBB = function () {
-                var format = new ol.format.WKT();
-
-                var feature = format.readFeature($scope.bounding_box, {
-                    dataProjection: 'EPSG:4326',
-                    featureProjection: 'EPSG:3857'
-                });
-                feature.setProperties($scope.item_state);
-                return feature;
-            };
-
-            // Define taxlot UBID centroid box
-            var taxlotCentroid = function () {
-                var format = new ol.format.WKT();
-
-                var feature = format.readFeature($scope.centroid, {
-                    dataProjection: 'EPSG:4326',
-                    featureProjection: 'EPSG:3857'
-                });
-                feature.setProperties($scope.item_state);
-                return feature;
-            };
-
-            var taxlotBBSources = function () {
-                var features = [taxlotBB()];
-                return new ol.source.Vector({ features: features });
-            };
-
-            var taxlotCentroidSources = function () {
-                var features = [taxlotCentroid()];
-                return new ol.source.Vector({ features: features });
-            };
-
-            var clusterSource = function () {
-                return new ol.source.Cluster({
-                    source: buildingSources(),
-                    distance: 45
-                });
-            };
-
-            // style for building ubid bounding and centroid boxes
-            var buildingStyle = function (/*feature*/) {
-                return new ol.style.Style({
-                    stroke: new ol.style.Stroke({
-                        color: '#185189',
-                        width: 2
-                    })
-                });
-            };
-
-            // style for taxlot ubid bounding and centroid boxes
-            var taxlotStyle = function (/*feature*/) {
-                return new ol.style.Style({
-                    stroke: new ol.style.Stroke({
-                        color: '#10A0A0',
-                        width: 2
-                    })
-                });
-            };
-
-            $scope.building_bb_layer = new ol.layer.Vector({
-                source: buildingBBSources(),
-                zIndex: $scope.layers.building_bb_layer.zIndex,
-                style: buildingStyle
-            });
-
-            $scope.building_centroid_layer = new ol.layer.Vector({
-                source: buildingCentroidSources(),
-                zIndex: $scope.layers.building_centroid_layer.zIndex,
-                style: buildingStyle
-            });
-
-            $scope.taxlot_bb_layer = new ol.layer.Vector({
-                source: taxlotBBSources(),
-                zIndex: $scope.layers.taxlot_bb_layer.zIndex,
-                style: taxlotStyle
-            });
-
-            $scope.taxlot_centroid_layer = new ol.layer.Vector({
-                source: taxlotCentroidSources(),
-                zIndex: $scope.layers.taxlot_centroid_layer.zIndex,
-                style: taxlotStyle
-            });
-
-            // Render map
-            var layers = [];
-            if ($scope.inventory_type === 'properties') {
-                layers = [base_layer, $scope.building_bb_layer, $scope.building_centroid_layer];
-            } else {
-                layers = [base_layer, $scope.points_layer, $scope.taxlot_bb_layer, $scope.taxlot_centroid_layer];
-            }
-            $scope.map = new ol.Map({
-                target: 'map',
-                layers: layers
-            });
-
-
-            // Zoom and center based on provided points (none, all, or a subset)
-            var zoomCenter = function (bounding_box_source, extra_view_options) {
-                if (_.isUndefined(extra_view_options)) extra_view_options = {};
-                if (bounding_box_source.isEmpty()) {
-                    // Default view with no points is the middle of US
-                    var empty_view = new ol.View({
-                        center: ol.proj.fromLonLat([-99.066067, 39.390897]),
-                        zoom: 4.5
-                    });
-                    $scope.map.setView(empty_view);
+                // store a mapping of layers z-index and visibility
+                $scope.layers = {};
+                $scope.layers.base_layer = { zIndex: 0, visible: 1 };
+                if ($scope.inventory_type === 'properties') {
+                    $scope.layers.points_layer = { zIndex: 2, visible: 1 };
+                    $scope.layers.building_bb_layer = { zIndex: 3, visible: 1 };
+                    $scope.layers.building_centroid_layer = { zIndex: 4, visible: 1 };
+                    $scope.layers.taxlot_bb_layer = { zIndex: 5, visible: 0 };
+                    $scope.layers.taxlot_centroid_layer = { zIndex: 6, visible: 0 };
                 } else {
-                    var extent = bounding_box_source.getExtent();
-
-                    var view_options = Object.assign({
-                        size: $scope.map.getSize(),
-                        padding: [1, 1, 1, 1],
-                    }, extra_view_options);
-                    $scope.map.getView().fit(extent, view_options);
+                    // taxlots
+                    $scope.layers.points_layer = { zIndex: 2, visible: 1 };
+                    $scope.layers.building_bb_layer = { zIndex: 3, visible: 0 };
+                    $scope.layers.building_centroid_layer = { zIndex: 4, visible: 0 };
+                    $scope.layers.taxlot_bb_layer = { zIndex: 5, visible: 1 };
+                    $scope.layers.taxlot_centroid_layer = { zIndex: 6, visible: 1 };
                 }
-            };
+    
+                // Map
+                var base_layer = new ol.layer.Tile({
+                    source: new ol.source.Stamen({
+                        layer: 'terrain'
+                    }),
+                    zIndex: $scope.layers.base_layer.zIndex // Note: This is used for layer toggling.
+                });
+    
+                // This uses the bounding box instead of the lat/long. See var BuildingPoint function in inventory_map_controller for reference
+                var buildingBoundingBox = function () {
+                    var format = new ol.format.WKT();
+    
+                    var feature = format.readFeature($scope.bounding_box, {
+                        dataProjection: 'EPSG:4326',
+                        featureProjection: 'EPSG:3857'
+                    });
+    
+                    feature.setProperties($scope.item_state);
+                    return feature;
+                }
+    
+                var buildingSources = function () {
+                    var features = [buildingBoundingBox()];
+                    return new ol.source.Vector({ features: features });
+                };
+    
+                // Define building UBID bounding box
+                var buildingBB = function () {
+                    var format = new ol.format.WKT();
+    
+                    var feature = format.readFeature($scope.bounding_box, {
+                        dataProjection: 'EPSG:4326',
+                        featureProjection: 'EPSG:3857'
+                    });
+                    feature.setProperties($scope.item_state);
+                    return feature;
+                };
+    
+                // Define building UBID centroid box
+                var buildingCentroid = function () {
+                    var format = new ol.format.WKT();
+    
+                    var feature = format.readFeature($scope.centroid, {
+                        dataProjection: 'EPSG:4326',
+                        featureProjection: 'EPSG:3857'
+                    });
+                    feature.setProperties($scope.item_state);
+                    return feature;
+                };
+    
+                var buildingBBSources = function () {
+                    var features = [buildingBB()];
+                    return new ol.source.Vector({ features: features });
+                };
+    
+                var buildingCentroidSources = function () {
+                    var features = [buildingCentroid()];
+                    return new ol.source.Vector({ features: features });
+                };
+    
+                // Define taxlot UBID bounding box
+                var taxlotBB = function () {
+                    var format = new ol.format.WKT();
+    
+                    var feature = format.readFeature($scope.bounding_box, {
+                        dataProjection: 'EPSG:4326',
+                        featureProjection: 'EPSG:3857'
+                    });
+                    feature.setProperties($scope.item_state);
+                    return feature;
+                };
+    
+                // Define taxlot UBID centroid box
+                var taxlotCentroid = function () {
+                    var format = new ol.format.WKT();
+    
+                    var feature = format.readFeature($scope.centroid, {
+                        dataProjection: 'EPSG:4326',
+                        featureProjection: 'EPSG:3857'
+                    });
+                    feature.setProperties($scope.item_state);
+                    return feature;
+                };
+    
+                var taxlotBBSources = function () {
+                    var features = [taxlotBB()];
+                    return new ol.source.Vector({ features: features });
+                };
+    
+                var taxlotCentroidSources = function () {
+                    var features = [taxlotCentroid()];
+                    return new ol.source.Vector({ features: features });
+                };
+    
+                var clusterSource = function () {
+                    return new ol.source.Cluster({
+                        source: buildingSources(),
+                        distance: 45
+                    });
+                };
+    
+                // style for building ubid bounding and centroid boxes
+                var buildingStyle = function (/*feature*/) {
+                    return new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color: '#185189',
+                            width: 2
+                        })
+                    });
+                };
+    
+                // style for taxlot ubid bounding and centroid boxes
+                var taxlotStyle = function (/*feature*/) {
+                    return new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color: '#10A0A0',
+                            width: 2
+                        })
+                    });
+                };
+    
+                $scope.building_bb_layer = new ol.layer.Vector({
+                    source: buildingBBSources(),
+                    zIndex: $scope.layers.building_bb_layer.zIndex,
+                    style: buildingStyle
+                });
+    
+                $scope.building_centroid_layer = new ol.layer.Vector({
+                    source: buildingCentroidSources(),
+                    zIndex: $scope.layers.building_centroid_layer.zIndex,
+                    style: buildingStyle
+                });
+    
+                $scope.taxlot_bb_layer = new ol.layer.Vector({
+                    source: taxlotBBSources(),
+                    zIndex: $scope.layers.taxlot_bb_layer.zIndex,
+                    style: taxlotStyle
+                });
+    
+                $scope.taxlot_centroid_layer = new ol.layer.Vector({
+                    source: taxlotCentroidSources(),
+                    zIndex: $scope.layers.taxlot_centroid_layer.zIndex,
+                    style: taxlotStyle
+                });
+    
+                // Render map
+                var layers = [];
+                if ($scope.inventory_type === 'properties') {
+                    layers = [base_layer, $scope.building_bb_layer, $scope.building_centroid_layer];
+                } else {
+                    layers = [base_layer, $scope.points_layer, $scope.taxlot_bb_layer, $scope.taxlot_centroid_layer];
+                }
+                $scope.map = new ol.Map({
+                    target: 'map',
+                    layers: layers
+                });
+    
+    
+                // Zoom and center based on provided points (none, all, or a subset)
+                var zoomCenter = function (bounding_box_source, extra_view_options) {
+                    if (_.isUndefined(extra_view_options)) extra_view_options = {};
+                    if (bounding_box_source.isEmpty()) {
+                        // Default view with no points is the middle of US
+                        var empty_view = new ol.View({
+                            center: ol.proj.fromLonLat([-99.066067, 39.390897]),
+                            zoom: 4.5
+                        });
+                        $scope.map.setView(empty_view);
+                    } else {
+                        var extent = bounding_box_source.getExtent();
+    
+                        var view_options = Object.assign({
+                            size: $scope.map.getSize(),
+                            padding: [1, 1, 1, 1],
+                        }, extra_view_options);
+                        $scope.map.getView().fit(extent, view_options);
+                    }
+                };
+    
+                // Set initial zoom and center
+                zoomCenter(clusterSource().getSource());
+    
+    
+                // map styling
+                let element = $scope.map.getViewport().querySelector('.ol-unselectable');
+                element.style.border = '1px solid gray';
+                element.style.borderRadius = '3px'
+    
+                element = $scope.map.getViewport().querySelector('.ol-overlaycontainer-stopevent')
+                element.style.display = 'none';
+            }
 
-            // Set initial zoom and center
-            zoomCenter(clusterSource().getSource());
-
-
-            // map styling
-            let element = $scope.map.getViewport().querySelector('.ol-unselectable');
-            element.style.border = '1px solid gray';
-            element.style.borderRadius = '3px'
-
-            element = $scope.map.getViewport().querySelector('.ol-overlaycontainer-stopevent')
-            element.style.display = 'none';
 
         }]);
