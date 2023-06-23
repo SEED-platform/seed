7801f75b18d10774ff8b0ebee4236405f449e245
Alex Swindler <Alex.Swindler@nrel.gov>
2023-06-23 15:36:50 -0600

    Fix serializer & Swagger description

--- config/urls.py
+++ config/urls.py
@@ -26,7 +26,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="SEED API",
         default_version='v3',
-        description="Test description",
+        description="SEED Platform API Documentation",
         # terms_of_service="https://www.google.com/policies/terms/",
         # contact=openapi.Contact(email="contact@snippets.local"),
         # license=openapi.License(name="BSD License"),
