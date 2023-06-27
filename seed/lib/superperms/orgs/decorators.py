16cec5b44dec359002c21beef904f8e06ea4f097
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2023-06-27 11:06:57 -0600

    AH test case class, decorator, and compliance_metric tests (#4108)
    * start of perm test
    
    * fix factories
    
    * add new ALI test helpers and decorator, fix compliance_metric tests
    
    * add decorator to update and destroy endpoints
    
    * cleanup
    
    * small fixes
    
    * Default to root in fake property
    
    * add ali tests for compliance-metric update and delete endpoints
    
    * Fix isort
    
    ---------
    
    Co-authored-by: Nicholas Long <nicholas.long@nrel.gov>
    Co-authored-by: Hannah Eslinger <heslinge@nrel.gov>

--- seed/lib/superperms/orgs/decorators.py
+++ seed/lib/superperms/orgs/decorators.py
@@ -9,16 +9,19 @@ from functools import wraps
 from inspect import signature
 
 from django.conf import settings
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseForbidden, JsonResponse
+from rest_framework import status
 
 from seed.lib.superperms.orgs.models import (
     ROLE_MEMBER,
     ROLE_OWNER,
     ROLE_VIEWER,
+    AccessLevelInstance,
     Organization,
     OrganizationUser
 )
 from seed.lib.superperms.orgs.permissions import get_org_id
+from seed.models import PropertyView, TaxLotView
 
 # Allow Super Users to ignore permissions.
 ALLOW_SUPER_USER_PERMS = getattr(settings, 'ALLOW_SUPER_USER_PERMS', True)
@@ -64,6 +67,11 @@ def requires_viewer(org_user):
 def requires_superuser(org_user):
     """Only Django superusers have superuser perms."""
     return org_user.user.is_superuser
+
+
+def requires_root_member_access(org_user):
+    """ User must be an owner or member at the root access level"""
+    return org_user.access_level_instance.depth == 1 and org_user.role_level >= ROLE_MEMBER
 
 
 def can_create_sub_org(org_user):
@@ -130,6 +138,7 @@ PERMS = {
     'requires_owner': requires_owner,
     'requires_member': requires_member,
     'requires_viewer': requires_viewer,
+    'requires_root_member_access': requires_root_member_access,
     'can_create_sub_org': can_create_sub_org,
     'can_remove_org': can_remove_org,
     'can_invite_member': can_invite_member,
@@ -179,7 +188,6 @@ def _validate_permissions(perm_name, request, requires_org):
         org = Organization.objects.get(pk=org_id)
     except Organization.DoesNotExist:
         return _make_resp('org_dne')
-
     # Skip perms checks if settings allow super_users to bypass.
     if request.user.is_superuser and ALLOW_SUPER_USER_PERMS:
         request.access_level_instance_id = org.root.id
@@ -200,7 +208,6 @@ def _validate_permissions(perm_name, request, requires_org):
 
 def has_perm_class(perm_name: str, requires_org: bool = True):
     """Proceed if user from request has ``perm_name``."""
-
     def decorator(fn):
         if 'self' in signature(fn).parameters:
             @wraps(fn)
@@ -210,6 +217,47 @@ def has_perm_class(perm_name: str, requires_org: bool = True):
             @wraps(fn)
             def _wrapped(request, *args, **kwargs):
                 return _validate_permissions(perm_name, request, requires_org) or fn(request, *args, **kwargs)
+
+        return _wrapped
+
+    return decorator
+
+
+def assert_hiarchary_access(request, property_view_id_kwarg=None, taxlot_view_id_kwarg=None, body_ali_id=None, *args, **kwargs):
+    if property_view_id_kwarg:
+        property_view = PropertyView.objects.get(pk=kwargs[property_view_id_kwarg])
+        requests_ali = property_view.property.access_level_instance
+
+    elif taxlot_view_id_kwarg:
+        taxlot_view = TaxLotView.objects.get(pk=kwargs[taxlot_view_id_kwarg])
+        requests_ali = taxlot_view.taxlot.access_level_instance
+
+    elif body_ali_id:
+        requests_ali = AccessLevelInstance.objects.get(pk=request.data[body_ali_id])
+
+    else:
+        property_view = PropertyView.objects.get(pk=request.GET['property_view_id'])
+        requests_ali = property_view.property.access_level_instance
+
+    user_ali = AccessLevelInstance.objects.get(pk=request.access_level_instance_id)
+    if not (user_ali == requests_ali or requests_ali.is_descendant_of(user_ali)):
+        return JsonResponse({
+            'status': 'error',
+            'message': 'No such resource.'
+        }, status=status.HTTP_404_NOT_FOUND)
+
+
+def has_hiarchary_access(property_view_id_kwarg=None, taxlot_view_id_kwarg=None, body_ali_id=None):
+    """Must be called after has_perm_class"""
+    def decorator(fn):
+        if 'self' in signature(fn).parameters:
+            @wraps(fn)
+            def _wrapped(self, request, *args, **kwargs):
+                return assert_hiarchary_access(request, property_view_id_kwarg, taxlot_view_id_kwarg, body_ali_id, *args, **kwargs) or fn(self, request, *args, **kwargs)
+        else:
+            @wraps(fn)
+            def _wrapped(request, *args, **kwargs):
+                return assert_hiarchary_access(request, property_view_id_kwarg, taxlot_view_id_kwarg, body_ali_id, *args, **kwargs) or fn(request, *args, **kwargs)
 
         return _wrapped
 
--- seed/tests/test_compliance_metrics_views.py
+++ seed/tests/test_compliance_metrics_views.py
@@ -6,42 +6,35 @@ See also https://github.com/seed-platform/seed/main/LICENSE.md
 """
 import json
 
-from django.test import TestCase
+from django.core import serializers
 from django.urls import reverse
 
-from seed.models import Column, ComplianceMetric, FilterGroup, User
-from seed.test_helpers.fake import FakeCycleFactory
-from seed.utils.organizations import create_organization
+from seed.models import ComplianceMetric, FilterGroup
+from seed.tests.util import AccessLevelBaseTestCase
 
 
-class ComplianceMetricViewTests(TestCase):
+class ComplianceMetricViewTests(AccessLevelBaseTestCase):
     """
     Test ComplianceMetric model
     """
 
     def setUp(self):
-        user_details = {
-            'username': 'test_user@demo.com',
-            'password': 'test_pass',
-            'email': 'test_user@demo.com',
-            'first_name': 'Johnny',
-            'last_name': 'Energy',
-        }
-        self.user = User.objects.create_superuser(**user_details)
-        self.org, _, _ = create_organization(self.user, "test-organization-a")
-        self.client.login(**user_details)
-        self.cycle1 = FakeCycleFactory(organization=self.org, user=self.user).get_cycle(name="Cycle A")
-        self.cycle2 = FakeCycleFactory(organization=self.org, user=self.user).get_cycle(name="Cycle B")
-        self.cycle3 = FakeCycleFactory(organization=self.org, user=self.user).get_cycle(name="Cycle C")
-        self.cycle4 = FakeCycleFactory(organization=self.org, user=self.user).get_cycle(name="Cycle D")
 
-        self.column1 = Column.objects.create(column_name='column 1', organization=self.org,)
-        self.column2 = Column.objects.create(column_name='column 2', organization=self.org,)
-        self.column3 = Column.objects.create(column_name='column 3', organization=self.org,)
-        self.column4 = Column.objects.create(column_name='column 4', organization=self.org,)
-        self.column5 = Column.objects.create(column_name='column 5', organization=self.org,)
-        self.column6 = Column.objects.create(column_name='column 6', organization=self.org,)
-        self.column7 = Column.objects.create(column_name='column 7', organization=self.org,)
+        # set up org, all users, and factories
+        super().setUp()
+
+        self.cycle1 = self.cycle_factory.get_cycle(name="Cycle A")
+        self.cycle2 = self.cycle_factory.get_cycle(name="Cycle B")
+        self.cycle3 = self.cycle_factory.get_cycle(name="Cycle C")
+        self.cycle4 = self.cycle_factory.get_cycle(name="Cycle D")
+
+        self.column1 = self.column_factory.get_column('column 1', is_extra_data=True)
+        self.column2 = self.column_factory.get_column('column 2', is_extra_data=True)
+        self.column3 = self.column_factory.get_column('column 3', is_extra_data=True)
+        self.column4 = self.column_factory.get_column('column 4', is_extra_data=True)
+        self.column5 = self.column_factory.get_column('column 5', is_extra_data=True)
+        self.column6 = self.column_factory.get_column('column 6', is_extra_data=True)
+        self.column7 = self.column_factory.get_column('column 7', is_extra_data=True)
 
         self.x_axes1 = [self.column5, self.column6, self.column7]
         self.x_axes2 = [self.column7]
@@ -52,7 +45,7 @@ class ComplianceMetricViewTests(TestCase):
         self.filter_group = FilterGroup.objects.create(
             name='filter group 1',
             organization_id=self.org.id,
-            inventory_type=1,  # Tax Lot
+            inventory_type=0,  # Property
             query_dict={'year_built__lt': ['1980']},
         )
         self.filter_group.save()
@@ -108,6 +101,18 @@ class ComplianceMetricViewTests(TestCase):
     def test_compliance_metric_create_endpoint(self):
         self.assertEqual(2, len(ComplianceMetric.objects.all()))
 
+        test_metric_details = {
+            "name": "compliance metric 3",
+            "actual_emission_column": self.column3.id,
+            "target_emission_column": self.column4.id,
+            "emission_metric_type": "Target > Actual for Compliance",
+            "filter_group": self.filter_group.id,
+            "cycles": [self.cycle1.id, self.cycle2.id],
+            "x_axis_columns": [self.column5.id, self.column6.id]
+        }
+
+        # can create as ROOT-LEVEL OWNER
+        self.login_as_root_owner()
         response = self.client.get(
             reverse('api:v3:compliance_metrics-list') + '?organization_id=' + str(self.org.id),
             content_type='application/json'
@@ -117,19 +122,11 @@ class ComplianceMetricViewTests(TestCase):
 
         response = self.client.post(
             reverse('api:v3:compliance_metrics-list') + '?organization_id=' + str(self.org.id),
-            data=json.dumps({
-                "name": "compliance metric 3",
-                "actual_emission_column": self.column3.id,
-                "target_emission_column": self.column4.id,
-                "emission_metric_type": "Target > Actual for Compliance",
-                "filter_group": self.filter_group.id,
-                "cycles": [self.cycle1.id, self.cycle2.id],
-                "x_axis_columns": [self.column5.id, self.column6.id]
-
-            }),
+            data=json.dumps(test_metric_details),
             content_type='application/json'
         )
         data = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
 
         self.assertEqual('compliance metric 3', data['compliance_metric']['name'])
         self.assertEqual(self.org.id, data['compliance_metric']['organization_id'])
@@ -163,6 +160,27 @@ class ComplianceMetricViewTests(TestCase):
         self.assertEqual(2, len(data['compliance_metrics']))
         self.assertEqual(2, len(ComplianceMetric.objects.all()))
 
+        # CAN CREATE as ROOT-LEVEL MEMBER:
+        self.login_as_root_member()
+        test_metric_details['name'] = "compliance ROOT MEMBER"
+
+        response = self.client.post(
+            reverse('api:v3:compliance_metrics-list') + '?organization_id=' + str(self.org.id),
+            data=json.dumps(test_metric_details),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+
+        # CANNOT CREATE AS CHILD-LEVEL MEMBER:
+        self.login_as_child_member()
+        test_metric_details['name'] = "compliance CHILD MEMBER"
+        response = self.client.post(
+            reverse('api:v3:compliance_metrics-list') + '?organization_id=' + str(self.org.id),
+            data=json.dumps(test_metric_details),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 403)
+
     def test_compliance_metric_create_bad_data(self):
         response = self.client.post(
             reverse('api:v3:compliance_metrics-list') + '?organization_id=' + str(self.org.id),
@@ -178,6 +196,7 @@ class ComplianceMetricViewTests(TestCase):
         self.assertEqual(expected, data['message'])
 
     def test_compliance_metric_retrieve_endpoint(self):
+        self.login_as_child_member()
         response = self.client.get(
             reverse('api:v3:compliance_metrics-detail', args=[self.compliance_metric1.id]) + '?organization_id=' + str(self.org.id)
         )
@@ -194,10 +213,12 @@ class ComplianceMetricViewTests(TestCase):
         self.assertEqual('error', data['status'])
         self.assertEqual('ComplianceMetric with id 99999999 does not exist', data['message'])
 
-    def test_data_view_update_endpoint(self):
+    def test_compliance_metric_update_endpoint(self):
         self.assertEqual('compliance metric 1', self.compliance_metric1.name)
         self.assertEqual(3, len(self.compliance_metric1.x_axis_columns.all()))
 
+        # CAN UPDATE as ROOT-LEVEL MEMBER:
+        self.login_as_root_member()
         response = self.client.put(
             reverse('api:v3:compliance_metrics-detail', args=[self.compliance_metric1.id]) + '?organization_id=' + str(self.org.id),
             data=json.dumps({
@@ -212,11 +233,7 @@ class ComplianceMetricViewTests(TestCase):
         self.assertEqual('updated name', data['compliance_metric']['name'])
         self.assertEqual(2, len(data['compliance_metric']['x_axis_columns']))
 
-        cm1 = ComplianceMetric.objects.get(id=self.compliance_metric1.id)
-        self.assertEqual('updated name', cm1.name)
-        self.assertEqual(2, len(cm1.x_axis_columns.all()))
-        self.assertEqual(self.column3.id, cm1.x_axis_columns.first().id)
-
+        # test cannot update a non-existing compliance metric
         response = self.client.put(
             reverse('api:v3:compliance_metrics-detail', args=[99999]) + '?organization_id=' + str(self.org.id),
             data=json.dumps({
@@ -228,5 +245,150 @@ class ComplianceMetricViewTests(TestCase):
         self.assertEqual('error', data['status'])
         self.assertEqual('ComplianceMetric with id 99999 does not exist', data['message'])
 
+        # CANNOT UPDATE AS CHILD-LEVEL MEMBER:
+        self.login_as_child_member()
+        response = self.client.put(
+            reverse('api:v3:compliance_metrics-detail', args=[self.compliance_metric1.id]) + '?organization_id=' + str(self.org.id),
+            data=json.dumps({
+                "name": "another name"
+            }),
+            content_type='application/json'
+        )
+        data = json.loads(response.content)
+        self.assertEqual('error', data['status'])
+        self.assertEqual(response.status_code, 403)
 
-# TODO: add tests for calculating compliance metrics
+        cm1 = ComplianceMetric.objects.get(id=self.compliance_metric1.id)
+        self.assertEqual('updated name', cm1.name)
+        self.assertEqual(2, len(cm1.x_axis_columns.all()))
+        self.assertEqual(self.column3.id, cm1.x_axis_columns.first().id)
+
+    def test_compliance_metric_delete_endpoint(self):
+
+        # CANNOT DELETE AS CHILD-LEVEL MEMBER:
+        self.login_as_child_member()
+        response = self.client.delete(
+            reverse('api:v3:compliance_metrics-detail', args=[self.compliance_metric2.id]) + '?organization_id=' + str(self.org.id),
+            content_type='application/json'
+        )
+        data = json.loads(response.content)
+        self.assertEqual('error', data['status'])
+        self.assertEqual(response.status_code, 403)
+
+        compliance_metrics = ComplianceMetric.objects.all().order_by('created')
+        self.assertEqual(2, len(compliance_metrics))
+
+        # CAN DELETE AS ROOT-LEVEL MEMBER:
+        self.login_as_root_member()
+        response = self.client.delete(
+            reverse('api:v3:compliance_metrics-detail', args=[self.compliance_metric2.id]) + '?organization_id=' + str(self.org.id),
+            content_type='application/json'
+        )
+        data = json.loads(response.content)
+        self.assertEqual('success', data['status'])
+        self.assertEqual(response.status_code, 200)
+
+        compliance_metrics = ComplianceMetric.objects.all().order_by('created')
+        self.assertEqual(1, len(compliance_metrics))
+
+
+class ComplianceMetricEvaluationTests(AccessLevelBaseTestCase):
+    """
+    Test ComplianceMetric model's ability to evaluate propertyview values
+    """
+    def setUp(self):
+
+        # set up org, all users, and factories
+        super().setUp()
+
+        self.cycle1 = self.cycle_factory.get_cycle(name="Cycle A")
+        self.cycle2 = self.cycle_factory.get_cycle(name="Cycle B")
+        self.cycle3 = self.cycle_factory.get_cycle(name="Cycle C")
+
+        self.site_eui = self.column_factory.get_column('site_eui')
+        self.total_ghg_emissions = self.column_factory.get_column('total_ghg_emissions')
+        self.source_eui = self.column_factory.get_column('source_eui')
+        self.total_marginal_ghg_emissions = self.column_factory.get_column('total_marginal_ghg_emissions')
+        self.column5 = self.column_factory.get_column('column 5', is_extra_data=True)
+
+        self.x_axes = [self.column5]
+
+        self.cycles = [self.cycle1, self.cycle2, self.cycle3]
+
+        self.filter_group = FilterGroup.objects.create(
+            name='filter group 1',
+            organization_id=self.org.id,
+            inventory_type=0,  # Property
+            query_dict={'year_built__lt': ['1980']},
+        )
+        self.filter_group.save()
+
+        # metric (just energy without filter group)
+        self.compliance_metric = ComplianceMetric.objects.create(
+            name='compliance metric 1',
+            organization=self.org,
+            actual_energy_column=self.site_eui,
+            target_energy_column=self.source_eui,
+            energy_metric_type=0
+        )
+        self.compliance_metric.x_axis_columns.set(self.x_axes)
+        self.compliance_metric.cycles.set(self.cycles)
+
+        # generate two different types of properties (2 for root and 2 for child)
+        self.office1 = self.property_factory.get_property(access_level_instance=self.root_level_instance)
+        self.office2 = self.property_factory.get_property(access_level_instance=self.child_level_instance)
+        self.retail3 = self.property_factory.get_property(access_level_instance=self.root_level_instance)
+        self.retail4 = self.property_factory.get_property(access_level_instance=self.child_level_instance)
+
+        self.view10 = self.property_view_factory.get_property_view(prprty=self.office1, cycle=self.cycle1, site_eui=60, source_eui=59, total_ghg_emissions=500)
+        self.view11 = self.property_view_factory.get_property_view(prprty=self.office2, cycle=self.cycle1, site_eui=70, source_eui=59, total_ghg_emissions=400)
+        self.view12 = self.property_view_factory.get_property_view(prprty=self.retail3, cycle=self.cycle1, site_eui=65, source_eui=62, total_ghg_emissions=300)
+        self.view13 = self.property_view_factory.get_property_view(prprty=self.retail4, cycle=self.cycle1, site_eui=72, source_eui=62, total_ghg_emissions=350)
+
+        serializers.serialize('json', [self.view10])
+
+        self.view20 = self.property_view_factory.get_property_view(prprty=self.office1, cycle=self.cycle2, site_eui=58, source_eui=59, total_ghg_emissions=480)
+        self.view21 = self.property_view_factory.get_property_view(prprty=self.office2, cycle=self.cycle2, site_eui=68, source_eui=59, total_ghg_emissions=380)
+        self.view22 = self.property_view_factory.get_property_view(prprty=self.retail3, cycle=self.cycle2, site_eui=63, source_eui=59, total_ghg_emissions=280)
+        self.view23 = self.property_view_factory.get_property_view(prprty=self.retail4, cycle=self.cycle2, site_eui=70, source_eui=59, total_ghg_emissions=330)
+
+    # calculating compliance metrics
+    def test_compliance_metric_get_data_permissions(self):
+
+        # login as root level owner/member and retrieve data
+        self.login_as_root_owner()
+        response = self.client.get(
+            reverse('api:v3:compliance_metrics-evaluate', args=[self.compliance_metric.id]) + '?organization_id=' + str(self.org.id),
+            content_type='application/json'
+        )
+        assert response.status_code == 200
+        data = json.loads(response.content)
+        self.assertEqual('success', data['status'])
+
+        properties_by_cycle = data['data']['properties_by_cycles'][str(self.cycle1.id)]
+        self.assertEqual(len(properties_by_cycle), 4)
+
+        self.login_as_root_member()
+        response = self.client.get(
+            reverse('api:v3:compliance_metrics-evaluate', args=[self.compliance_metric.id]) + '?organization_id=' + str(self.org.id),
+            content_type='application/json'
+        )
+        assert response.status_code == 200
+        data = json.loads(response.content)
+        self.assertEqual('success', data['status'])
+
+        properties_by_cycle = data['data']['properties_by_cycles'][str(self.cycle1.id)]
+        self.assertEqual(len(properties_by_cycle), 4)
+
+        # login as child and retrieve data
+        self.login_as_child_member()
+        response = self.client.get(
+            reverse('api:v3:compliance_metrics-evaluate', args=[self.compliance_metric.id]) + '?organization_id=' + str(self.org.id),
+            content_type='application/json'
+        )
+        assert response.status_code == 200
+        data = json.loads(response.content)
+        self.assertEqual('success', data['status'])
+
+        properties_by_cycle = data['data']['properties_by_cycles'][str(self.cycle1.id)]
+        self.assertEqual(len(properties_by_cycle), 2)
