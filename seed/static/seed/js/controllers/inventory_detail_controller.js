e1602a4f91d1d84d26311a005e81b699ec8b4ef3
Ross Perry <perryr16@gmail.com>
2023-07-13 07:08:43 -0600

    simplify ali path

--- seed/static/seed/js/controllers/inventory_detail_controller.js
+++ seed/static/seed/js/controllers/inventory_detail_controller.js
@@ -123,11 +123,16 @@ angular.module('BE.seed.controller.inventory_detail', [])
       $scope.historical_items = inventory_payload.history;
       $scope.item_state = inventory_payload.state;
       $scope.inventory_docs = $scope.inventory_type == 'properties' ? inventory_payload.property.inventory_documents : null;
-      const singular_type = $scope.inventory_type == 'properties' ? 'property' : 'taxlot'
-      let ali = inventory_payload[singular_type].access_level_instance
-      $scope.ali_path = typeof(ali) == 'object' ? ali.path : undefined
-      // the first key in the path (<org name>: 'root') is not necessary to display
-      $scope.ali_path && delete $scope.ali_path[$scope.organization.name]
+      const ali = $scope.inventory_type == 'properties' ?
+        inventory_payload.property.access_level_instance :
+        inventory_payload.taxlot.access_level_instance
+      
+      $scope.ali_path = {}
+      if (typeof(ali) == 'object') {
+        $scope.ali_path = ali.path 
+        // the first key in the path (<org name>: 'root') is not necessary to display
+        delete $scope.ali_path[$scope.organization.name]
+      }
 
       $scope.order_historical_items_with_scenarios = () => {
         $scope.historical_items_with_scenarios = $scope.historical_items ? $scope.historical_items.filter(item => !_.isEmpty(item.state.scenarios)) : []
