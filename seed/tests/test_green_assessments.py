3596cf0a0c1cd8dcd6b58b5c69626ba3aec6b87c
Hannah Eslinger <heslinge@nrel.gov>
2023-08-15 13:42:14 -0600

    Add permissions to GreenAssessmentURLViewSet

--- seed/tests/test_green_assessments.py
+++ seed/tests/test_green_assessments.py
@@ -11,7 +11,11 @@ import json
 
 from django.urls import reverse_lazy
 
-from seed.models import GreenAssessment
+from seed.models import (
+    GreenAssessment,
+    GreenAssessmentProperty,
+    GreenAssessmentURL
+)
 from seed.tests.util import AccessLevelBaseTestCase
 
 
@@ -76,6 +80,82 @@ class TestGreenAssessmentsPermissions(AccessLevelBaseTestCase):
     def test_green_assessments_patch(self):
         url = reverse_lazy('api:v3:green_assessments-detail', args=[self.green_assessment.pk]) + "?organization_id=" + str(self.org.id)
         params = json.dumps({"name": "boo", "recognition_type": "AWD", "is_numeric_score": True})
+
+        # child user cannot
+        self.login_as_child_member()
+        response = self.client.patch(url, params, content_type='application/json')
+        assert response.status_code == 403
+
+        # root users can
+        self.login_as_root_owner()
+        response = self.client.patch(url, params, content_type='application/json')
+        assert response.status_code == 200
+
+
+class TestGreenAssessmentUrlsPermissions(AccessLevelBaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.view = self.property_view_factory.get_property_view()
+        self.green_assessment = GreenAssessment.objects.create(organization_id=self.org.id, is_numeric_score=True)
+        self.property_assessment = GreenAssessmentProperty.objects.create(assessment=self.green_assessment, view=self.view)
+        self.green_assessment_url = GreenAssessmentURL.objects.create(property_assessment=self.property_assessment)
+
+    def test_green_assessment_urls_list(self):
+        url = reverse_lazy('api:v3:green_assessment_urls-list') + "?organization_id=" + str(self.org.id)
+
+        # child user cannot
+        self.login_as_child_member()
+        response = self.client.get(url, content_type='application/json')
+        assert response.status_code == 403
+
+        # root users can
+        self.login_as_root_owner()
+        response = self.client.get(url, content_type='application/json')
+        assert response.status_code == 200
+
+    def test_green_assessment_urls_get(self):
+        url = reverse_lazy('api:v3:green_assessment_urls-detail', args=[self.green_assessment_url.pk]) + "?organization_id=" + str(self.org.id)
+
+        # child user cannot
+        self.login_as_child_member()
+        response = self.client.get(url, content_type='application/json')
+        assert response.status_code == 403
+
+        # root users can
+        self.login_as_root_owner()
+        response = self.client.get(url, content_type='application/json')
+        assert response.status_code == 200
+
+    def test_green_assessment_urls_destroy(self):
+        url = reverse_lazy('api:v3:green_assessment_urls-detail', args=[self.green_assessment_url.pk]) + "?organization_id=" + str(self.org.id)
+
+        # child user cannot
+        self.login_as_child_member()
+        response = self.client.delete(url, content_type='application/json')
+        assert response.status_code == 403
+
+        # root users can
+        self.login_as_root_owner()
+        response = self.client.delete(url, content_type='application/json')
+        assert response.status_code == 204
+
+    def test_green_assessment_urls_put(self):
+        url = reverse_lazy('api:v3:green_assessment_urls-detail', args=[self.green_assessment_url.pk]) + "?organization_id=" + str(self.org.id)
+        params = json.dumps({"url": "http://whatever.com", "property_assessment": self.property_assessment.pk})
+
+        # child user cannot
+        self.login_as_child_member()
+        response = self.client.put(url, params, content_type='application/json')
+        assert response.status_code == 403
+
+        # root users can
+        self.login_as_root_owner()
+        response = self.client.put(url, params, content_type='application/json')
+        assert response.status_code == 200
+
+    def test_green_assessment_urls_patch(self):
+        url = reverse_lazy('api:v3:green_assessment_urls-detail', args=[self.green_assessment_url.pk]) + "?organization_id=" + str(self.org.id)
+        params = json.dumps({"url": "http://whatever.com", "property_assessment": self.property_assessment.pk})
 
         # child user cannot
         self.login_as_child_member()
