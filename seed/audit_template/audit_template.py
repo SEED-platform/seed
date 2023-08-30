1527530403a9c64846b0c5aedfd9c991669913ba
Ross Perry <perryr16@gmail.com>
2023-08-30 15:08:57 -0600

    base for batch export to at

--- seed/audit_template/audit_template.py
+++ seed/audit_template/audit_template.py
@@ -99,6 +99,131 @@ class AuditTemplate(object):
             raise validation_client.ValidationClientException(f"Expected JSON response from Audit Template: {response.text}")
 
         return response_body['token'], ""
+    
+    def batch_export_to_audit_template(self, states):
+        results = {'success': [], 'failure': []}
+        for state in states:
+            response, message = self.export_to_audit_template(state)
+            if response:
+                results['success'].append({state.id: response}) 
+            else:
+                results['failure'].append({state.id: message})
+        
+        return results
+
+    def export_to_audit_template(self, state):
+        org = Organization.objects.get(pk=self.org_id)
+        url = f'{self.API_URL}/building_sync/upload'
+        token, message = self.get_api_token()
+        if not token:
+            return None, message
+        
+        try:
+            xml_string, _ = self.build_xml(state, org.audit_template_report_type)
+            if not xml_string:
+                return None, 'Unable to create xml from property state'
+        except Exception as e:
+            return None, f'Unexpected error creating building xml {e}'
+
+        try:    
+            files = {'audit_file': ('at_export.xml', xml_string)}
+            body = {'token': token}
+            response = requests.post(url, data=body, files=files)
+            if response.status_code != 200:
+                return None, f'Expected 200 response from Audit Template upload but got {response.status_code}: {response.content}'
+        except Exception as e:
+            return None, f'Unexpected error from Audit Template: {e}'
+        
+        return response, ''
+    
+    def build_xml(self, state, report_type):
+        view = state.propertyview_set.first()
+
+        gfa = state.gross_floor_area
+        if type(gfa) == int:
+            gross_floor_area = str(gfa)
+        elif gfa.units != ureg.feet**2:
+            gross_floor_area = str(gfa.to(ureg.feet ** 2).magnitude)
+        else:
+            gross_floor_area = str(gfa.magnitude)
+
+        XSI_URI = 'http://www.w3.org/2001/XMLSchema-instance'
+        nsmap = {
+            'xsi': XSI_URI,
+        }
+        nsmap.update(NAMESPACES)
+        E = ElementMaker(
+            namespace=BUILDINGSYNC_URI,
+            nsmap=nsmap
+        )
+        doc = (
+            E.BuildingSync(
+                {
+                    etree.QName(XSI_URI,
+                                'schemaLocation'): 'http://buildingsync.net/schemas/bedes-auc/2019 https://raw.github.com/BuildingSync/schema/v2.3.0/BuildingSync.xsd',
+                    'version': '2.3.0'
+                },
+                E.Facilities(
+                    E.Facility(
+                        {'ID': 'Facility-69909846999990'},
+                        E.Sites(
+                            E.Site(
+                                {'ID': 'SiteType-69909846999991'},
+                                E.Buildings(
+                                    E.Building(
+                                        {'ID': 'BuildingType-69909846999992'},
+                                        E.PremisesName(state.property_name),
+                                        E.PremisesNotes('Note-1'),
+                                        E.PremisesIdentifiers(
+                                            E.PremisesIdentifier(
+                                                E.IdentifierLabel('Custom'),
+                                                E.IdentifierCustomName('SEED Property View ID'),
+                                                E.IdentifierValue(str(view.id))
+                                            )
+                                        ),
+                                        E.Address(
+                                            E.StreetAddressDetail(
+                                                E.Simplified(
+                                                    E.StreetAddress(state.address_line_1)
+                                                ),
+                                            ),
+                                            E.City(state.city),
+                                            E.State(state.state),
+                                            E.PostalCode(str(state.postal_code)),
+                                        ),
+                                        E.FloorAreas(
+                                            E.FloorArea(
+                                                E.FloorAreaType('Gross'),
+                                                E.FloorAreaValue(gross_floor_area),
+                                            ),
+                                        ),
+                                        E.YearOfConstruction(str(state.year_built)),
+                                    )
+                                )
+                            )
+                        ),
+                        E.Reports(
+                            E.Report(
+                                {'ID': "ReportType-69909846999993"},
+                                E.LinkedPremisesOrSystem(
+                                    E.Building(
+                                        E.LinkedBuildingID ({'IDref': "BuildingType-69909846999992"})
+                                    ),
+                                ),
+                                E.UserDefinedFields(
+                                    E.UserDefinedField(
+                                        E.FieldName('Audit Template Report Type'),
+                                        E.FieldValue(report_type),
+                                    ),
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+        return etree.tostring(doc, pretty_print=True).decode('utf-8'), []
 
 
 @shared_task
@@ -152,93 +277,3 @@ def _batch_get_building_xml(org_id, cycle_id, token, properties, progress_key):
     # Call the PropertyViewSet to update the property view with xml data
     property_view_set = PropertyViewSet()
     property_view_set.batch_update_with_building_sync(result, org_id, cycle_id, progress_data.key)
-
-
-def build_xml(state, report_type):
-    view = state.propertyview_set.first()
-
-    gfa = state.gross_floor_area
-    if type(gfa) == int:
-        gross_floor_area = str(gfa)
-    elif gfa.units != ureg.feet**2:
-        gross_floor_area = str(gfa.to(ureg.feet ** 2).magnitude)
-    else:
-        gross_floor_area = str(gfa.magnitude)
-
-    XSI_URI = 'http://www.w3.org/2001/XMLSchema-instance'
-    nsmap = {
-        'xsi': XSI_URI,
-    }
-    nsmap.update(NAMESPACES)
-    E = ElementMaker(
-        namespace=BUILDINGSYNC_URI,
-        nsmap=nsmap
-    )
-    doc = (
-        E.BuildingSync(
-            {
-                etree.QName(XSI_URI,
-                            'schemaLocation'): 'http://buildingsync.net/schemas/bedes-auc/2019 https://raw.github.com/BuildingSync/schema/v2.3.0/BuildingSync.xsd',
-                'version': '2.3.0'
-            },
-            E.Facilities(
-                E.Facility(
-                    {'ID': 'Facility-69909846999990'},
-                    E.Sites(
-                        E.Site(
-                            {'ID': 'SiteType-69909846999991'},
-                            E.Buildings(
-                                E.Building(
-                                    {'ID': 'BuildingType-69909846999992'},
-                                    E.PremisesName(state.property_name),
-                                    E.PremisesNotes('Note-1'),
-                                    E.PremisesIdentifiers(
-                                        E.PremisesIdentifier(
-                                            E.IdentifierLabel('Custom'),
-                                            E.IdentifierCustomName('SEED Property View ID'),
-                                            E.IdentifierValue(str(view.id))
-                                        )
-                                    ),
-                                    E.Address(
-                                        E.StreetAddressDetail(
-                                            E.Simplified(
-                                                E.StreetAddress(state.address_line_1)
-                                            ),
-                                        ),
-                                        E.City(state.city),
-                                        E.State(state.state),
-                                        E.PostalCode(str(state.postal_code)),
-                                    ),
-                                    E.FloorAreas(
-                                        E.FloorArea(
-                                            E.FloorAreaType('Gross'),
-                                            E.FloorAreaValue(gross_floor_area),
-                                        ),
-                                    ),
-                                    E.YearOfConstruction(str(state.year_built)),
-                                )
-                            )
-                        )
-                    ),
-                    E.Reports(
-                        E.Report(
-                            {'ID': "ReportType-69909846999993"},
-                            E.LinkedPremisesOrSystem(
-                                E.Building(
-                                    E.LinkedBuildingID ({'IDref': "BuildingType-69909846999992"})
-                                ),
-                            ),
-                            E.UserDefinedFields(
-                                E.UserDefinedField(
-                                    E.FieldName('Audit Template Report Type'),
-                                    E.FieldValue(report_type),
-                                ),
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-
-    return etree.tostring(doc, pretty_print=True).decode('utf-8'), []
