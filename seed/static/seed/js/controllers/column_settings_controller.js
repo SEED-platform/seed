5e6dff84a2cb18ba3a3917ca9453e6978735798e
Ross Perry <perryr16@gmail.com>
2023-09-12 14:12:59 -0600

    remove matching preview from frontend

--- seed/static/seed/js/controllers/column_settings_controller.js
+++ seed/static/seed/js/controllers/column_settings_controller.js
@@ -16,7 +16,6 @@ angular.module('BE.seed.controller.column_settings', [])
     'auth_payload',
     'columns_service',
     'modified_service',
-    'organization_service',
     'spinner_utility',
     'urls',
     'naturalSort',
@@ -35,7 +34,6 @@ angular.module('BE.seed.controller.column_settings', [])
       auth_payload,
       columns_service,
       modified_service,
-      organization_service,
       spinner_utility,
       urls,
       naturalSort,
@@ -123,7 +121,6 @@ angular.module('BE.seed.controller.column_settings', [])
         column.is_matching_criteria = !column.is_matching_criteria;
         $scope.setModified();
       };
-
 
       $scope.matching_status = function(column) {
         if (column.is_extra_data) return 'ineligible';
@@ -341,27 +338,6 @@ angular.module('BE.seed.controller.column_settings', [])
         $state.reload();
       };
 
-      $scope.complete_column_update = function () {
-        var matching_criteria_changed = _.find(_.values(diff), function (delta) {
-          return _.has(delta, 'is_matching_criteria');
-        });
-
-        if (matching_criteria_changed) {
-          // reset the spinner and run whole org match merge link
-          spinner_utility.show(undefined, $('.display')[0]);
-
-          organization_service.match_merge_link($scope.org.id, $scope.inventory_type).then(function (response) {
-            organization_service.check_match_merge_link_status(response.progress_key).then(function (completion_notice) {
-              organization_service.get_match_merge_link_result($scope.org.id, completion_notice.unique_id).then(column_update_complete);
-            });
-          }).catch(function () {
-            Notification.error('There was an error trying to match, merge, link records for this organization.');
-          });
-        } else {
-          column_update_complete();
-        }
-      };
-
       // Saves the modified columns
       $scope.save_settings = function () {
         $scope.columns_updated = false;
@@ -389,7 +365,7 @@ angular.module('BE.seed.controller.column_settings', [])
           });
 
           spinner_utility.show();
-          $q.all(promises).then($scope.complete_column_update, function (data) {
+          $q.all(promises).then(column_update_complete, function (data) {
             $scope.$emit('app_error', data);
           });
         }).catch(function () { // User cancelled
--- seed/static/seed/js/services/organization_service.js
+++ seed/static/seed/js/services/organization_service.js
@@ -166,40 +166,10 @@ angular.module('BE.seed.service.organization', []).factory('organization_service
       });
     };
 
-    organization_factory.match_merge_link = function (org_id, inventory_type) {
-      return $http.post('/api/v3/organizations/' + org_id + '/match_merge_link/', {
-        inventory_type: inventory_type
-      }).then(function (response) {
-        return response.data;
-      });
-    };
-
     organization_factory.geocoding_columns = function (org_id) {
       return $http.get('/api/v3/organizations/' + org_id + '/geocoding_columns/').then(function (response) {
         return response.data;
       });
-    };
-
-    organization_factory.match_merge_link_preview = function (org_id, inventory_type, criteria_change_columns) {
-      return $http.post('/api/v3/organizations/' + org_id + '/match_merge_link_preview/', {
-        inventory_type: inventory_type,
-        add: criteria_change_columns.add,
-        remove: criteria_change_columns.remove
-      }).then(function (response) {
-        return response.data;
-      });
-    };
-
-    organization_factory.get_match_merge_link_result = function (org_id, match_merge_link_id) {
-      return $http.get('/api/v3/organizations/' + org_id + '/match_merge_link_result/' + '?match_merge_link_id=' + match_merge_link_id).then(function (response) {
-        return response.data;
-      });
-    };
-
-    organization_factory.check_match_merge_link_status = function (progress_key) {
-      var deferred = $q.defer();
-      checkStatusLoop(deferred, progress_key);
-      return deferred.promise;
     };
 
     organization_factory.reset_all_passwords = function (org_id) {
