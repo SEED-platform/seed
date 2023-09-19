6b7d800a1a558005abef93189a29fdecad2daf89
Ross Perry <perryr16@gmail.com>
2023-09-19 10:53:06 -0600

    remove commit from outside branch

--- seed/models/events.py
+++ seed/models/events.py
@@ -17,7 +17,7 @@ class Event(TimeStampedModel):
 
 
 class ATEvent(Event):
-    building_file = models.ForeignKey("BuildingFile", on_delete=models.CASCADE)
+    building_file = models.ForeignKey("BuildingFile", on_delete=models.PROTECT)
     audit_date = models.CharField(max_length=100, null=True)
     # has a one to many with Scenario
 
