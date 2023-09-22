7bc16d27a0d195e603a548c62e9b7c97239b064d
Nicholas Long <nicholas.long@nrel.gov>
2023-09-22 10:06:24 -0600

    oops, missed this formatting

--- seed/management/commands/add_eeej_datasources.py
+++ seed/management/commands/add_eeej_datasources.py
@@ -2,7 +2,6 @@
 SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
 See also https://github.com/seed-platform/seed/main/LICENSE.md
 """
-
 from django.core.management.base import BaseCommand
 
 from seed.lib.geospatial.eeej import add_eeej_data
