0d90c8efc7fc75ea4d3acd77dd5922861eb2be3d
Hannah Eslinger <heslinge@nrel.gov>
2023-09-26 15:37:13 -0600

    Add permissions to GeocodeViewSet (#4241)

--- seed/views/v3/geocode.py
+++ seed/views/v3/geocode.py
@@ -12,6 +12,7 @@ from rest_framework.decorators import action
 
 from seed.decorators import ajax_request_class
 from seed.lib.superperms.orgs.decorators import has_perm_class
+from seed.lib.superperms.orgs.models import AccessLevelInstance
 from seed.models.properties import PropertyState, PropertyView
 from seed.models.tax_lots import TaxLotState, TaxLotView
 from seed.utils.api import OrgMixin, api_endpoint_class
@@ -41,13 +42,16 @@ class GeocodeViewSet(viewsets.ViewSet, OrgMixin):
         """
         body = dict(request.data)
         org_id = self.get_organization(request)
+        access_level_instance = AccessLevelInstance.objects.get(pk=request.access_level_instance_id)
         property_view_ids = body.get('property_view_ids')
         taxlot_view_ids = body.get('taxlot_view_ids')
 
         if property_view_ids:
             property_views = PropertyView.objects.filter(
                 id__in=property_view_ids,
-                cycle__organization_id=org_id
+                cycle__organization_id=org_id,
+                property__access_level_instance__lft__gte=access_level_instance.lft,
+                property__access_level_instance__rgt__lte=access_level_instance.rgt,
             )
             properties = PropertyState.objects.filter(
                 id__in=Subquery(property_views.values('state_id'))
@@ -57,7 +61,9 @@ class GeocodeViewSet(viewsets.ViewSet, OrgMixin):
         if taxlot_view_ids:
             taxlot_views = TaxLotView.objects.filter(
                 id__in=taxlot_view_ids,
-                cycle__organization_id=org_id
+                cycle__organization_id=org_id,
+                taxlot__access_level_instance__lft__gte=access_level_instance.lft,
+                taxlot__access_level_instance__rgt__lte=access_level_instance.rgt,
             )
             taxlots = TaxLotState.objects.filter(
                 id__in=Subquery(taxlot_views.values('state_id'))
@@ -87,6 +93,7 @@ class GeocodeViewSet(viewsets.ViewSet, OrgMixin):
         """
         body = dict(request.data)
         org_id = self.get_organization(request)
+        access_level_instance = AccessLevelInstance.objects.get(pk=request.access_level_instance_id)
         property_view_ids = body.get('property_view_ids')
         taxlot_view_ids = body.get('taxlot_view_ids')
 
@@ -95,7 +102,9 @@ class GeocodeViewSet(viewsets.ViewSet, OrgMixin):
         if property_view_ids:
             property_views = PropertyView.objects.filter(
                 id__in=property_view_ids,
-                cycle__organization_id=org_id
+                cycle__organization_id=org_id,
+                property__access_level_instance__lft__gte=access_level_instance.lft,
+                property__access_level_instance__rgt__lte=access_level_instance.rgt,
             )
             result["properties"] = {
                 'not_geocoded': PropertyState.objects.filter(
@@ -123,7 +132,9 @@ class GeocodeViewSet(viewsets.ViewSet, OrgMixin):
         if taxlot_view_ids:
             taxlot_views = TaxLotView.objects.filter(
                 id__in=taxlot_view_ids,
-                cycle__organization_id=org_id
+                cycle__organization_id=org_id,
+                taxlot__access_level_instance__lft__gte=access_level_instance.lft,
+                taxlot__access_level_instance__rgt__lte=access_level_instance.rgt,
             )
             result["tax_lots"] = {
                 'not_geocoded': TaxLotState.objects.filter(
