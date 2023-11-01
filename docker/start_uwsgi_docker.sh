f6dbad06865b9545d5f8d557e723079f9e52a3b8
Damon Haley <damon.haley@nrel.gov>
2023-11-01 13:33:31 -0600

    disable wait-for-it in Stratus - managed services are always running.

--- docker/start_uwsgi_docker.sh
+++ docker/start_uwsgi_docker.sh
@@ -2,24 +2,29 @@
 
 cd /seed
 
-echo "Waiting for postgres to start"
-if [ -v POSTGRES_HOST ];
-then
-   POSTGRES_ACTUAL_HOST=$POSTGRES_HOST
+# Check if 'STRATUS_MANAGED_SERVICES_ENABLED' is not set or if its value is 'TRUE'
+if [[ "${STRATUS_MANAGED_SERVICES_ENABLED}" == "on" ]]; then
+    echo "'STRATUS_MANAGED_SERVICES_ENABLED' is set and not equal to 'on'. Skipping wait-for-it.sh execution."
 else
-   POSTGRES_ACTUAL_HOST=db-postgres
-fi
-/usr/local/wait-for-it.sh --strict $POSTGRES_ACTUAL_HOST:$POSTGRES_PORT
+    cd /seed
 
-echo "Waiting for redis to start"
-if [ -v REDIS_HOST ];
-then
-    REDIS_ACTUAL_HOST=$REDIS_HOST
-else
-    REDIS_ACTUAL_HOST=db-redis
-fi
+    echo "Waiting for postgres to start"
+    if [ -v POSTGRES_HOST ]; then
+        POSTGRES_ACTUAL_HOST=$POSTGRES_HOST
+    else
+        POSTGRES_ACTUAL_HOST=db-postgres
+    fi
+    /usr/local/wait-for-it.sh --strict $POSTGRES_ACTUAL_HOST:$POSTGRES_PORT
 
-/usr/local/wait-for-it.sh --strict $REDIS_ACTUAL_HOST:6379
+    echo "Waiting for redis to start"
+    if [ -v REDIS_HOST ]; then
+        REDIS_ACTUAL_HOST=$REDIS_HOST
+    else
+        REDIS_ACTUAL_HOST=db-redis
+    fi
+
+    /usr/local/wait-for-it.sh --strict $REDIS_ACTUAL_HOST:6379
+fi
 
 # collect static resources before starting and compress the assets
 ./manage.py collectstatic --no-input -i package.json -i npm-shrinkwrap.json -i node_modules/openlayers-ext/index.html
