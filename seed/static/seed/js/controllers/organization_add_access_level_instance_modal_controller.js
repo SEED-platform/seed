4f58dcc30c8f0d676b687b10172494c2559637e9
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2024-01-24 12:11:35 -0700

    AH UI Fixes for access level modals (#4501)
    * ah ui fixes
    
    * Small fixes
    
    ---------
    
    Co-authored-by: Alex Swindler <Alex.Swindler@nrel.gov>

--- seed/static/seed/js/controllers/organization_add_access_level_instance_modal_controller.js
+++ seed/static/seed/js/controllers/organization_add_access_level_instance_modal_controller.js
@@ -25,8 +25,9 @@ angular.module('BE.seed.controller.organization_add_access_level_instance_modal'
       $scope.level_names = level_names;
       $scope.selected_level_index = null;
       $scope.parent = null;
-      $scope.potental_parents = [];
+      $scope.potential_parents = [];
       $scope.new_level_instance_name = '';
+
 
       /* Build out access_level_instances_by_depth recursively */
       const access_level_instances_by_depth = {};
@@ -41,10 +42,16 @@ angular.module('BE.seed.controller.organization_add_access_level_instance_modal'
       calculate_access_level_instances_by_depth(access_level_tree, 1);
 
       $scope.change_selected_level_index = function () {
-        const new_level_instance_depth = parseInt($scope.selected_level_index);
-        $scope.potental_parents = access_level_instances_by_depth[new_level_instance_depth];
+        const new_level_instance_depth = parseInt($scope.selected_level_index, 10);
+        $scope.potential_parents = access_level_instances_by_depth[new_level_instance_depth];
         $scope.parent = null;
       };
+
+      // attempt to default the access level
+      if ($scope.level_names.length > 1){
+        $scope.selected_level_index = 1;
+        $scope.change_selected_level_index();
+      }
 
       $scope.create_new_level_instance = function () {
         organization_service.create_organization_access_level_instance(org_id, $scope.parent.id, $scope.new_level_instance_name)
