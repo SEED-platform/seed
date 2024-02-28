7d72d83cfac2a6ebb75cb01ce57a2f890ba2813b
kflemin <2205659+kflemin@users.noreply.github.com>
2024-02-28 14:31:37 -0700

    fix test

--- seed/tests/test_analysis_pipelines.py
+++ seed/tests/test_analysis_pipelines.py
@@ -1170,7 +1170,7 @@ class TestEeejPipeline(TestCase):
 
         apv_ids = [self.property_view_dac.id, self.property_view_not_dac.id]
         apvs = [self.property_view_dac, self.property_view_not_dac]
-        loc_data_by_property_view, errors_by_property_view_id = _get_data_for_census_tract_fetch(apv_ids, self.org)
+        loc_data_by_property_view, errors_by_property_view_id = _get_data_for_census_tract_fetch(apv_ids, self.org, True)
         results, errors_by_apv_id = _get_eeej_indicators(apvs, loc_data_by_property_view)
         self.assertEqual(len(results), 2)
         # DAC
