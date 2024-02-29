71cd2de8089a6a134f673f0f8b4cb51ae6e04639
Ross Perry <perryr16@gmail.com>
2024-02-28 17:56:50 -0700

    precommit

--- seed/static/seed/js/controllers/portfolio_summary_controller.js
+++ seed/static/seed/js/controllers/portfolio_summary_controller.js
@@ -211,7 +211,7 @@ angular.module('BE.seed.controller.portfolio_summary', [])
 
           get_paginated_properties(page, per_page, cycle_priority[1], access_level_instance_id, false, property_ids).then((result1) => {
             properties = result1.results;
-            // if results0 returns fewer (or no) properties than retults1, use results1 for ui-grid oconfig
+            // if results0 returns fewer (or no) properties than retults1, use results1 for ui-grid config
             if (result1.pagination.num_pages > $scope.inventory_pagination.num_pages) {
               baseline_first = !baseline_first
               $scope.inventory_pagination = result1.pagination
