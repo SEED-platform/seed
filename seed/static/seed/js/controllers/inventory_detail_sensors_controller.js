e23e8541f246ad032c03bbe30b86b617b56cbb8b
kflemin <2205659+kflemin@users.noreply.github.com>
2024-03-18 13:44:23 -0600

    make delete button consistent across seed

--- seed/static/seed/js/controllers/inventory_detail_sensors_controller.js
+++ seed/static/seed/js/controllers/inventory_detail_sensors_controller.js
@@ -126,10 +126,10 @@ angular.module('BE.seed.controller.inventory_detail_sensors', []).controller('in
         displayName: 'Actions',
         enableHiding: false,
         cellTemplate:
-          '<div style="display: flex; justify-content: space-around; align-content: center">' +
+          '<div style="display: flex; justify-content: space-around; align-content: center;margin:2px;">' +
           '<button type="button" ng-show="grid.appScope.menu.user.organization.user_role !== \'viewer\'" class="btn-primary" style="border-radius: 4px;" ng-click="grid.appScope.open_sensors_upload_modal(row.entity)" translate>UPLOAD_SENSORS_BUTTON</button>' +
           '<button type="button" ng-show="grid.appScope.menu.user.organization.user_role !== \'viewer\'" class="btn-primary" style="border-radius: 4px;" ng-click="grid.appScope.open_sensor_readings_upload_modal(row.entity)" translate>UPLOAD_SENSOR_READINGS_BUTTON</button>' +
-          '<button type="button" ng-show="grid.appScope.menu.user.organization.user_role !== \'viewer\'" class="btn-primary" style="border-radius: 4px;" ng-click="grid.appScope.open_delete_data_logger_modal(row.entity)" ><i class="fa-solid fa-lg fa-trash"></button>' +
+          '<button type="button" ng-show="grid.appScope.menu.user.organization.user_role !== \'viewer\'" class="btn-danger" style="border-radius: 4px; aria-label="Delete" ng-click="grid.appScope.open_delete_data_logger_modal(row.entity)" ><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>' +
           '</div>',
         enableColumnMenu: false,
         enableColumnMoving: false,
