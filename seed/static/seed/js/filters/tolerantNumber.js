900022596c592af6be674492ef5c68e29b9e0035
Alex Swindler <alex.swindler@nrel.gov>
2024-03-20 11:33:53 -0600

    Fix broken license URLs (#4578)
    Fix broken license urls

--- seed/static/seed/js/filters/tolerantNumber.js
+++ seed/static/seed/js/filters/tolerantNumber.js
@@ -1,6 +1,6 @@
 /**
  * SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
- * See also https://github.com/seed-platform/seed/main/LICENSE.md
+ * See also https://github.com/SEED-platform/seed/blob/main/LICENSE.md
  *
  * TolerantNumber
  * Convert to number with comma separators and fixed decimals if possible, otherwise return input
--- seed/static/seed/js/services/salesforce_mapping_service.js
+++ seed/static/seed/js/services/salesforce_mapping_service.js
@@ -1,6 +1,6 @@
 /**
  * SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
- * See also https://github.com/seed-platform/seed/main/LICENSE.md
+ * See also https://github.com/SEED-platform/seed/blob/main/LICENSE.md
  */
 angular.module('BE.seed.service.salesforce_mapping', []).factory('salesforce_mapping_service', [
   '$http',
