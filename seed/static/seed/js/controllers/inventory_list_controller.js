b13d89291c9ed6dbd8e097d4ce2ec1ec65b1c206
Hannah Eslinger <heslinge@nrel.gov>
2024-03-27 09:25:16 -0600

    Empty label section remains same size when toggled (#4588)
    * Empty label section remains same size when toggled
    
    * Added fix to the Portfolio Summary page, fixed pixel-perfect layout
    
    ---------
    
    Co-authored-by: Alex Swindler <Alex.Swindler@nrel.gov>

--- seed/static/seed/js/controllers/inventory_list_controller.js
+++ seed/static/seed/js/controllers/inventory_list_controller.js
@@ -451,7 +451,7 @@ angular.module('BE.seed.controller.inventory_list', []).controller('inventory_li
     $scope.max_label_width = 750;
     $scope.get_label_column_width = () => {
       if (!$scope.show_full_labels) {
-        return 30;
+        return 31;
       }
       let maxWidth = 0;
       const renderContainer = document.body.getElementsByClassName('ui-grid-render-container-left')[0];
@@ -467,7 +467,8 @@ angular.module('BE.seed.controller.inventory_list', []).controller('inventory_li
           }
         });
       });
-      return maxWidth > $scope.max_label_width ? $scope.max_label_width : maxWidth + 2;
+      maxWidth = Math.max(31, maxWidth + 2);
+      return Math.min(maxWidth, $scope.max_label_width);
     };
 
     // Reduce labels to only records found in the current cycle
