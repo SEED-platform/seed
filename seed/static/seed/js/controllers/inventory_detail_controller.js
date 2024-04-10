56e699482ac3c2684cfcee39083cea279648689e
Ross Perry <perryr16@gmail.com>
2024-04-10 15:32:54 -0600

    hide form

--- seed/static/seed/js/controllers/inventory_detail_controller.js
+++ seed/static/seed/js/controllers/inventory_detail_controller.js
@@ -453,6 +453,7 @@ angular.module('BE.seed.controller.inventory_detail', []).controller('inventory_
      */
     $scope.on_save = () => {
       $scope.open_match_merge_link_warning_modal($scope.save_item, 'edit');
+      const x = 10
     };
 
     const notify_merges_and_links = (result) => {
@@ -475,7 +476,7 @@ angular.module('BE.seed.controller.inventory_detail', []).controller('inventory_
      * save_item: saves the user's changes to the Property/TaxLot State object.
      */
     const save_item_resolve = (data) => {
-      $scope.$emit('finished_saving');
+      $scope.edit_form_showing = false;
       notify_merges_and_links(data);
       if (data.view_id) {
         reload_with_view_id(data.view_id);
@@ -488,7 +489,7 @@ angular.module('BE.seed.controller.inventory_detail', []).controller('inventory_
     };
 
     const save_item_reject = () => {
-      $scope.$emit('finished_saving');
+      $scope.edit_form_showing = false;
     };
 
     const save_item_catch = (data) => {
@@ -497,9 +498,9 @@ angular.module('BE.seed.controller.inventory_detail', []).controller('inventory_
 
     $scope.save_item = () => {
       if ($scope.inventory_type === 'properties') {
-        inventory_service.update_property($scope.inventory.view_id, $scope.diff()).then(save_item_resolve, save_item_reject).catch(save_item_catch);
+        inventory_service.update_property($scope.inventory.view_id, $scope.diff()).then(save_item_resolve, save_item_reject).then(() => $scope.edit_form_showing=false).catch(save_item_catch);
       } else if ($scope.inventory_type === 'taxlots') {
-        inventory_service.update_taxlot($scope.inventory.view_id, $scope.diff()).then(save_item_resolve, save_item_reject).catch(save_item_catch);
+        inventory_service.update_taxlot($scope.inventory.view_id, $scope.diff()).then(save_item_resolve, save_item_reject).then(() => $scope.edit_form_showing=false).catch(save_item_catch);
       }
     };
 
@@ -754,6 +755,7 @@ angular.module('BE.seed.controller.inventory_detail', []).controller('inventory_
       });
 
       modalInstance.result.then(accept_action, () => {
+        console.log('a')
         // Do nothing if cancelled
       });
     };
