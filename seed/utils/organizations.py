613f91cb27628c1e5f6acfb815c02f056ff075e9
Ross Perry <perryr16@gmail.com>
2024-04-12 12:10:12 -0600

    refactor for cycles

--- seed/utils/organizations.py
+++ seed/utils/organizations.py
@@ -24,7 +24,7 @@ from seed.models import (
     Column,
     ColumnMappingProfile,
     PropertyState,
-    TaxLotState
+    TaxLotState,
 )
 from seed.models.data_quality import DataQualityCheck
 
@@ -199,116 +199,105 @@ def public_feed(org, request):
     """
     base_url = request.build_absolute_uri('/')
     params = request.query_params
-    page = get_int(params.get('page'), 1)
-    per_page = get_int(params.get('per_page'), 100)
+    page = _get_int(params.get('page'), 1)
+    per_page = _get_int(params.get('per_page'), 100)
     label_filter = params.get('label_filter', None)
-    properties_param = params.get('properties', True)
-    taxlots_param = params.get('taxlots', False)
-    cycles = params.get('cycles', False)
+    properties_param = params.get('properties', 'true').lower() == 'true'
+    taxlots_param = params.get('taxlots', 'true').lower() == 'true'
+    cycles = params.get('cycles', None)
+    if cycles is not None:
+        cycles = cycles.split(',')
+    else: 
+        cycles = list(org.cycles.values_list('id', flat=True))
+
+    data = {}
+    p_count = 0
+    t_count = 0
 
     if properties_param:
-        p_states = org.propertystate_set.filter(propertyview__isnull=False).order_by('-updated')
+        data['properties'], p_count = _add_states_to_data(base_url, PropertyState, 'propertyview', page, per_page, label_filter, cycles)
+      
     if taxlots_param:
-        t_states = org.taxlotstate_set.filter(taxlotview__isnull=False).order_by('-updated')
+        data['taxlots'], t_count = _add_states_to_data(base_url, TaxLotState, 'taxlotview', page, per_page, label_filter, cycles)
 
-    if label_filter is not None:
-        p_states = p_states.filter(propertyview__labels__name__in=[label_filter])
-        t_states = t_states.filter(taxlotview__labels__name__in=[label_filter])
 
-    p_states_paginated = paginate_states(p_states, page, per_page)
-    t_states_paginated = paginate_states(t_states, page, per_page)
-
-    max_count = p_states.count() if p_states.count() > t_states.count() else t_states.count()
     pagination = {
         'page': page,
-        'total_pages': int(max_count / per_page) + 1,
+        'total_pages': int(max(p_count, t_count) / per_page) + 1,
         'per_page': per_page,
     }
 
     if properties_param:
-        pagination['properties'] = p_states.count(),
+        pagination['properties'] = p_count
     if taxlots_param:
-        pagination['taxlots'] = t_states.count(),
+        pagination['taxlots'] = t_count
 
-    # gonna need public columns for properties and taxlots and extra data boolean
-    p_public_columns = (
-        Column.objects.filter(shared_field_type=1, table_name='PropertyState')
-        .annotate(column_name_lower=Lower('column_name'))
-        .order_by('column_name_lower')
-        .values_list('column_name', 'is_extra_data')
-    )
-    t_public_columns = (
-        Column.objects.filter(shared_field_type=1, table_name='TaxLotState')
-        .annotate(column_name_lower=Lower('column_name'))
-        .order_by('column_name_lower')
-        .values_list('column_name', 'is_extra_data')
-    )
-
-    data = {}
-    if properties_param:
-        data['properties'] = []
-        for p_state in p_states_paginated:
-            # what if matching criteria dne? a whole bunch of None's
-            add_state_to_data(base_url, data['properties'], p_state.propertyview_set.first(), p_state, p_public_columns)
-
-    if taxlots_param:
-        data['taxlots'] = []
-        for t_state in t_states_paginated:
-            add_state_to_data(base_url, data['taxlots'], t_state.taxlotview_set.first(), t_state, t_public_columns)
-
-    # RIGHT HERE. make sure properties and taxlots param works 
-    # then do cycles
-    # then do documentaiton
     return {
         'pagination': pagination, 
-        'organization': {'id': org.id, 'name':org.name},
         'query_params': {
             'label_filter': label_filter,
             'cycles': cycles if cycles else 'all',
             'properties': properties_param,
             'taxlots': taxlots_param
         },
-        'label_filter': label_filter,
+        'organization': {'id': org.id, 'name':org.name},
         'data': data
     }
 
 
-def paginate_states(states, page, per_page):
+def _add_states_to_data(base_url, StateClass, view_string, page, per_page, label_filter, cycles):
+    states = StateClass.objects.filter(**{f'{view_string}__cycle__in': cycles}).order_by('-updated')
+
+    if label_filter is not None:
+        states = states.filter(**{f'{view_string}__labels__name__in': [label_filter]})
+
     paginator = Paginator(states, per_page)
     try:
-        return paginator.page(page)
+        states_paginated = paginator.page(page)
     except EmptyPage:
-        return paginator.page(paginator.num_pages)
+        states_paginated = paginator.page(paginator.num_pages)
+
+    public_columns = (
+        Column.objects.filter(shared_field_type=1, table_name=StateClass._meta.object_name)
+        .annotate(column_name_lower=Lower('column_name'))
+        .order_by('column_name_lower')
+        .values_list('column_name', 'is_extra_data')
+    )
+
+    data = []
+    for state in states_paginated:
+        view = getattr(state, f'{view_string}_set').first()
+
+        state_data = {
+            'id': view.id,
+            'cycle': view.cycle.name,
+            'updated': state.updated,
+            'created': state.created,
+            'labels': ', '.join(view.labels.all().values_list('name', flat=True))
+        }
+
+        for (name, extra_data) in public_columns:
+            if name in ['updated', 'created']:
+                continue
+            if not extra_data:
+                value = getattr(state, name, None)
+            else:
+                value = state.extra_data.get(name, None)
+            if isinstance(value, pint.Quantity):
+                # convert pint to string with units (json cannot display exponents)
+                value = f'{value.m} {value.u}'
+
+            state_data[name] = value
+
+        state_data['json_link'] = f'{base_url}api/v3/{"properties" if type(state) == PropertyState else "taxlots"}/{view.id}/?organization_id={view.cycle.organization.id}'
+        state_data['html_link'] = f'{base_url}app/#/{"properties" if type(state) == PropertyState else "taxlots"}/{view.id}'
+
+        data.append(state_data)
+
+    return data, len(states)
 
 
-def add_state_to_data(base_url, data, view, state, public_columns):
-    state_data = {
-        'id': view.id,
-        'cycle': view.cycle.name,
-        'updated': state.updated,
-        'created': state.created,
-        'labels': ', '.join(view.labels.all().values_list('name', flat=True))
-    }
-    for (name, extra_data) in public_columns:
-        if name in ['updated', 'created']:
-            continue
-        if not extra_data:
-            value = getattr(state, name, None)
-        else:
-            value = state.extra_data.get(name, None)
-        if isinstance(value, pint.Quantity):
-            # convert pint to string with units (json cannot display exponents)
-            value = f'{value.m} {value.u}'
-
-        state_data[name] = value
-
-    state_data['json_link'] = f'{base_url}api/v3/{"properties" if type(state) == PropertyState else "taxlots"}/{view.id}/?organization_id={view.cycle.organization.id}'
-    state_data['html_link'] = f'{base_url}app/#/{"properties" if type(state) == PropertyState else "taxlots"}/{view.id}'
-
-    data.append(state_data)
-
-
-def get_int(value, default):
+def _get_int(value, default):
     try:
         result = int(float(value))
         return result if result > 0 else default
