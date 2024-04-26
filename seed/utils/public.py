55bc8d5c4efbfb568efb4270d1fc4d5203c34dcb
Ross Perry <perryr16@gmail.com>
2024-04-26 13:33:44 -0600

    refactor

--- seed/utils/public.py
+++ seed/utils/public.py
@@ -1,11 +1,12 @@
 import pint
 from django.core.paginator import EmptyPage, Paginator
 from django.db.models.functions import Lower
+from urllib.parse import urlencode
 
 from seed.models import Column, PropertyState, TaxLotState
 
 
-def public_feed(org, request, flat_cycle=False):
+def public_feed(org, request, html_view=False):
     """
     Format all property and taxlot state data to be displayed on a public feed
     """
@@ -28,31 +29,37 @@ def public_feed(org, request, flat_cycle=False):
     p_count = 0
     t_count = 0
     if properties_param:
-        data["properties"], p_count = _add_states_to_data(base_url, PropertyState, "propertyview", page, per_page, labels, cycles, org.id, flat_cycle)
+        data["properties"], p_count = _add_states_to_data(base_url, PropertyState, "propertyview", page, per_page, labels, cycles, org.id, html_view)
 
     if taxlots_param:
-        data["taxlots"], t_count = _add_states_to_data(base_url, TaxLotState, "taxlotview", page, per_page, labels, cycles, org.id, flat_cycle)
+        data["taxlots"], t_count = _add_states_to_data(base_url, TaxLotState, "taxlotview", page, per_page, labels, cycles, org.id, html_view)
 
     pagination = {
         "page": page,
         "total_pages": int(max(p_count, t_count) / per_page) + 1,
         "per_page": per_page,
     }
+    if not html_view:
+        organization =  {"id": org.id, "name": org.name}
+    else: 
+        organization = {"organization_id": org.id, "organization_name": org.name}
+
 
     if properties_param:
-        pagination["properties"] = p_count
+        pagination["property_count"] = p_count
     if taxlots_param:
-        pagination["taxlots"] = t_count
+        pagination["taxlot_count"] = t_count
+
 
     return {
         "pagination": pagination,
-        "query_params": {"labels": labels, "cycles": cycles if cycles else "all", "properties": properties_param, "taxlots": taxlots_param},
-        "organization": {"id": org.id, "name": org.name},
+        "query_params": {"labels": labels, "cycle_ids": cycles if cycles else "all", "properties": properties_param, "taxlots": taxlots_param},
+        "organization": organization,
         "data": data,
     }
 
 
-def _add_states_to_data(base_url, state_class, view_string, page, per_page, labels, cycles, org_id, flat_cycle=False):
+def _add_states_to_data(base_url, state_class, view_string, page, per_page, labels, cycles, org_id, html_view=False):
     states = state_class.objects.filter(**{f"{view_string}__cycle__in": cycles}).order_by("-updated")
 
     if labels is not None:
@@ -76,7 +83,7 @@ def _add_states_to_data(base_url, state_class, view_string, page, per_page, labe
         view = getattr(state, f"{view_string}_set").first()
 
         state_data = {"id": view.id} 
-        if flat_cycle:
+        if html_view:
             state_data["cycle_id"] = view.cycle.id
             state_data["cycle_name"] = view.cycle.name 
         else:
@@ -100,10 +107,13 @@ def _add_states_to_data(base_url, state_class, view_string, page, per_page, labe
 
             state_data[name] = value
 
-        state_data["json_link"] = (
-            f'{base_url}api/v3/{"properties" if type(state) == PropertyState else "taxlots"}/{view.id}/?organization_id={view.cycle.organization.id}'
-        )
-        state_data["html_link"] = f'{base_url}app/#/{"properties" if type(state) == PropertyState else "taxlots"}/{view.id}'
+        json_link = f'{base_url}api/v3/{"properties" if type(state) == PropertyState else "taxlots"}/{view.id}/?organization_id={view.cycle.organization.id}'
+        html_link = f'{base_url}app/#/{"properties" if type(state) == PropertyState else "taxlots"}/{view.id}'
+        if not html_view:
+            state_data["json_link"] = json_link
+            state_data["html_link"] = html_link
+        else:
+            state_data["links"] = f"<p><a href={json_link} target='_blank'>JSON</a>, <a href={html_link} target='_blank'>HTML</a></p>"
 
         data.append(state_data)
 
@@ -116,3 +126,123 @@ def _get_int(value, default):
         return result if result > 0 else default
     except (ValueError, TypeError):
         return default
+    
+def dict_to_table(data, title):
+    if not len(data):
+        return f"<div class='title'>{title}: None</div>"
+    
+    html = f"<div class='title'>{title}</div>\n<table>\n"
+    headers = data[0].keys()
+    header_row = '<tr>' + ''.join(f'<th>{header}</th>' for header in headers) + '</tr>\n'
+    html += header_row
+    for datum in data:
+        row = '<tr>' + ''.join(f'<td>{datum[header]}</td>' for header in headers) + '<tr/>\n'
+        html += row
+    html += '</table>'
+
+    return html
+
+def page_navigation_link(base_url, pagination, query_params, next_page):
+    page = pagination['page']
+    total_pages = pagination['total_pages']
+
+    if next_page:
+        action_text = "Next"
+        query_params['page'] = page + 1 if page < total_pages else total_pages
+        condition = page < total_pages
+    else:
+        action_text = "Previous"
+        query_params['page'] = page -1 if page > 1 else 1
+        condition = page > 1
+
+    if condition:
+        return f"<a type='button' href='{base_url}?{urlencode(query_params)}'>{action_text}</a>"
+    else:
+        return ""
+
+
+PUBLIC_HTML_DISABLED = """                
+    <html>
+        <div style="'PT Sans Narrow', 'Helvetica Neue', helvetica, arial, sans-serif;"> 
+            Public feed is not enabled for organization '{org.name}'. Public feed can be enabled in
+            <a href="/app/#/accounts/{org.id}" target="_blank">organization settings</a>.
+        </div>
+    </html>
+"""
+
+PUBLIC_HTML_HEADER = """
+    <div class="logo_container">
+        <a class="logo" href="/">
+        <div class="logo_text">
+            <span class="logo_text_seed">Seed</span>
+            <span class="logo_text_platform">Platform™</span>
+        </div>
+        </a>
+    </div>
+"""
+
+PUBLIC_HTML_STYLE = """
+            body {
+                font-family: 'PT Sans Narrow', 'Helvetica Neue', helvetica, arial, sans-serif;
+                font-weight: normal;
+                margin: 0;
+            }
+            .logo_container {
+                display: flex;
+                height: 50px;
+                background: #dcdcdc;
+
+                .logo {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    color: black;
+
+                    .logo_text {
+                        padding: 0 20px;
+                        font-size: 24px;
+                        font-family: 'PT Sans Narrow', 'Helvetica Neue', helvetica, arial, sans-serif;
+                        font-weight: normal;
+                        text-transform: uppercase;
+
+                        .logo_text_seed {
+                        font-family: 'PT Sans', 'Helvetica Neue', helvetica, arial, sans-serif;
+                        font-weight: bold;
+                        }
+                    }
+                }
+            }
+            .content {
+                width: 100vw;
+                overflow: scroll;
+
+                .title {
+                    margin-left: 20px;
+                }
+                table, th, td {
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                    padding: 0 8px;
+                    widthL 100%;
+                }
+                table {
+                    margin: 20px;
+                }
+                th, td {
+                    white-space: nowrap;
+                }
+                .table-controls {
+                    margin: 20px;
+                    display: flex;
+                    a {
+                        border: 1px solid gray;
+                        border-radius: 3px;
+                        padding: 5px;
+                        margin-right: 5px;
+                        text-decoration: none;
+                        color: gray;
+                    }
+                }
+                
+            }
+        """
--- seed/views/v3/public.py
+++ seed/views/v3/public.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse, HttpResponse
+from django.urls import reverse_lazy
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from urllib.parse import urlencode
 
 from seed.models import Organization
-from seed.utils.public import public_feed
+from seed.utils.public import public_feed, dict_to_table, page_navigation_link, PUBLIC_HTML_STYLE, PUBLIC_HTML_HEADER, PUBLIC_HTML_DISABLED
 
 
 class PublicOrganizationViewSet(viewsets.ViewSet):
@@ -48,101 +50,72 @@ class PublicOrganizationViewSet(viewsets.ViewSet):
 
     @action(detail=True, methods=["get"], url_path="feed.html")
     def feed_html(self, request, pk):
+        """
+        Returns all property and taxlot state data for a given organization as an HTML string. The results are ordered by "state.update".
+
+        Optional and configurable url query_params:
+        :query_param labels: comma separated list of case sensitive label names. Results will include inventory that has any of the listed labels. Default is all inventory
+        :query_param cycles: comma separated list of cycle ids. Results include inventory from the listed cycles. Default is all cycles
+        :query_param properties: boolean to return properties. Default is True
+        :query_param taxlots: boolan to return taxlots. Default is True
+        :query_param page: integer page number
+        :query_param per_page: integer results per page
+
+        Example requests:
+        {seed_url}/api/v3/public/organizations/feed.html?{query_param1}={value1}&{query_param2}={value2}
+        dev1.seed-platform.org/api/v3/public/organizations/1/feed.html
+        dev1.seed-platform.org/api/v3/public/organizations/1/feed.html?page=2&labels=Compliant&cycles=1,2,3&taxlots=False
+        """
 
         try:
             org = Organization.objects.get(pk=pk)
         except Organization.DoesNotExist:
             return JsonResponse({"erorr": "Organization does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
+        if not org.public_feed_enabled:
+            return HttpResponse(PUBLIC_HTML_DISABLED)
         
-        style= """
-            body {
-                font-family: 'PT Sans Narrow', 'Helvetica Neue', helvetica, arial, sans-serif;
-                font-weight: normal;
-            }
-            .logo_container {
-                display: flex;
-                height: 50px;
+        query_params = request.GET.copy()
+        base_url = f"/api/v3/public/organizations/{pk}/feed.html"
+        data = public_feed(org, request, html_view=True)
 
-                .logo {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    color: black;
+        table_properties = ""
+        if data['query_params'].get('properties'):
+            table_properties = dict_to_table(data['data'].get('properties', []), 'Properties')
 
-                    .logo_text {
-                        padding: 0 20px;
-                        font-size: 24px;
-                        font-family: 'PT Sans Narrow', 'Helvetica Neue', helvetica, arial, sans-serif;
-                        font-weight: normal;
-                        text-transform: uppercase;
+        table_taxlots = ""
+        if data['query_params'].get('taxlots'):
+            table_taxlots = dict_to_table(data['data'].get('taxlots', []), 'Tax Lots')
 
-                        .logo_text_seed {
-                        font-family: 'PT Sans', 'Helvetica Neue', helvetica, arial, sans-serif;
-                        font-weight: bold;
-                        }
-                    }
-                }
-            }
-            .content {
-                width: 100vw;
-                overflow: scroll;
+        params = [{**data.get('pagination'), **data.get('organization'), **data.get('query_params')}]
+        params_table = dict_to_table(params, "")
 
-                table, th, td {
-                    border: 1px solid black;
-                    border-collapse: collapse;
-                    padding: 0 8px;
-                    widthL 100%;
-                }
-                table {
-                    margin: 20px;
-                }
-                th, td {
-                    white-space: nowrap;
-                }
-                
-            }
-        """
-        header = """
-            <div class="logo_container">
-                <a class="logo" href="/">
-                <div class="logo_text">
-                    <span class="logo_text_seed">Seed</span>
-                    <span class="logo_text_platform">Platform™</span>
-                </div>
-                </a>
+        page_controls = f"""
+            <div class='table-controls'>
+                {page_navigation_link(base_url, data['pagination'], query_params, False)}
+                {page_navigation_link(base_url, data['pagination'], query_params, True)}
             </div>
         """
-        def dict_to_table(data):
-            html = "<table>\n"
-
-            headers = data[0].keys()
-            header_row = '<tr>' + ''.join(f'<th>{header}</th>' for header in headers) + '</tr>\n'
-            html += header_row
-
-            for datum in data:
-
-                row = '<tr>' + ''.join(f'<td>{datum[header]}</td>' for header in headers) + '<tr/>\n'
-                html += row
-
-            html += '</table>'
-
-            return html
-        
-        data = public_feed(org, request, flat_cycle=True)
-        table = dict_to_table(data['data']['properties'])
-
-        content = f"""<div class='content'>{table}</div>"""
+        content = f"""
+            <div class='content'>
+                {params_table}
+                {page_controls}
+                {table_properties}
+                {table_taxlots}
+            </div>
+        """
         html = f"""
             <html>
                 <head>
                 <style>
-                    {style}
+                    {PUBLIC_HTML_STYLE}
                 </style>
                 </head>
                 <body>
-                    {header}
+                    {PUBLIC_HTML_HEADER}
                     {content}
                 </body>
-            </html"""
+            </html
+        """
+
         return HttpResponse(html)
