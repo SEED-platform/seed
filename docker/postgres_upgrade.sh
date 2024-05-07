e9f1f78f6243003f06f87d4f9d46e2f3ebc7cb8d
Damon Haley <damon.haley@nrel.gov>
2024-05-07 12:49:26 -0600

    add verbose flag.

--- docker/postgres_upgrade.sh
+++ docker/postgres_upgrade.sh
@@ -3,6 +3,7 @@
 # Initialize variables with default values for optional parameters
 SOURCE_PG_CONV_VERSION='13'
 SOURCE_TS_CONV_VERSION='2.14.2'
+VERBOSE_MODE=false  # Default verbose mode is set to false
 
 # Initialize variables for required parameters without default values
 TARGET_PG_TEST_VERSION=""
@@ -27,9 +28,13 @@ while [[ $# -gt 0 ]]; do
             TARGET_TS_TEST_VERSION="$2"
             shift 2
             ;;
+        --verbose)
+            VERBOSE_MODE=true
+            shift
+            ;;
         *)
             echo "Unknown argument: $1"
-            echo "Usage: $0 [--source-pg-conv-version <version>] [--source-ts-conv-version <version>] --target-pg-test-version <version> --target-ts-test-version <version>"
+            echo "Usage: $0 [--source-pg-conv-version <version>] [--source-ts-conv-version <version>] --target-pg-test-version <version> --target-ts-test-version <version> [--verbose]"
             exit 1
             ;;
     esac
@@ -38,7 +43,7 @@ done
 # Check if any of the required variables are still empty
 if [ -z "$TARGET_PG_TEST_VERSION" ] || [ -z "$TARGET_TS_TEST_VERSION" ]; then
     echo "Error: Missing required arguments."
-    echo "Usage: $0 [--source-pg-conv-version <version>] [--source-ts-conv-version <version>] --target-pg-test-version <version> --target-ts-test-version <version>"
+    echo "Usage: $0 [--source-pg-conv-version <version>] [--source-ts-conv-version <version>] --target-pg-test-version <version> --target-ts-test-version <version> [--verbose]"
     exit 1
 fi
 
@@ -48,7 +53,6 @@ echo "Source Timescale Test Version: $SOURCE_TS_CONV_VERSION"
 echo "Target PostgreSQL Test Version: $TARGET_PG_TEST_VERSION"
 echo "Target Timescale Test Version: $TARGET_TS_TEST_VERSION"
 
-# Correctly define the variables using underscores instead of hyphens
 PG_CONV_STRING="PostgreSQL ${SOURCE_PG_CONV_VERSION}"
 PG_TEST_STRING="PostgreSQL ${TARGET_PG_TEST_VERSION}"
 
@@ -57,7 +61,7 @@ echo "PG_CONV_STRING set to: $PG_CONV_STRING"
 echo "PG_TEST_STRING set to: $PG_TEST_STRING"
 
 echo "Starting standard docker-compose dev environment..."
-docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+docker compose -f docker-compose.yml -f docker-compose.dev.yml up --remove-orphans -d
 
 # set intermediary source conversion postgres container from docker-compose.pgupgrade.yml
 
@@ -94,7 +98,13 @@ echo "Disconnecting all other users from the 'seed' database..."
 docker-compose exec -T db-postgres psql --user=seed -d postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'seed';"
 export PG12_DUMP="/share/seed-pg${POSTGRES_VERSION}-${TIMESCALE_VERSION}-$(date +%Y%m%d%H%M%S).dump"
 echo "Creating PostgresV: ${POSTGRES_VERSION} TimescaleV: ${TIMESCALE_VERSION} dump file of $PG12_DUMP..."
-docker-compose exec -T db-postgres pg_dump --username=seed --dbname=seed --verbose --no-owner --no-acl -Z7 -Fc -f "${PG12_DUMP}"
+# Execute pg_dump command with or without --verbose flag based on VERBOSE_MODE
+if $VERBOSE_MODE; then
+    docker-compose exec -T db-postgres pg_dump --username=seed --dbname=seed --verbose --no-owner --no-acl -Z7 -Fc -f "${PG12_DUMP}"
+else
+    docker-compose exec -T db-postgres pg_dump --username=seed --dbname=seed --no-owner --no-acl -Z7 -Fc -f "${PG12_DUMP}"
+fi
+
 
 
 echo "============================================"
@@ -175,7 +185,13 @@ POSTGRES_VERSION=$(docker-compose exec -T ${SOURCE_PG_TIMESCALE_CONV_SERVICE} ps
 TIMESCALE_VERSION=$(docker-compose exec -T ${SOURCE_PG_TIMESCALE_CONV_SERVICE} psql --user=seed -d postgres -t -A -c "SELECT extversion FROM pg_extension WHERE extname='timescaledb';")
 export CONV_DUMP="/share/seed-pg${POSTGRES_VERSION}-${TIMESCALE_VERSION}-$(date +%Y%m%d%H%M%S).dump"
 echo "Dumping upgraded Postgres Version: ${POSTGRES_VERSION}, TimescaleDB Version: ${TIMESCALE_VERSION} to file: ${CONV_DUMP}..."
-docker-compose exec -T ${SOURCE_PG_TIMESCALE_CONV_SERVICE} pg_dump --username=seed --dbname=seed --verbose --no-owner --no-acl -Z7 -Fc -f "${CONV_DUMP}"
+if $VERBOSE_MODE; then
+    docker-compose exec -T ${SOURCE_PG_TIMESCALE_CONV_SERVICE} pg_dump --username=seed --dbname=seed --verbose --no-owner --no-acl -Z7 -Fc -f "${CONV_DUMP}"
+else
+    docker-compose exec -T ${SOURCE_PG_TIMESCALE_CONV_SERVICE} pg_dump --username=seed --dbname=seed --no-owner --no-acl -Z7 -Fc -f "${CONV_DUMP}"
+fi
+
+
 
 
 echo "============================================"
@@ -232,7 +248,11 @@ POSTGRES_VERSION=$(docker-compose exec -T ${TARGET_PG_TIMESCALE_TEST_SERVICE} ps
 TIMESCALE_VERSION=$(docker-compose exec -T ${TARGET_PG_TIMESCALE_TEST_SERVICE} psql --user=seed -d postgres -t -A -c "SELECT extversion FROM pg_extension WHERE extname='timescaledb';")
 export TEST_DUMP="/share/seed-pg${POSTGRES_VERSION}-${TIMESCALE_VERSION}-$(date +%Y%m%d%H%M%S).dump"
 echo "Dumping upgraded PostgresV: ${POSTGRES_VERSION} TimescaleV: ${TIMESCALE_VERSION} file of $TEST_DUMP..."
-docker-compose exec -T ${TARGET_PG_TIMESCALE_TEST_SERVICE} pg_dump --username=seed --dbname=seed --verbose --no-owner --no-acl -Z7 -Fc -f "${TEST_DUMP}"
+if $VERBOSE_MODE; then
+    docker-compose exec -T ${TARGET_PG_TIMESCALE_TEST_SERVICE} pg_dump --username=seed --dbname=seed --verbose --no-owner --no-acl -Z7 -Fc -f "${TEST_DUMP}"
+else
+    docker-compose exec -T ${TARGET_PG_TIMESCALE_TEST_SERVICE} pg_dump --username=seed --dbname=seed --no-owner --no-acl -Z7 -Fc -f "${TEST_DUMP}"
+fi
 
 export COMPOSE_FILE=docker-compose.pgupgrade.yml
 docker-compose stop ${SOURCE_PG_TIMESCALE_CONV_SERVICE}
