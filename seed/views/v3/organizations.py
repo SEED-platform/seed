696494d3f1e33a0400beb9d73914f2800852f0ee
Ross Perry <perryr16@gmail.com>
2024-05-14 13:18:33 -0600

    bug fix

--- seed/views/v3/organizations.py
+++ seed/views/v3/organizations.py
@@ -149,7 +149,7 @@ def _dict_org(request, organizations):
             "access_level_names": o.access_level_names,
             "public_feed_enabled": o.public_feed_enabled,
             "public_feed_labels": o.public_feed_labels,
-            "public_geojson_enabled": o.public_feed_labels,
+            "public_geojson_enabled": o.public_geojson_enabled,
         }
         orgs.append(org)
 
@@ -508,34 +508,23 @@ class OrganizationViewSet(viewsets.ViewSet):
             org.geocoding_enabled = geocoding_enabled
 
         # Update public_feed_enabled option
-        public_feed_enabled = posted_org.get("public_feed_enabled", False)
+        public_feed_enabled = posted_org.get("public_feed_enabled")
         if public_feed_enabled:
-            logging.error('pub feed enabled')
             org.public_feed_enabled = True
         elif org.public_feed_enabled:
-            logging.error('>>> org.public_feed_enabled')
             org.public_feed_enabled = False
             org.public_feed_labels = False
             org.public_geojson_enabled = False
 
         # Update public_feed_labels option
-        public_feed_labels = posted_org.get("public_feed_labels")
-        if public_feed_labels is None:
-            logging.error('labels none')
-            pass
-        elif public_feed_enabled and public_feed_labels != org.public_feed_labels:
-            logging.error('labels diff')
+        public_feed_labels = posted_org.get("public_feed_labels", False)
+        if public_feed_enabled and public_feed_labels != org.public_feed_labels:
             org.public_feed_labels = public_feed_labels
 
         # Update public_geojson_enabled option
-        public_geojson_enabled = posted_org.get("public_geojson_enabled")
-        if public_geojson_enabled is None:
-            logging.error('geo none')
-            pass
-        elif public_feed_enabled and public_geojson_enabled != org.public_geojson_enabled:
+        public_geojson_enabled = posted_org.get("public_geojson_enabled", False)
+        if public_feed_enabled and public_geojson_enabled != org.public_geojson_enabled:
             org.public_geojson_enabled = public_geojson_enabled
-            logging.error('>>> org pub geojson %s', org.public_geojson_enabled)
-            logging.error('>>> pub geojson %s', public_geojson_enabled)
 
         # Update BETTER Analysis API Key if it's been changed
         better_analysis_api_key = posted_org.get("better_analysis_api_key", "").strip()
