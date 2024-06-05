259cff32e531188c791f05d2d04a577f4a471c90
Ross Perry <ross.perry@Rosss-MBP-2.home>
2024-06-05 09:37:40 -0600

    update test

--- seed/tests/test_property_view_labels.py
+++ seed/tests/test_property_view_labels.py
@@ -3,24 +3,15 @@ SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and othe
 See also https://github.com/SEED-platform/seed/blob/main/LICENSE.md
 """
 
-import json
 from datetime import datetime
-from django.forms.models import model_to_dict
-from django.test import TestCase
-from quantityfield.units import ureg
 from django.urls import reverse_lazy
 
-
-from seed.models import Column, DerivedColumnParameter, Goal, PropertyView, PropertyViewLabel
-from seed.models.data_quality import DataQualityCheck, DataQualityTypeCastError, Rule, StatusLabel, UnitMismatchError
-from seed.models.derived_columns import DerivedColumn
-from seed.models.models import ASSESSED_RAW
+from seed.models import Column, PropertyViewLabel
+from seed.models.data_quality import StatusLabel
 from seed.test_helpers.fake import (
-    FakeDerivedColumnFactory,
     FakeGoalFactory,
     FakePropertyFactory,
     FakePropertyStateFactory,
-    FakeTaxLotStateFactory,
     FakeColumnFactory,
     FakeCycleFactory,
     FakePropertyViewFactory,
@@ -80,22 +71,27 @@ class PropertyLabelViewTests(AccessLevelBaseTestCase):
 
         labels = StatusLabel.objects.all()
         self.pvl1 = PropertyViewLabel.objects.create(propertyview=self.view1, statuslabel=labels[0])
-        self.pvl1 = PropertyViewLabel.objects.create(propertyview=self.view2, statuslabel=labels[1])
-        self.pvl1 = PropertyViewLabel.objects.create(propertyview=self.view3, statuslabel=labels[2], goal=self.goal1)
-        self.pvl1 = PropertyViewLabel.objects.create(propertyview=self.view4, statuslabel=labels[3], goal=self.goal1)
-        self.pvl1 = PropertyViewLabel.objects.create(propertyview=self.view5, statuslabel=labels[2], goal=self.goal2)
-        self.pvl1 = PropertyViewLabel.objects.create(propertyview=self.view6, statuslabel=labels[4], goal=self.goal2)
+        self.pvl2 = PropertyViewLabel.objects.create(propertyview=self.view2, statuslabel=labels[1])
+        self.pvl3 = PropertyViewLabel.objects.create(propertyview=self.view3, statuslabel=labels[2], goal=self.goal1)
+        self.pvl4 = PropertyViewLabel.objects.create(propertyview=self.view4, statuslabel=labels[3], goal=self.goal1)
+        self.pvl5 = PropertyViewLabel.objects.create(propertyview=self.view5, statuslabel=labels[2], goal=self.goal2)
+        self.pvl6 = PropertyViewLabel.objects.create(propertyview=self.view6, statuslabel=labels[4], goal=self.goal2)
 
     def test_property_view_label_viewset(self):
         url = reverse_lazy("api:v3:property_view_labels-list-by-goal")
-        response = self.client.get(url, {"organization_id": self.org.id, "goal_id": self.goal1.id}, content_type="application/json")
-        response = json.loads(response.content)
-        labels = response["message"]
+        params = {"organization_id": self.org.id, "goal_id": self.goal1.id, "cycle": "current"}
+        response = self.client.get(url, params, content_type="application/json")
+        labels = response.json()
+        assert len(labels) == 0
 
+        params["cycle"] = "baseline"
+        response = self.client.get(url, params, content_type="application/json")
+        labels = response.json()
         assert len(labels) == 4
         assert labels[0]["goal"] == None
         assert labels[1]["goal"] == None
         assert labels[2]["goal"] == self.goal1.id
         assert labels[3]["goal"] == self.goal1.id
+
 
         
