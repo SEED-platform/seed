6b8cca5a65daf70bcb8ccd0fac67093986d1ff6c
Ross Perry <ross.perry@Rosss-MBP-2.home>
2024-06-12 13:43:15 -0600

    precommit

--- seed/utils/celery.py
+++ seed/utils/celery.py
@@ -2,7 +2,9 @@
 SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
 See also https://github.com/SEED-platform/seed/blob/main/LICENSE.md
 """
+
 import logging
+
 import celery
 
 
@@ -11,7 +13,7 @@ def get_celery_worker_count():
         app = celery.Celery("seed")
         app.config_from_object("django.conf:settings", namespace="CELERY")
         inspector = app.control.inspect()
-        stats = inspector.stats()        
+        stats = inspector.stats()
         if not stats:
             return 1
         total_workers = sum(info["pool"]["max-concurrency"] for worker, info in stats.items())
