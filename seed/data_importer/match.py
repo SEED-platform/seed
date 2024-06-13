68a58afccdf7fe51e877f5329e97bb619748ae4f
Ross Perry <ross.perry@Rosss-MBP-2.home>
2024-06-13 13:13:24 -0600

    precommit

--- seed/data_importer/match.py
+++ seed/data_importer/match.py
@@ -3,7 +3,7 @@
 SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
 See also https://github.com/SEED-platform/seed/blob/main/LICENSE.md
 """
-import logging
+
 import datetime as dt
 import math
 
