7d0af945c2e0bfdc721197987ca2fb06235584a4
Ross Perry <ross.perry@Rosss-MBP-2.home>
2024-06-21 09:55:04 -0600

    lint

--- seed/static/seed/js/services/two_factor_service.js
+++ seed/static/seed/js/services/two_factor_service.js
@@ -3,78 +3,69 @@
  * See also https://github.com/SEED-platform/seed/blob/main/LICENSE.md
  */
 angular.module('BE.seed.service.two_factor', []).factory('two_factor_service', [
-    '$http',
-    'user_service',
-    ($http, user_service) => {
-        const two_factor_factory = {};
+  '$http',
+  'user_service',
+  ($http, user_service) => {
+    const two_factor_factory = {};
 
-        two_factor_factory.set_method = (user_email, methods) => {
-            return $http.post(
-                '/api/v3/two_factor/set_method/',
-                {
-                    user_email: user_email,
-                    methods: methods,
-                },
-                {
-                    params: {
-                        organization_id: user_service.get_organization().id
-                    }
-                }
-            )
-            .then((response) => response.data)
-            .catch((response) => response)
+    two_factor_factory.set_method = (user_email, methods) => $http.post(
+      '/api/v3/two_factor/set_method/',
+      {
+        user_email,
+        methods
+      },
+      {
+        params: {
+          organization_id: user_service.get_organization().id
         }
+      }
+    )
+      .then((response) => response.data)
+      .catch((response) => response);
 
-        two_factor_factory.resend_token_email = (user_email) => {
-            return $http.post(
-                '/api/v3/two_factor/resend_token_email/',
-                {
-                    user_email: user_email,
-                },
-                {
-                    params: {
-                        organization_id: user_service.get_organization().id
-                    }
-                }
-            )
-            .then((response) => response)
-            .catch((response) => response)
+    two_factor_factory.resend_token_email = (user_email) => $http.post(
+      '/api/v3/two_factor/resend_token_email/',
+      {
+        user_email
+      },
+      {
+        params: {
+          organization_id: user_service.get_organization().id
         }
+      }
+    )
+      .then((response) => response)
+      .catch((response) => response);
 
-        two_factor_factory.generate_qr_code = (user_email) => {
-            return $http.post(
-                '/api/v3/two_factor/generate_qr_code/',
-                {
-                    user_email: user_email,
-                },
-                {
-                    params: {
-                        organization_id: user_service.get_organization().id
-                    }
-                }
-            )
-            .then((response) => response)
-            .catch((response) => response)
+    two_factor_factory.generate_qr_code = (user_email) => $http.post(
+      '/api/v3/two_factor/generate_qr_code/',
+      {
+        user_email
+      },
+      {
+        params: {
+          organization_id: user_service.get_organization().id
         }
+      }
+    )
+      .then((response) => response)
+      .catch((response) => response);
 
-        two_factor_factory.verify_code = (code, user_email) => {
-            return $http.post(
-                '/api/v3/two_factor/verify_code/',
-                {
-                    code: code,
-                    user_email: user_email,
-                },
-                {
-                    params: {
-                        organization_id: user_service.get_organization().id
-                    }
-                }
-            )
-            .then((response) => response)
-            .catch((response) => response)
+    two_factor_factory.verify_code = (code, user_email) => $http.post(
+      '/api/v3/two_factor/verify_code/',
+      {
+        code,
+        user_email
+      },
+      {
+        params: {
+          organization_id: user_service.get_organization().id
         }
+      }
+    )
+      .then((response) => response)
+      .catch((response) => response);
 
-        return two_factor_factory
-
-    }
-])
+    return two_factor_factory;
+  }
+]);
