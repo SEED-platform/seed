4a4141df9a6155322a520b4bea55bb6fc5094cf1
Ross Perry <ross.perry@Rosss-MBP-2.home>
2024-07-01 14:16:27 -0600

    clarifying text

--- seed/static/seed/js/controllers/data_quality_admin_controller.js
+++ seed/static/seed/js/controllers/data_quality_admin_controller.js
@@ -129,8 +129,8 @@ angular.module('BE.seed.controller.data_quality_admin', []).controller('data_qua
     ];
 
     $scope.cross_cycle_options = [
-      { bool: true, label: 'Cross-Cycle' },
-      { bool: false, label: 'Within Cycle' }
+      { bool: true, label: '% Change aross cycle' },
+      { bool: false, label: 'Within cycle' }
     ];
 
     $scope.columns = _.map(angular.copy(columns.filter((col) => !col.derived_column)), (col) => {
