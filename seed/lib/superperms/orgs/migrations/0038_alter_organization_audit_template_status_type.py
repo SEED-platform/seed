d52e66c7ac80e1997884cde9c13050cd6fec2bdc
Ross Perry <ross.perry@rosss-mbp-2.lan>
2024-08-19 13:21:33 -0400

    precommit

--- seed/lib/superperms/orgs/migrations/0038_alter_organization_audit_template_status_type.py
+++ seed/lib/superperms/orgs/migrations/0038_alter_organization_audit_template_status_type.py
@@ -4,20 +4,19 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('orgs', '0037_organization_display_water_units'),
+        ("orgs", "0037_organization_display_water_units"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='organization',
-            name='audit_template_status_type',
-            field=models.CharField(blank=True, default='Complies', max_length=34),
+            model_name="organization",
+            name="audit_template_status_type",
+            field=models.CharField(blank=True, default="Complies", max_length=34),
         ),
         migrations.RenameField(
-            model_name='organization',
-            old_name='audit_template_status_type',
-            new_name='audit_template_status_types',
+            model_name="organization",
+            old_name="audit_template_status_type",
+            new_name="audit_template_status_types",
         ),
     ]
