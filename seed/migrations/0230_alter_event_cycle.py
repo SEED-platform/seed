c6debfaee42842ab835bad8da67a168dd59c6f80
kflemin <2205659+kflemin@users.noreply.github.com>
2024-09-20 15:03:03 -0600

    update migration order

--- seed/migrations/0230_alter_event_cycle.py
+++ seed/migrations/0230_alter_event_cycle.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("seed", "0228_alter_analysis_service"),
+        ("seed", "0229_alter_analysis_service"),
     ]
 
     operations = [
