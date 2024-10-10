b52973f906b5a74e218ce387874080f8ef682670
Caleb Rutan <caleb.rutan@deptagency.com>
2024-10-10 13:51:08 -0400

    ensure only numeric axis return data - remove lines 1270 and 1271 if the all zeros returns should not be visible

--- seed/views/v3/organizations.py
+++ seed/views/v3/organizations.py
@@ -1207,9 +1207,10 @@ class OrganizationViewSet(viewsets.ViewSet):
         data = [
             d[axis]
             for d in [apply_display_unit_preferences(organization, d) for d in filtered_properties.values(axis)]
-            if axis in d and d[axis] is not None
+            if axis in d and d[axis] is not None and isinstance(d[axis], (int, float))
         ]
 
+        logging.error(f"Data: {data}")
         if len(data) > 0:
             percentiles = np.percentile(data, [5, 25, 50, 75, 95])
             # order the cols: sum, min, 5%, 25%, mean, median (50%), 75, 95, max
