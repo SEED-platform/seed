0940e9b9b90de498aeb8017c5049fa77982c2a6c
Ross Perry <ross.perry@Rosss-MacBook-Pro-2.local>
2024-11-05 11:50:02 -0700

    dev - meeds pagination

--- seed/views/v3/goals.py
+++ seed/views/v3/goals.py
@@ -278,6 +278,7 @@ class GoalViewSet(ModelViewSetWithoutPatch, OrgMixin):
         }
 
         # PAGINATION
+        # FILTERS
 
         return JsonResponse({"status": "success", "data": results})
     
