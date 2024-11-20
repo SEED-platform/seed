90c6a7831900e3cc68f918f7b2f4b094ff9f3ed2
kflemin <2205659+kflemin@users.noreply.github.com>
2024-11-19 22:38:35 -0700

    include derived columns in y axis

--- seed/static/seed/js/controllers/organization_settings_controller.js
+++ seed/static/seed/js/controllers/organization_settings_controller.js
@@ -312,11 +312,11 @@ angular.module('SEED.controller.organization_settings', []).controller('organiza
     };
 
     const acceptable_column_types = ['area', 'eui', 'float', 'integer', 'number'];
-    const filtered_columns = _.filter($scope.columns, (column) => _.includes(acceptable_column_types, column.data_type));
-
+    // filtered columns should include derived columns
+    const filtered_columns = _.filter($scope.columns, (column) => column.derived_column || _.includes(acceptable_column_types, column.data_type));
+    
     $scope.selected_x_columns = $scope.org.default_reports_x_axis_options.map((c) => c.id);
     $scope.available_x_columns = () => $scope.columns.filter(({ id }) => !$scope.selected_x_columns.includes(id));
-
     $scope.add_x_column = (x_column_id) => {
       $scope.selected_x_columns.push(x_column_id);
     };
