272905283d309a762cef06759452eded5f6f8f90
Caleb Rutan <crutan@users.noreply.github.com>
2025-02-03 10:28:14 -0500

    Add user serializer (#4940)
    * add user serializer, enable it for v3/users/current, v3/users/retrieve and v3/users/update
    
    * update tests to account for new user serializer
    
    * update tests to account for new user serializer

---
+++ seed/serializers/users.py
@@ -0,0 +1,42 @@
+"""
+SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
+See also https://github.com/SEED-platform/seed/blob/main/LICENSE.md
+"""
+
+from django_otp import devices_for_user
+from django_otp.plugins.otp_email.models import EmailDevice
+from django_otp.plugins.otp_totp.models import TOTPDevice
+from rest_framework import serializers
+
+from seed.landing.models import SEEDUser as User
+from seed.views.main import _get_default_org as get_default_org_for_user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email", "username", "api_key", "is_superuser", "id", "pk")
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+
+        two_factor_devices = list(devices_for_user(instance))
+        if two_factor_devices and isinstance(two_factor_devices[0], EmailDevice):
+            ret["two_factor_method"] = "email"
+        elif two_factor_devices and isinstance(two_factor_devices[0], TOTPDevice):
+            ret["two_factor_method"] = "token"
+        else:
+            ret["two_factor_method"] = "disabled"
+
+        additional_fields = dict(
+            list(
+                zip(
+                    ("org_id", "org_name", "org_role", "ali_name", "ali_id", "is_ali_root", "is_ali_leaf"),
+                    get_default_org_for_user(instance),
+                )
+            )
+        )
+        for k, v in additional_fields.items():
+            ret[k] = v
+
+        return ret
--- seed/views/v3/users.py
+++ seed/views/v3/users.py
@@ -10,9 +10,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db import IntegrityError
 from django.http import JsonResponse
-from django_otp import devices_for_user
 from django_otp.plugins.otp_email.models import EmailDevice
-from django_otp.plugins.otp_totp.models import TOTPDevice
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import serializers, status, viewsets
 from rest_framework.decorators import action
@@ -28,7 +26,6 @@ from seed.utils.api import OrgMixin, api_endpoint_class
 from seed.utils.api_schema import AutoSchemaHelper, swagger_auto_schema_org_query_param
 from seed.utils.organizations import create_organization
 from seed.utils.users import get_role_from_js
-from seed.views.main import _get_default_org as get_default_org_for_user
 
 _log = logging.getLogger(__name__)
 
@@ -259,23 +256,8 @@ class UserViewSet(viewsets.ViewSet, OrgMixin):
                 required: true
                 type: string
         """
-        response = dict(
-            list(
-                zip(
-                    ("org_id", "org_name", "org_role", "ali_name", "ali_id", "is_ali_root", "is_ali_leaf"),
-                    get_default_org_for_user(request.user),
-                )
-            )
-        )
-        response["pk"] = request.user.id
-        response["id"] = request.user.id
-        response["first_name"] = request.user.first_name
-        response["last_name"] = request.user.last_name
-        response["email"] = request.user.email
-        response["username"] = request.user.username
-        response["is_superuser"] = request.user.is_superuser
-        response["api_key"] = request.user.api_key
-        return JsonResponse(response)
+
+        return JsonResponse(request.user.serialize())
 
     @swagger_auto_schema(
         manual_parameters=[AutoSchemaHelper.query_org_id_field()],
@@ -383,24 +365,7 @@ class UserViewSet(viewsets.ViewSet, OrgMixin):
         else:
             return content
 
-        two_factor_devices = list(devices_for_user(user))
-        if two_factor_devices and isinstance(two_factor_devices[0], EmailDevice):
-            two_factor_method = "email"
-        elif two_factor_devices and isinstance(two_factor_devices[0], TOTPDevice):
-            two_factor_method = "token"
-        else:
-            two_factor_method = "disabled"
-
-        return JsonResponse(
-            {
-                "status": "success",
-                "first_name": user.first_name,
-                "last_name": user.last_name,
-                "email": user.email,
-                "api_key": user.api_key,
-                "two_factor_method": two_factor_method,
-            }
-        )
+        return JsonResponse(user.serialize())
 
     @ajax_request_class
     @action(detail=True, methods=["POST"])
@@ -458,15 +423,7 @@ class UserViewSet(viewsets.ViewSet, OrgMixin):
         user.email = json_user.get("email")
         user.username = json_user.get("email", "").lower()
         user.save()
-        return JsonResponse(
-            {
-                "status": "success",
-                "first_name": user.first_name,
-                "last_name": user.last_name,
-                "email": user.email,
-                "api_key": user.api_key,
-            }
-        )
+        return JsonResponse(user.serialize())
 
     @swagger_auto_schema(
         request_body=AutoSchemaHelper.schema_factory(
