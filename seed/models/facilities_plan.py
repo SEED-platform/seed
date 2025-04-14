0c20738966b1b83433bda8dbd8f5c9b57c26056b
Hannah Eslinger <heslinge@nrel.gov>
2025-04-14 12:31:42 -0600

    create FacilitiesPlanRun

--- seed/models/facilities_plan.py
+++ seed/models/facilities_plan.py
@@ -7,9 +7,11 @@ import logging
 
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models import BooleanField, Case, F, FloatField, Sum, Value, When
+from django.db.models.functions import Cast
 
-from seed.lib.superperms.orgs.models import Organization
-from seed.models import Column
+from seed.lib.superperms.orgs.models import AccessLevelInstance, Organization
+from seed.models import Column, Cycle, PropertyView
 
 logger = logging.getLogger(__name__)
 
@@ -44,3 +46,102 @@ class FacilitiesPlan(models.Model):
         constraints = [
             models.UniqueConstraint(fields=["organization", "name"], name="unique_name_for_plan"),
         ]
+
+    def calculate_properties_percentage_of_total_energy_usage(self, ali: AccessLevelInstance, cycle: Cycle):
+        # check that we have all the required columns
+        required_columns = [
+            "electric_energy_usage_column",
+            "gas_energy_usage_column",
+            "steam_energy_usage_column",
+            "include_in_total_denominator_column",
+        ]
+        missing_columns = [c for c in required_columns if getattr(self, c) is None]
+        if missing_columns:
+            raise ValueError(f"`calculate_properties_selected_by_plan` requires the following null columns: {missing_columns}")
+
+        # get relevant properties
+        properties = PropertyView.objects.filter(property__access_level_instance=ali, cycle=cycle)
+
+        # calculate properties total energy usage
+        properties = properties.annotate(
+            total_energy_usage=_get_column_or_zero(self.electric_energy_usage_column)
+            + _get_column_or_zero(self.gas_energy_usage_column)
+            + _get_column_or_zero(self.steam_energy_usage_column)
+        )
+
+        # calculate properties percentage of total energy usage
+        properties_included_in_denominator = properties.filter(**{_get_column_model_field(self.include_in_total_denominator_column): True})
+        denominator = properties_included_in_denominator.aggregate(Sum("total_energy_usage"))["total_energy_usage__sum"]
+        properties = properties.annotate(
+            percentage_of_total_energy_usage=Cast(F("total_energy_usage"), FloatField()) / denominator,
+        )
+
+        # calculate required_in_plan (We're weeding out the nones, which mess up ordering later)
+        properties = properties.annotate(
+            required_in_plan=Case(
+                When(**{_get_column_model_field(self.require_in_plan_column): True}, then=Value(True)),
+                default=Value(False),
+                output_field=BooleanField(),
+            )
+        )
+
+        return properties
+
+    def get_selected_properties(self, ali: AccessLevelInstance, cycle: Cycle):
+        run = FacilitiesPlanRun.objects.create(facilities_plan=self, cycle=cycle, ali=ali)
+        all_properties = self.calculate_properties_percentage_of_total_energy_usage(ali, cycle).order_by(
+            "-required_in_plan", "-percentage_of_total_energy_usage"
+        )
+        energy_running_sum_percentage = 0
+
+        for rank, p in enumerate(all_properties):
+            energy_running_sum_percentage += p.percentage_of_total_energy_usage
+
+            FacilitiesPlanRunProperty.objects.create(
+                run=run,
+                view=p,
+                total_energy_usage=p.total_energy_usage,
+                percentage_of_total_energy_usage=p.percentage_of_total_energy_usage,
+                rank=rank,
+                running_percentage=energy_running_sum_percentage,
+                running_square_footage=0,
+            )
+
+        return run
+
+
+def _get_column_or_zero(column):
+    c = _get_column_model_field(column)
+
+    return Case(
+        # regex to see if field is present and numeric
+        When(**{f"{c}__regex": r"^\d+(\.\d+)?$"}, then=Cast(F(c), FloatField())),
+        default=Value(0.0),
+        output_field=FloatField(),
+    )
+
+
+def _get_column_model_field(column):
+    if column.is_extra_data:
+        return "state__extra_data__" + column.column_name
+    elif column.derived_column:
+        return "state__derived_data__" + column.column_name
+    else:
+        return "state__" + column.column_name
+
+
+class FacilitiesPlanRun(models.Model):
+    facilities_plan = models.ForeignKey(FacilitiesPlan, on_delete=models.CASCADE, related_name="runs")
+    cycle = models.ForeignKey(Cycle, on_delete=models.SET_NULL, null=True)
+    ali = models.ForeignKey(AccessLevelInstance, on_delete=models.SET_NULL, null=True)
+
+
+class FacilitiesPlanRunProperty(models.Model):
+    run = models.ForeignKey(FacilitiesPlanRun, on_delete=models.SET_NULL, null=True, related_name="property_rankings")
+    view = models.ForeignKey(PropertyView, on_delete=models.SET_NULL, null=True)
+
+    total_energy_usage = models.FloatField()
+    percentage_of_total_energy_usage = models.FloatField()
+    rank = models.IntegerField()
+    running_percentage = models.FloatField()
+    running_square_footage = models.FloatField()
--- seed/tests/test_facilities_plan.py
+++ seed/tests/test_facilities_plan.py
@@ -9,7 +9,11 @@ from django.test import TestCase
 from django.urls import reverse
 
 from seed.landing.models import SEEDUser as User
-from seed.models import FacilitiesPlan
+from seed.lib.superperms.orgs.models import AccessLevelInstance
+from seed.models import Column, Cycle, FacilitiesPlan, PropertyView
+from seed.test_helpers.fake import (
+    FakePropertyViewFactory,
+)
 from seed.utils.organizations import create_organization
 
 
@@ -23,7 +27,30 @@ class FacilitiesPlanTests(TestCase):
         self.org, _, _ = create_organization(self.user)
         self.client.login(**user_details)
 
-        self.facilities_plan = FacilitiesPlan.objects.create(organization=self.org, name="test", energy_running_sum_percentage=0.85)
+        self.cycle = Cycle.objects.first()
+        self.ali = AccessLevelInstance.objects.first()
+
+        col_args = {"table_name": "PropertyState", "organization": self.org, "is_extra_data": True}
+        self.electric_energy_usage_column = Column.objects.create(
+            column_name="electric_energy_usage_column", data_type="number", **col_args
+        )
+        self.gas_energy_usage_column = Column.objects.create(column_name="gas_energy_usage_column", data_type="number", **col_args)
+        self.steam_energy_usage_column = Column.objects.create(column_name="steam_energy_usage_column", data_type="number", **col_args)
+        self.include_in_total_denominator_column = Column.objects.create(column_name="include_in_total_denominator_column", **col_args)
+        self.require_in_plan_column = Column.objects.create(column_name="require_in_plan_column", **col_args)
+        self.facilities_plan = FacilitiesPlan.objects.create(
+            organization=self.org,
+            name="test",
+            electric_energy_usage_column=self.electric_energy_usage_column,
+            gas_energy_usage_column=self.gas_energy_usage_column,
+            steam_energy_usage_column=self.steam_energy_usage_column,
+            include_in_total_denominator_column=self.include_in_total_denominator_column,
+            require_in_plan_column=self.require_in_plan_column,
+            energy_running_sum_percentage=0.60,
+        )
+        self.property_view_factory = FakePropertyViewFactory(organization=self.org, user=self.user)
+
+        self.maxDiff = None
 
     def test_list(self):
         response = self.client.get(
@@ -39,7 +66,22 @@ class FacilitiesPlanTests(TestCase):
                         "id": self.facilities_plan.id,
                         "organization": self.org.id,
                         "name": "test",
-                        "energy_running_sum_percentage": 0.85,
+                        "energy_running_sum_percentage": 0.6,
+                        "compliance_cycle_year_column": None,
+                        "include_in_total_denominator_column": self.include_in_total_denominator_column.id,
+                        "exclude_from_plan_column": None,
+                        "require_in_plan_column": self.require_in_plan_column.id,
+                        "gross_floor_area_column": None,
+                        "building_category_column": None,
+                        "electric_eui_column": None,
+                        "gas_eui_column": None,
+                        "steam_eui_column": None,
+                        "total_eui_column": None,
+                        "electric_energy_usage_column": self.electric_energy_usage_column.id,
+                        "gas_energy_usage_column": self.gas_energy_usage_column.id,
+                        "steam_energy_usage_column": self.steam_energy_usage_column.id,
+                        "electric_data_source_column": None,
+                        "gas_data_source_column": None,
                     }
                 ],
             },
@@ -51,7 +93,6 @@ class FacilitiesPlanTests(TestCase):
             content_type="application/json",
         )
 
-        print(response.json())
         self.assertDictEqual(
             response.json(),
             {
@@ -60,7 +101,216 @@ class FacilitiesPlanTests(TestCase):
                     "id": self.facilities_plan.id,
                     "organization": self.org.id,
                     "name": "test",
-                    "energy_running_sum_percentage": 0.85,
+                    "energy_running_sum_percentage": 0.6,
+                    "compliance_cycle_year_column": None,
+                    "include_in_total_denominator_column": self.include_in_total_denominator_column.id,
+                    "exclude_from_plan_column": None,
+                    "require_in_plan_column": self.require_in_plan_column.id,
+                    "gross_floor_area_column": None,
+                    "building_category_column": None,
+                    "electric_eui_column": None,
+                    "gas_eui_column": None,
+                    "steam_eui_column": None,
+                    "total_eui_column": None,
+                    "electric_energy_usage_column": self.electric_energy_usage_column.id,
+                    "gas_energy_usage_column": self.gas_energy_usage_column.id,
+                    "steam_energy_usage_column": self.steam_energy_usage_column.id,
+                    "electric_data_source_column": None,
+                    "gas_data_source_column": None,
                 },
             },
+        )
+
+    def test_get_selected_properties(self):
+        # Setup
+        PropertyView.objects.all().delete()
+        for e in [10, 20, 30, 40]:
+            self.property_view_factory.get_property_view(
+                cycle=self.cycle,
+                extra_data={
+                    "electric_energy_usage_column": e,
+                    "gas_energy_usage_column": 0,
+                    "steam_energy_usage_column": 0,
+                    "include_in_total_denominator_column": True,
+                },
+            )
+
+        # Action
+        results = self.facilities_plan.get_selected_properties(self.ali, self.cycle)
+
+        # Assert
+        self.assertListEqual(
+            [
+                {
+                    "total_energy_usage": round(r.total_energy_usage, 2),
+                    "percentage_of_total_energy_usage": round(r.percentage_of_total_energy_usage, 2),
+                    "rank": round(r.rank, 2),
+                    "running_percentage": round(r.running_percentage, 2),
+                    "running_square_footage": round(r.running_square_footage, 2),
+                }
+                for r in results.property_rankings.all()
+            ],
+            [
+                {
+                    "total_energy_usage": 40.0,
+                    "percentage_of_total_energy_usage": 0.4,
+                    "rank": 0,
+                    "running_percentage": 0.4,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 30.0,
+                    "percentage_of_total_energy_usage": 0.3,
+                    "rank": 1,
+                    "running_percentage": 0.7,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 20.0,
+                    "percentage_of_total_energy_usage": 0.2,
+                    "rank": 2,
+                    "running_percentage": 0.9,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 10.0,
+                    "percentage_of_total_energy_usage": 0.1,
+                    "rank": 3,
+                    "running_percentage": 1.0,
+                    "running_square_footage": 0.0,
+                },
+            ],
+        )
+
+    def test_get_selected_properties_missing_columns(self):
+        # Setup
+        PropertyView.objects.all().delete()
+        for e in [10, 20, 30, 40]:
+            self.property_view_factory.get_property_view(
+                cycle=self.cycle,
+                extra_data={
+                    "electric_energy_usage_column": e,
+                    "gas_energy_usage_column": None,
+                    # "steam_energy_usage_column": 0,  # oh no! i'm missing
+                    "include_in_total_denominator_column": True,
+                },
+            )
+
+        # Action
+        results = self.facilities_plan.get_selected_properties(self.ali, self.cycle)
+
+        # Assert
+        self.assertListEqual(
+            [
+                {
+                    "total_energy_usage": round(r.total_energy_usage, 2),
+                    "percentage_of_total_energy_usage": round(r.percentage_of_total_energy_usage, 2),
+                    "rank": round(r.rank, 2),
+                    "running_percentage": round(r.running_percentage, 2),
+                    "running_square_footage": round(r.running_square_footage, 2),
+                }
+                for r in results.property_rankings.all()
+            ],
+            [
+                {
+                    "total_energy_usage": 40.0,
+                    "percentage_of_total_energy_usage": 0.4,
+                    "rank": 0,
+                    "running_percentage": 0.4,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 30.0,
+                    "percentage_of_total_energy_usage": 0.3,
+                    "rank": 1,
+                    "running_percentage": 0.7,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 20.0,
+                    "percentage_of_total_energy_usage": 0.2,
+                    "rank": 2,
+                    "running_percentage": 0.9,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 10.0,
+                    "percentage_of_total_energy_usage": 0.1,
+                    "rank": 3,
+                    "running_percentage": 1.0,
+                    "running_square_footage": 0.0,
+                },
+            ],
+        )
+
+    def test_get_selected_properties_require_in_plan(self):
+        # Setup
+        PropertyView.objects.all().delete()
+        properties = []
+        for e in [10, 20, 30, 40]:
+            properties.append(
+                self.property_view_factory.get_property_view(
+                    cycle=self.cycle,
+                    extra_data={
+                        "electric_energy_usage_column": e,
+                        "gas_energy_usage_column": 0,
+                        "steam_energy_usage_column": 0,
+                        "include_in_total_denominator_column": True,
+                    },
+                )
+            )
+
+        # the first two properties must be included in the plan
+        properties[0].state.extra_data["require_in_plan_column"] = True
+        properties[0].state.save()
+        properties[1].state.extra_data["require_in_plan_column"] = True
+        properties[1].state.save()
+        properties[2].state.extra_data["require_in_plan_column"] = False
+        properties[2].state.save()
+
+        # Action
+        results = self.facilities_plan.get_selected_properties(self.ali, self.cycle)
+
+        # Assert
+        self.assertListEqual(
+            [
+                {
+                    "total_energy_usage": round(r.total_energy_usage, 2),
+                    "percentage_of_total_energy_usage": round(r.percentage_of_total_energy_usage, 2),
+                    "rank": round(r.rank, 2),
+                    "running_percentage": round(r.running_percentage, 2),
+                    "running_square_footage": round(r.running_square_footage, 2),
+                }
+                for r in results.property_rankings.all()
+            ],
+            [
+                {
+                    "total_energy_usage": 20.0,
+                    "percentage_of_total_energy_usage": 0.2,
+                    "rank": 0,
+                    "running_percentage": 0.2,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 10.0,
+                    "percentage_of_total_energy_usage": 0.1,
+                    "rank": 1,
+                    "running_percentage": 0.3,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 40.0,
+                    "percentage_of_total_energy_usage": 0.4,
+                    "rank": 2,
+                    "running_percentage": 0.7,
+                    "running_square_footage": 0.0,
+                },
+                {
+                    "total_energy_usage": 30.0,
+                    "percentage_of_total_energy_usage": 0.3,
+                    "rank": 3,
+                    "running_percentage": 1.0,
+                    "running_square_footage": 0.0,
+                },
+            ],
         )
