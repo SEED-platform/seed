c3eb906d6d98b8ec1e4fa04d7936bcd05812bda5
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2025-05-09 12:43:33 -0600

    Fix bsyncr xml timeseries ordering (#4982)
    fix bsyncr xml timeseries ordering

--- seed/analysis_pipelines/bsyncr.py
+++ seed/analysis_pipelines/bsyncr.py
@@ -283,11 +283,12 @@ def _build_bsyncr_input(analysis_property_view, meter):
                                         E.TimeSeries(
                                             {"ID": f"TimeSeries-{i}"},
                                             E.StartTimestamp(reading.start_time.isoformat()),
+                                            E.EndTimestamp(reading.end_time.isoformat()),
                                             E.IntervalFrequency("Month"),
                                             E.IntervalReading(str(reading.reading)),
                                             E.ResourceUseID({"IDref": elec_resource_id}),
                                         )
-                                        for i, reading in enumerate(meter.meter_readings.all())
+                                        for i, reading in enumerate(meter.meter_readings.all().order_by("start_time"))
                                     ]
                                 ),
                             )
