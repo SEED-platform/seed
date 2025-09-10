47a2aedd25835100460983663b46e7e07ee35f37
Ross Perry <ross.perry@Rosss-MacBook-Pro-2.local>
2025-09-10 15:38:19 -0600

    regex refeactor

--- seed/static/seed/js/controllers/inventory_list_controller.js
+++ seed/static/seed/js/controllers/inventory_list_controller.js
@@ -1915,7 +1915,7 @@ angular.module('SEED.controller.inventory_list', []).controller('inventory_list_
     };
 
     // https://regexr.com/6cka2
-    const combinedRegex = /^(!?)=\s*(-?\d+(?:\.\d+)?)$|^(!?)=?\s*"((?:[^"]|\\")*)"$|^(<=?|>=?|!?=)\s*((-?\d+(?:\.\d+)?)|(\d{4}-\d{2}(?:-\d{2})?))$/;
+    const combinedRegex = /^(!?)=\s*(-?\d+(?:\.\d+)?)$|^(!?)=?\s*"((?:[^"]|\\")*)"$|^(<=|>=|!=|=|<|>|!)\s*((-?\d+(?:\.\d+)?)|(\d{4}-\d{2}(?:-\d{2})?))$/;
     const parseFilter = (expression) => {
       // parses an expression string into an object containing operator and value
       const filterData = expression.match(combinedRegex);
@@ -1966,6 +1966,8 @@ angular.module('SEED.controller.inventory_list', []).controller('inventory_list_
             case '>=':
               return { string: '>=', operator: 'gte', value };
             case '!=':
+              return { string: 'is not', operator: 'ne', value };
+            case '!':
               return { string: 'is not', operator: 'ne', value };
             case '=':
               return { string: 'is', operator: 'exact', value };
