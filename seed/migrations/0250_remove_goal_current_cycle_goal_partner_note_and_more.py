437e377e6459023220dca17f09e6532d67b7bb3a
heslinge <han.eslinger@gmail.com>
2025-12-09 12:38:01 -0700

    Fix migrations

--- seed/migrations/0250_remove_goal_current_cycle_goal_partner_note_and_more.py
+++ seed/migrations/0250_remove_goal_current_cycle_goal_partner_note_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("orgs", "0044_add_bb_salesforce_enabled"),
+        ("orgs", "0249_alter_analysis_service"),
         ("seed", "0248_update_bsync_measures_v2_7_0"),
     ]
 
