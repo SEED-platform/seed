ead74de09368dd925242fc1fb3a6f4bd1c0aae4e
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2026-01-13 14:39:49 -0700

    Fix code retrieval for bb salesforce URL (#5165)
    fix code retrieval for bb salesforce URL

--- seed/static/seed/js/controllers/salesforce_login_controller.js
+++ seed/static/seed/js/controllers/salesforce_login_controller.js
@@ -5,7 +5,6 @@
 angular.module('SEED.controller.salesforce_login', []).controller('salesforce_login_controller', [
   '$scope',
   '$state',
-  '$location',
   '$window',
   'bb_salesforce_service',
   'organization_id',
@@ -13,7 +12,6 @@ angular.module('SEED.controller.salesforce_login', []).controller('salesforce_lo
   function (
     $scope,
     $state,
-    $location,
     $window,
     bb_salesforce_service,
     organization_id
@@ -21,7 +19,10 @@ angular.module('SEED.controller.salesforce_login', []).controller('salesforce_lo
     $scope.organization_id = organization_id;
     $scope.write_permission = ($scope.menu.user.is_ali_root || !$scope.menu.user.is_ali_leaf) && !$scope.viewer;
     $scope.AUTHENTICATION_STATE = 'PENDING'; // PENDING, FAILURE, SUCCESS
-    const code = $location.search().code;
+    $scope.error_message = null;
+    // get code from browser URL (code gets put in the middle of the URL by salesforce)
+    const params = new URLSearchParams($window.location.search);
+    const code = params.get('code');
     bb_salesforce_service.get_token(code, organization_id).then((data) => {
       if (data.status === 'success') {
         $scope.AUTHENTICATION_STATE = 'SUCCESS';
