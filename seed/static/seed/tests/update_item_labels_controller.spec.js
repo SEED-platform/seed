917646a08866d4006f4091f2ca6e9b097f3e4e0b
Alex Swindler <alex.swindler@nrel.gov>
2026-01-27 10:26:49 -0700

    Update copyright to 2026 (#5160)

--- seed/static/seed/tests/update_item_labels_controller.spec.js
+++ seed/static/seed/tests/update_item_labels_controller.spec.js
@@ -1,5 +1,5 @@
 /**
- * SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
+ * SEED Platform (TM), Copyright (c) Alliance for Energy Innovation, LLC, and other contributors.
  * See also https://github.com/SEED-platform/seed/blob/main/LICENSE.md
  */
 describe('controller: update_item_labels_modal_controller', () => {
