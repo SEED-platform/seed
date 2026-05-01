9db54f440d27b85c0943286b5c408e30ec7fe764
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2026-05-01 16:06:11 -0600

    lint

--- seed/static/seed/js/controllers/copy_to_different_cycle_modal_controller.js
+++ seed/static/seed/js/controllers/copy_to_different_cycle_modal_controller.js
@@ -32,12 +32,26 @@ angular.module('SEED.controller.copy_to_different_cycle_modal', []).controller('
       result: {}
     };
 
+    const handle_response = (message, error = false) => {
+      $scope.status.status_message = message;
+      if (error) {
+        $scope.status.in_progress = false;
+        $scope.status.complete = false;
+      } else {
+        $scope.status.complete = true;
+        $scope.status.in_progress = false;
+      }
+    };
+
     $scope.save = () => {
       $scope.status.in_progress = true;
+      const cycle_id = $scope.selected_cycle.id;
+      const column_ids = $scope.selected_column_list_profile.columns.map((c) => c.id);
+
       inventory_service.copy_to_cycle(
-        cycle_id = $scope.selected_cycle.id,
-        view_ids = view_ids,
-        column_ids = $scope.selected_column_list_profile.columns.map((c) => c.id)
+        cycle_id,
+        view_ids,
+        column_ids
       ).then((response) => {
         const data = response.data;
         if (response.status !== 200) {
