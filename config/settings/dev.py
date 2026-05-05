813769072f900b6a6632f4a8073a856a8ab69744
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2026-05-05 17:21:57 -0600

    add new BETTER-to-BSYNC building type mappings & 2 small tweaks for sr new UI

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -13,6 +13,10 @@ DEBUG = True
 compress = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
 
 COMPRESS_ENABLED = compress
 COMPRESS_OFFLINE = compress
