61a867b3627a20053b2ee4c19b19f152fb113343
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2026-05-13 21:22:41 -0600

    Make filter groups optional for custom report configurations (#5232)
    * make filter groups optional for custom reports
    
    * precommit
    
    * fix for tests to pass

--- seed/models/data_views.py
+++ seed/models/data_views.py
@@ -7,7 +7,7 @@ import contextlib
 import logging
 
 from django.db import models
-from django.db.models import Avg, Count, F, Max, Min, Sum
+from django.db.models import Avg, Count, F, Max, Min, Q, Sum
 from django.http import QueryDict
 
 from seed.lib.superperms.orgs.models import Organization
@@ -24,15 +24,33 @@ class DataView(models.Model):
     name = models.CharField(max_length=255, unique=True)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     cycles = models.ManyToManyField(Cycle)
-    filter_groups = models.ManyToManyField(FilterGroup)
+    filter_groups = models.ManyToManyField(FilterGroup, blank=True)
 
     def get_inventory(self, user_ali):
+        if not self.filter_groups.exists():
+            return {}
         views_by_filter_group_id, _ = self.views_by_filter(user_ali)
         return views_by_filter_group_id
 
     def views_by_filter(self, user_ali):
         filter_group_views = {}
         views_by_filter_group_id = {}
+
+        if not self.filter_groups.exists():
+            # No filter groups: include all property views per cycle
+            views_by_filter_group_id[0] = {}
+            filter_group_views[0] = {}
+            for cycle in self.cycles.all():
+                views = PropertyView.objects.filter(
+                    cycle=cycle,
+                    property__organization=self.organization,
+                )
+                filter_group_views[0][cycle.id] = views
+                for view in views:
+                    view_name = self._format_property_display_field(view)
+                    views_by_filter_group_id[0][view.id] = view_name
+            return views_by_filter_group_id, filter_group_views
+
         for filter_group in self.filter_groups.all():
             views_by_filter_group_id[filter_group.id] = {}
             filter_group_views[filter_group.id] = {}
@@ -104,13 +122,17 @@ class DataView(models.Model):
         response["views_by_filter_group_id"], views_by_filter = self.views_by_filter(user_ali)
 
         # assign data based on source column id
+        # Build list of (filter_id, filter_name) pairs
+        filter_entries = [(fg.id, fg.name) for fg in self.filter_groups.all()]
+        if not filter_entries:
+            filter_entries = [(0, "All")]
+
         for column in columns:
             data = response["columns_by_id"]
             column_id = column.id
             data[column_id] = {"filter_groups_by_id": {}, "unit": None}
 
-            for filter_group in self.filter_groups.all():
-                filter_id = filter_group.id
+            for filter_id, _filter_name in filter_entries:
                 data[column_id]["filter_groups_by_id"][filter_id] = {"cycles_by_id": {}}
 
                 for cycle in self.cycles.all():
@@ -134,9 +156,11 @@ class DataView(models.Model):
 
     def _format_graph_data(self, response, columns, views_by_filter):
         # {filter_group: filter_group.name, column: column.column_name, aggregation: aggregation.name, data: [1,2,3]},
-        for filter_group in self.filter_groups.all():
-            filter_id = filter_group.id
-            filter_name = filter_group.name
+        filter_entries = [(fg.id, fg.name) for fg in self.filter_groups.all()]
+        if not filter_entries:
+            filter_entries = [(0, "All")]
+
+        for filter_id, filter_name in filter_entries:
             for column in columns:
                 for aggregation in [Avg, Max, Min, Sum, Count]:  # NEED TO ADD 'views_by_label' for scatter plot
                     self._format_aggregation_name(aggregation)
@@ -281,6 +305,8 @@ class DataView(models.Model):
         org_id = self.organization.id
         columns = Column.retrieve_all(org_id=org_id, inventory_type="property", only_used=False, include_related=False)
         annotations = {}
+        order_by = []
+        filters = Q()
         try:
             filters, annotations, order_by = build_view_filters_and_sorts(query_dict, columns, "property")
         except Exception:
