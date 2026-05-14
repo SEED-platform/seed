b8bb11c1c3ee6eaa73619fc6169507e47e3f988c
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2026-05-13 21:22:10 -0600

    update summary endpoint to return total gfa like v3 did

--- seed/views/v4/analyses.py
+++ seed/views/v4/analyses.py
@@ -65,11 +65,15 @@ class AnalysisViewSet(viewsets.ViewSet, OrgMixin):
             for c in columns
         ]
 
+        gfa_list = PropertyState.objects.filter(id__in=state_ids).values_list("gross_floor_area", flat=True)
+        total_sqft = sum(x.magnitude for x in gfa_list if x is not None) if any(x is not None for x in gfa_list) else 0
+
         return JsonResponse(
             {
                 "status": "success",
                 "total_records": len(state_ids),
                 "number_extra_data_fields": len(extra_data_columns),
+                "total_sqft": total_sqft,
                 "stats": stats,
             }
         )
